30d7d1ec643552a0a506e49ed60c53898df6816d
Warren Strange <warren.strange@forgerock.com>
2017-06-14 11:05:43 -0600

    fix setup, change AM to milestone
    remove ingress/ directory as we now use helm. Add autoscale GKE sample
    
    updated setup.sh. Changed amster to a deployment so it is more robust / self healing

--- docker/toolbox/bin/setup.sh
+++ docker/toolbox/bin/setup.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # Edit this based on your environment!
-# Prerequisites: you must have VirtualBox and homebrew installed.
+# Prerequisites: you must have homebrew installed.
 brew update
+
+# If you installed VirtualBox via the downloaded .dmg, you may find minikube
+# won't start due to permissions. Installing via brew fixes this up:
+brew cask install virtualbox
 
 brew install docker-machine docker-compose kubernetes-helm kubernetes-cli git
 
@@ -12,6 +16,9 @@ echo "Creating a Minikube VM"
 # The command below defaults to using VirtualBox - which we assume you have installed
 # Add the --vm-driver=vmwarefusion  or --vm-driver=xhyve  if you want to change the hypervisor.
 minikube start --memory 8096 --kubernetes-version v1.6.4
+
+minikube status
+
 
 echo "Enabling the ingress controller"
 minikube addons enable ingress
