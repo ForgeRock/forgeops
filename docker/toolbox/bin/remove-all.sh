0c16bc434f7cd0aa8b3cb1385c160c43c356e750
David Goldsmith <David.Goldsmith@ForgeRock.com>
2017-08-01 14:17:37 -0700

    Updated default K8S cluster version, parameterized namespace for remove-all.sh

--- docker/toolbox/bin/remove-all.sh
+++ docker/toolbox/bin/remove-all.sh
@@ -1,23 +1,33 @@
 #!/usr/bin/env bash
 # This removes *all* helm charts in the current namespace and delete the PVCs / PV in the current namespace
 # Use with caution - this deletes all of your data as well...
-#
 
-# Get the namespace context
-NS=`kubectl config view | grep namespace | awk  '{print $2}'`
-DEFAULT_NAMESPACE=${NS:-default}
-export DEFAULT_NAMESPACE
+# Only argument is --namespace
+NAMESPACE=default
+while [[ $# > 0 ]]
+do
+  KEY=$1
+  shift
+  case $KEY in
+    # Which namespace to delete charts from
+    --namespace)
+      NAMESPACE=$1
+      shift
+      ;;
+  esac
+done
 
-releases=`helm list --namespace ${DEFAULT_NAMESPACE} -q`
-
+# Delete helm charts in specified namespace (of default namespace, if none specified)
+releases=`helm list --namespace ${NAMESPACE} -q`
 for r in ${releases}
+
 do
     echo "Deleting release $r"
     helm delete --purge $r
 done
 
-
-pvclist=`kubectl get pvc -o jsonpath='{.items[*].metadata.name}'`
+# Delete persistent volume claims
+pvclist=`kubectl get pvc --namespace ${NAMESPACE} -o jsonpath='{.items[*].metadata.name}'`
 
 for pvc in ${pvclist}
 do
