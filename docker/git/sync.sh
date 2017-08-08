2ffe90e6fa702e067d029210213dd52f192a3789
Warren Strange <warren.strange@forgerock.com>
2017-08-08 09:37:29 -0600

    DJ: add anti-affinity, git: add project directory setting

--- docker/git/sync.sh
+++ docker/git/sync.sh
@@ -4,8 +4,10 @@
 set -x
 # Top level directory where git projects will be cloned to.
 GIT_ROOT=${GIT_ROOT:=/git}
+GIT_PROJECT_DIR="${GIT_PROJECT_DIR:-forgeops-init}"
 
-GIT_AUTOSAVE_BRANCH=${GIT_AUTOSAVE_BRANCH:-autosave}
+
+GIT_AUTOSAVE_BRANCH="${GIT_AUTOSAVE_BRANCH:-autosave}"
 
 # Default time in seconds between commit / push.
 INTERVAL=${GIT_PUSH_INTERVAL:-300}
@@ -22,8 +24,8 @@ fi
 
 export GIT_SSH_COMMAND="ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /etc/git-secret/ssh"
 
-# We don't know the name of the git repo that was cloned, but there should only be a single config directory under the GIT_ROOT.
-cd ${GIT_ROOT}/*
+
+cd "${GIT_ROOT}/${GIT_PROJECT_DIR}"
 
 
 # This configures git to ignore file mode changes.
