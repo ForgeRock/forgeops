877f932b67386f3c686f4804f17e5c689cea2071
Warren Strange <warren.strange@forgerock.com>
2017-09-27 09:53:34 -0600

    script to update Helm dependencies

---
+++ helm/update-deps.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Utility script to update Helm chart dependencies. If you are installing Helm charts from
+# the source in this directory, update the deps before doing a helm install.
+
+for file in *
+do
+    # Is it a Helm chart?
+    if [ -r $file/Chart.yaml ]; then
+        echo "Updating Helm dependencies for $file"
+        helm dep up $file
+    fi
+done
