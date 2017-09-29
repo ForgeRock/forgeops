7826ab80b417d2f526d955ab425e6f7f8a8de428
Warren Strange <warren.strange@forgerock.com>
2017-09-29 10:51:25 -0600

    update dj benchmark. Default sshKey if it is not set

--- docker/opendj/benchmark.sh
+++ docker/opendj/benchmark.sh
@@ -8,22 +8,25 @@ BASEDN="${BASE_DN:-dc=openam,dc=forgerock,dc=org}"
 USERS="${NUMBER_SAMPLE_USERS:-1000000}"
 #HOST=userstore-0.userstore
 HOST=localhost
-
+CONNECTIONS=10
 
 
 echo "Searchrate test"
+# searchrate needs more time to warm up - so we double the iterations.
+ITER2=$(expr "${ITERATIONS}" \* 2 )
+
 time bin/searchrate  -h $HOST -p 1389 -D "cn=directory manager"  -j "${DIR_MANAGER_PW_FILE}" \
-    -F -c 4 -t 4 -m $ITERATIONS -b "${BASE_DN}" -g "rand(0,$USERS)" "(uid=user.%d)"
+    -F -c "${CONNECTIONS}" -t 4 -m "${ITER2}" -b "${BASE_DN}" -g "rand(0,$USERS)" "(uid=user.%d)"
 
 
 echo  "Authrate  test"
-time bin/authrate -h $HOST -p 1389 -D '%2$s' -w password -f -c 10  -m $ITERATIONS  \
+time bin/authrate -h $HOST -p 1389 -D '%2$s' -w password -f -c "${CONNECTIONS}"  -m $ITERATIONS  \
     -b "ou=people,$BASEDN" -s one -g "rand(0,$USERS)" "(uid=user.%d)"
 
 # modrate - this is destructive!
 echo "Modrate test"
 time bin/modrate -h $HOST -p 1389 -D "cn=directory manager" -j "${DIR_MANAGER_PW_FILE}" \
-    -F -c 4 -t 4 -m $ITERATIONS -b "uid=user.%d,ou=people,$BASEDN" \
+    -F -c "${CONNECTIONS}" -t 4 -m $ITERATIONS -b "uid=user.%d,ou=people,$BASEDN" \
     -g "rand(0,$USERS)" -g "randstr(16)" 'description:%2$s'
 
 
@@ -69,7 +72,7 @@ EOF
 echo "addrate test (slow)"
 
 # Reduce the number of iterations -because this test is slow!!
-ITER2=$(expr "${ITERATIONS}" / 100 )
+ITER2=$(expr "${ITERATIONS}" / 10 )
 time bin/addrate -h $HOST -p 1389 -D "cn=directory manager"  -j "${DIR_MANAGER_PW_FILE}"  \
-    -f -c 1 -C random -m "${ITER2}" -s 1000 /tmp/addrate.template
+    -F -c "${CONNECTIONS}" -C random -m "${ITER2}" -s 1000 /tmp/addrate.template
 
