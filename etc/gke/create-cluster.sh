a1fc2afa61def600bb6ec7e887f8e046c9da17ef
David Goldsmith <David.Goldsmith@ForgeRock.com>
2017-11-20 11:14:37 -0800

    CLOUD-132 Rebranded Google Container Engine to Google Kubernetes Engine

--- etc/gke/create-cluster.sh
+++ etc/gke/create-cluster.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2016-2017 ForgeRock AS. Use of this source code is subject to the
 # Common Development and Distribution License (CDDL) that can be found in the LICENSE file
 #
-# Sample script to create a Kubernetes cluster on Google Container Engine (GKE)
+# Sample script to create a Kubernetes cluster on Google Kubernetes Engine (GKE)
 # You must have the gcloud command installed and access to a GCP project.
 # See https://cloud.google.com/container-engine/docs/quickstart
 
