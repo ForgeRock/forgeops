64f8b38ad69388689d812b53a79df9e4d9be4edf
Warren Strange <warren.strange@forgerock.com>
2018-02-02 18:02:53 -0700

    update gke ingress for rbac. Add abilty to turn off replication job on opendj-cluster chart

--- etc/gke/gke-ingress.sh
+++ etc/gke/gke-ingress.sh
@@ -4,8 +4,12 @@
 # The loadBalancerIP can be used if you have a static IP to assign to the outer L4 load balancer service.
 # If this is not set a dynamic IP will be assigned.
 # The publishService.enabled attribute will tell nginx to publish the L4 IP as the ingress IP.
-helm install --namespace nginx  --set "controller.service.loadBalancerIP=35.184.100.105" \
-     --set "controller.publishService.enabled=true"  stable/nginx-ingress
+
+# Set this IP to your reserved IP. Must be in the same zone as your cluster.
+MYIP=xx.xx.xx.xx
+helm install --namespace nginx  --set "controller.service.loadBalancerIP=$MYIP" \
+    --set rbac.create=true \
+    --set "controller.publishService.enabled=true"  stable/nginx-ingress
 
 
 
