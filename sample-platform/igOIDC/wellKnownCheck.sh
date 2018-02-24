b3c24a2d05d496553f2114f3e7af5ac4497deab7
Jake Feasel <jake.feasel@forgerock.com>
2018-02-23 16:25:51 -0800

    More reliable method for testing well-known endpoint

--- sample-platform/igOIDC/wellKnownCheck.sh
+++ sample-platform/igOIDC/wellKnownCheck.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 while true; do
-    wget -O /dev/null $WELL_KNOWN_ENDPOINT
-    if [[ "$?" -eq 0 ]]; then
+    STATUS=`curl -I $WELL_KNOWN_ENDPOINT  2>/dev/null | head -n 1 | cut -d$' ' -f2`
+    echo $STATUS
+    if [[ "$STATUS" == "200" ]]; then
       break
     fi
     sleep 10
