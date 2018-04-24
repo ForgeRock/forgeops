f0f97db97daf922dc55fa483ecf50720b7beda59
Warren Strange <warren.strange@gmail.com>
2018-04-24 11:31:30 -0600

    Cleanup for release/6.0.0

--- etc/gke/gke-up.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Sample script to initialize GKE. This creates the cluster and configures Helm, the nginx ingress, and 
-# creates git credential secrets. Edit this for your requirements.
-./create-cluster.sh
-
-kubectl create -f storage.yaml
-
-helm init
