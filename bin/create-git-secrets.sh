1df115aa1f941aaf048d5f6f5e1f02116e201c50
Warren Strange <warren.strange@gmail.com>
2018-05-15 17:04:39 -0600

    update script comment

--- bin/create-git-secrets.sh
+++ bin/create-git-secrets.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
-# Copyright (c) 2016-2017 ForgeRock AS. Use of this source code is subject to the
-# Common Development and Distribution License (CDDL) that can be found in the LICENSE file
-#
-# Sample script to create a Kubernetes cluster on Google Kubernetes Engine (GKE)
-# You must have the gcloud command installed and access to a GCP project.
-# See https://cloud.google.com/container-engine/docs/quickstart
+# Copyright (c) 2016-2017 ForgeRock. All rights reserved.
 
-
-# This is a sample of how to create a git ssh secret. 
+# This is a sample of how to create a git ssh secret for configuration cloning.
 
 # Generate the key pair
 ssh-keygen -t rsa -C "forgeopsrobot@forgrock.com" -f id_rsa -N ''
