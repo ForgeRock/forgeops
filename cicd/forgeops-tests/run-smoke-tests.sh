0c75272ee44a8b8a7218abcc465691f1cb5aeead
pavel.balcarek <pavel.balcarek@forgerock.com>
2018-06-12 14:30:12 +0200

    Tailing results so we don't get too much spam in notifications

--- cicd/forgeops-tests/run-smoke-tests.sh
+++ cicd/forgeops-tests/run-smoke-tests.sh
@@ -3,4 +3,5 @@
 # Helper script for google cloud build to keep it simple to run tests
 # while having stdout in file to send to slack
 
-python3 forgeops-tests.py --suite tests/smoke &> results.txt
+python3 forgeops-tests.py --suite tests/smoke/am &> res.txt
+tail -n 7 res.txt > results.txt
