8b13c62349b6dd3237e2f4a59b286fcbb370fbd6
Pavel Balcarek <pavel.balcarek@forgerock.com>
2018-08-07 12:35:18 +0200

    Adding user create as temporary fix for smoke tests (#191)
    Users are now being created as part of tests (AM, Agents). This removes dependency on existing users.

--- cicd/forgeops-tests/tests/smoke/am/AMSmoke.py
+++ cicd/forgeops-tests/tests/smoke/am/AMSmoke.py
@@ -2,7 +2,7 @@
 Basic OpenAM smoke test suite.
 """
 import unittest
-from requests import get, post
+from requests import get, post, delete
 
 from config.ProductConfig import AMConfig
 
@@ -10,20 +10,53 @@ from config.ProductConfig import AMConfig
 class AMSmoke(unittest.TestCase):
     amcfg = AMConfig()
 
-    def test_ping(self):
+    def test_0_setup(self):
+        """
+        Setup a user that will be tested in user login.
+
+        """
+        headers = {'X-OpenAM-Username': 'amadmin', 'X-OpenAM-Password': 'password',
+                   'Content-Type': 'application/json', 'Accept-API-Version': 'resource=2.0, protocol=1.0'}
+        resp = post(url=self.amcfg.rest_authn_url, headers=headers)
+        self.assertEqual(200, resp.status_code, 'Admin login')
+        admin_token = resp.json()["tokenId"]
+
+        headers = {'iPlanetDirectoryPro': admin_token, 'Content-Type': 'application/json',
+                   'Accept-API-Version': 'resource=3.0, protocol=2.1'}
+        user_data = {'username': 'testuser',
+                     'userpassword': 'password',
+                     'mail': 'testuser@forgerock.com'}
+        resp = post(self.amcfg.am_url + '/json/realms/root/users/?_action=create', headers=headers, json=user_data)
+        self.assertEqual(201, resp.status_code, "Expecting test user to be created - HTTP-201")
+
+    def test_1_ping(self):
         resp = get(url=self.amcfg.am_url + '/isAlive.jsp')
         self.assertEqual(200, resp.status_code, "Ping OpenAM isAlive.jsp")
 
-    def test_admin_login(self):
+    def test_2_admin_login(self):
         headers = {'X-OpenAM-Username': 'amadmin', 'X-OpenAM-Password': 'password',
                    'Content-Type': 'application/json', 'Accept-API-Version': 'resource=2.0, protocol=1.0'}
         resp = post(url=self.amcfg.rest_authn_url, headers=headers)
         self.assertEqual(200, resp.status_code, 'Admin authn REST')
 
-    def test_user_login(self):
-        headers = {'X-OpenAM-Username': 'user.1',
+    def test_4_user_login(self):
+        headers = {'X-OpenAM-Username': 'testuser',
                    'X-OpenAM-Password': 'password',
                    'Content-Type': 'application/json',
                    'Accept-API-Version': 'resource=2.0, protocol=1.0'}
         resp = post(url=self.amcfg.rest_authn_url, headers=headers)
         self.assertEqual(200, resp.status_code, 'User authn REST')
+
+    def test_5_user_delete(self):
+
+        headers = {'X-OpenAM-Username': 'amadmin', 'X-OpenAM-Password': 'password',
+                   'Content-Type': 'application/json', 'Accept-API-Version': 'resource=2.0, protocol=1.0'}
+        resp = post(url=self.amcfg.rest_authn_url, headers=headers)
+        self.assertEqual(200, resp.status_code, 'Admin login')
+        admin_token = resp.json()["tokenId"]
+
+        headers = {'iPlanetDirectoryPro': admin_token, 'Content-Type': 'application/json',
+                   'Accept-API-Version': 'resource=3.0, protocol=2.1'}
+
+        resp = delete(self.amcfg.am_url + '/json/realms/root/users/testuser', headers=headers)
+        self.assertEqual(200, resp.status_code, "Expecting test user to be deleted - HTTP-200")
