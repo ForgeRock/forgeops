51ab1879a429033183fe17ef849d7cd382b8b0f8
pavel.balcarek <pavel.balcarek@forgerock.com>
2018-08-22 17:23:54 +0200

    Fix for nginx agent. Fix for log copy(hope it's final one)

--- cicd/forgeops-tests/get-logs-from-containers.sh
+++ cicd/forgeops-tests/get-logs-from-containers.sh
@@ -13,16 +13,19 @@ INGRESS="ingress"
 POD_LIST=$(kubectl -n=${NAMESPACE} get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
 
 # Go through all pods and get containers for each pod. Get logs from these containers
+E_TIME=$(date +%s)
 mkdir logs
+mkdir logs/${E_TIME}
+
 
 for pod in ${POD_LIST}; do
   echo "Getting info from:" ${pod}
   containers=$(kubectl -n=${NAMESPACE} get pod ${pod} -o jsonpath='{.spec.containers[*].name}' | tr " " "\n")
-  kubectl -n=${NAMESPACE} describe pod ${pod} > logs/${pod}-description.log
+  kubectl -n=${NAMESPACE} describe pod ${pod} > logs/${E_TIME}/${pod}-description.log
 
   for container in ${containers}; do
-    echo "LOG BEGINNING============>" > logs/${pod}-${container}.log
-    kubectl -n=${NAMESPACE} logs ${pod} ${container} >> logs/${pod}-${container}.log
-    echo "LOG END==================>" >> logs/${pod}-${container}.log
+    echo "LOG BEGINNING============>" > logs/${E_TIME}/${pod}-${container}.log
+    kubectl -n=${NAMESPACE} logs ${pod} ${container} >> logs/${E_TIME}/${pod}-${container}.log
+    echo "LOG END==================>" >> logs/${E_TIME}/${pod}-${container}.log
   done
 done
