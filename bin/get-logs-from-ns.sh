072c40c7e50eee9b002dda5a48a1bb09537c8d4b
pavel.balcarek <pavel.balcarek@forgerock.com>
2018-09-06 10:01:07 +0200

    Couple of modifications to log collection

--- bin/get-logs-from-ns.sh
+++ bin/get-logs-from-ns.sh
@@ -8,8 +8,11 @@
 #
 # Set NAMESPACE & EXPORT_PATH env vars to match your needs
 #
+
 : ${NAMESPACE:=smoke}
 : ${EXPORT_PATH:=.}
+
+# TODO - Add line limitation
 
 # Get all running pods, services, deployments and ingresses in namespace
 POD_LIST=$(kubectl -n=${NAMESPACE} get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
@@ -20,44 +23,43 @@ DEPLOYMENT_LIST=$(kubectl -n=${NAMESPACE} get deployment -o go-template --templa
 cd ${EXPORT_PATH}
 
 # Make folders for logs
-mkdir logs
-mkdir logs/pods
-mkdir logs/services
-mkdir logs/deployments
-mkdir logs/ingress
+E_TIME=$(date +%s)
+if [ ! -d "logs" ]; then
+  mkdir logs
+fi
+
+mkdir logs/${E_TIME}
+cd logs/${E_TIME}
+
+mkdir pods
+mkdir services
+mkdir deployments
+mkdir ingress
 
 
 # Get pods/container logs & descriptions
 for pod in ${POD_LIST}; do
   echo "Getting POD details from:" ${pod}
   containers=$(kubectl -n=${NAMESPACE} get pod ${pod} -o jsonpath='{.spec.containers[*].name}' | tr " " "\n")
-  kubectl -n=${NAMESPACE} describe pod ${pod} > logs/pods/${pod}-description.log
+  kubectl -n=${NAMESPACE} describe pod ${pod} > pods/${pod}-description.log
   # Iterate through all containers in pod
   for container in ${containers}; do
-    echo "LOG BEGINNING============>" > logs/pods/${pod}-${container}.log
-    kubectl -n=${NAMESPACE} logs ${pod} ${container} >> logs/pods/${pod}-${container}.log
-    echo "LOG END==================>" >> logs/pods/${pod}-${container}.log
+    kubectl -n=${NAMESPACE} logs ${pod} ${container} > pods/${pod}-${container}.log
   done
 done
 
 # Save service descriptions
 for service in ${SERVICE_LIST}; do
   echo "Getting service description from: " ${service}
-  echo "LOG BEGINNING============>" > logs/services/${service}.log
-  kubectl -n=${NAMESPACE} describe service ${service} >>  logs/services/${service}.log
-  echo "LOG END==================>" >>  logs/services/${service}.log
+  kubectl -n=${NAMESPACE} describe service ${service} >  services/${service}.log
 done
 
 for deployment in ${DEPLOYMENT_LIST}; do
   echo "Getting deployment description from: " ${deployment}
-  echo "LOG BEGINNING============>" > logs/deployments/${deployment}.log
-  kubectl -n=${NAMESPACE} describe deployment ${deployment} >>  logs/deployments/${deployment}.log
-  echo "LOG END==================>" >>  logs/deployments/${deployment}.log
+  kubectl -n=${NAMESPACE} describe deployment ${deployment} > deployments/${deployment}.log
 done
 
 for ingress in ${INGRESS_LIST}; do
   echo "Getting ingress description from: " ${ingress}
-  echo "LOG BEGINNING============>" > logs/ingress/${ingress}.log
-  kubectl -n=${NAMESPACE} describe ingress ${ingress} >>  logs/ingress/${ingress}.log
-  echo "LOG END==================>" >>  logs/ingress/${ingress}.log
+  kubectl -n=${NAMESPACE} describe ingress ${ingress} > ingress/${ingress}.log
 done
