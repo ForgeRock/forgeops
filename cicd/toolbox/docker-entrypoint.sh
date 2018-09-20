04b43c59ea982c0fe802f75ec22994ef94ac76a9
Warren Strange <warren.strange@gmail.com>
2018-09-20 17:39:10 -0600

    add toolbox dockerfile

--- cicd/toolbox/docker-entrypoint.sh
+++ cicd/toolbox/docker-entrypoint.sh
@@ -1,42 +1,19 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-# Clone any git repos if GIT_REPO is set
-/opt/toolbox/bin/git-init.sh
 
 echo "Cmd is $1"
 
-echo "Initializing helm"
+echo "Cloning forgeops"
+cd /
+git clone https://github.com/ForgeRock/forgeops.git 
 
-helm init
-
-echo "Addding ForgeRock Helm Repo"
-helm repo add forgerock https://storage.googleapis.com/forgerock-charts
-
-export HELM_REPO=forgerock
-
-
-# Try to guess what kind of cluster we are running on - and copy the right starter custom.yaml
-node=minikube
-kubectl get node | grep minikube  >/dev/null
-if [ $? -ne 0 ]; 
-then
-    node=gke
-fi
-
-echo "It looks like you are running on a $node cluster"
-
-echo "Creating default custom.yaml"
-
-case $node in 
-    minikube)   
-        cp templates/custom.yaml .  ;;
-    gke) 
-        cp templates/custom-gke.yaml custom.yaml  ;;
-esac 
+# configure context so kubens does not complain
+kubectl config set-context default
+kubectl config use-context default 
 
 
 do_pause() {
-    echo "Sleeping" 
+    echo "Sleeping awaiting your command" 
     while true
     do
         sleep 10000
@@ -56,4 +33,3 @@ pause)
 esac
 
 
-# todo: optionally run bin/startall.sh
