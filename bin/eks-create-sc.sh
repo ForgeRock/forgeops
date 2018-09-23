4819bace2778e8985db61bcd38edaf41744c1f71
Wajih Ahmed <wahmed@mbpfr.wfoo.net>
2018-09-23 14:44:39 -0400

    This script creates the required storage classes for EKS

---
+++ bin/eks-create-sc.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Copyright (c) 2016-2017 ForgeRock AS. Use of this source code is subject to the
+# Common Development and Distribution License (CDDL) that can be found in the LICENSE file
+#
+# Sample script to create a Kubernetes cluster on Google Kubernetes Engine (GKE)
+# You must have the gcloud command installed and access to a GCP project.
+# See https://cloud.google.com/container-engine/docs/quickstart
+
+kubectl create -f - <<EOF
+kind: StorageClass
+apiVersion: storage.k8s.io/v1
+metadata:
+  name: fast
+provisioner: kubernetes.io/aws-ebs
+parameters:
+  type: io1
+  zones: us-east-1d,us-east-1c
+  iopsPerGB: "15000"
+  fstype: ext4
+---
+kind: StorageClass
+apiVersion: storage.k8s.io/v1
+metadata:
+  name: standard
+provisioner: kubernetes.io/aws-ebs
+parameters:
+  type: gp2
+EOF
+
+
+kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
