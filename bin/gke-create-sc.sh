5f52933ed3b6dc447fd9f59e0580443df333f8fe
Wajih Ahmed <wahmed@mbpfr.wfoo.net>
2018-09-23 14:46:42 -0400

    Renaming the GKE create-sc script to prefix with gke- because we now have one for EKS and will have others too in time

---
+++ bin/gke-create-sc.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Copyright (c) 2016-2017 ForgeRock AS. Use of this source code is subject to the
+# Common Development and Distribution License (CDDL) that can be found in the LICENSE file
+#
+# Sample script to create a Kubernetes cluster on Google Kubernetes Engine (GKE)
+# You must have the gcloud command installed and access to a GCP project.
+# See https://cloud.google.com/container-engine/docs/quickstart
+
+kubectl create -f - <<EOF
+kind: StorageClass
+apiVersion: storage.k8s.io/v1beta1
+metadata:
+  name: fast
+provisioner: kubernetes.io/gce-pd
+parameters:
+  type: pd-ssd
+EOF
