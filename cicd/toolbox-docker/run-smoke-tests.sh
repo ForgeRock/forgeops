301f1b28ee2ed59f8f782eeacb5ef788ccbe53f0
pavel.balcarek <pavel.balcarek@forgerock.com>
2018-10-08 10:39:05 +0200

    Saving work

--- cicd/toolbox-docker/run-smoke-tests.sh
+++ cicd/toolbox-docker/run-smoke-tests.sh
@@ -7,9 +7,13 @@ cd /workspace/forgeops
 clean_ns() {
     echo "Cleaning up older deployments"
     kubens $1
+<<<<<<< c010c60eb1e3a813d278beddac9f76aec7862df1
     ./bin/remove-all.sh 
     kubectl delete secret frconfig
     kubectl delete configmap frconfig
+=======
+    /forgeops/bin/remove-all.sh -N $1
+>>>>>>> Saving work
 }
 
 deploy_smoke() {
@@ -17,9 +21,25 @@ deploy_smoke() {
     ./bin/deploy.sh samples/config/smoke-deployment
 }
 
+run_tests() {
+    echo "Running smoke tests"
+    cd /forgeops/cicd/forgeops-tests/
+    ./run-smoke-tests.sh
+}
+
+get_logs() {
+    echo "Getting logs from smoke namespace"
+    cd /forgeops/bin
+    NAMESPACE=$1 ./get-logs-from-ns.sh
+}
+
+sleep_for_30_minutes() {
+    echo "Sleeping for 30 minutes for runner to be able to get logs and reports"
+    sleep 1800
+}
 
 clean_ns smoke
 deploy_smoke
-
-# Todo: Run python tests...
-# Do we want the smoke tests to be a separate chart ?
+run_tests
+get_logs smoke
+sleep_for_30_minutes
