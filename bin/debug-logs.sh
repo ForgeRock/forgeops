11e8c362c15874022b2769a494c882f93e8af4f6
Warren Strange <warren.strange@gmail.com>
2018-10-09 10:26:02 -0600

    Enchance debug-logs script to show additional objects. [skip ci] CLOUD-821

--- bin/debug-logs.sh
+++ bin/debug-logs.sh
@@ -2,12 +2,10 @@
 #
 # Simple script to get all logs, descriptions, events from selected namespace.
 # Useful for debugging.
-#
 
-SERVICE="service"
-DEPLOYMENT="deployment"
-INGRESS="ingress"
 
+# Max lines to print for pod logs
+NUM_LINES=200
 
 kcontext=`kubectl config current-context`
 NS=`kubectl config view -o jsonpath="{.contexts[?(@.name==\"$kcontext\")].context.namespace}"`
@@ -27,15 +25,17 @@ E_TIME=`date`
 mkdir -p /tmp/forgeops
 rm /tmp/forgeops/*
 OUT=/tmp/forgeops/log-template.html
-TOC=/tmp/forgeops/log-toc.html
 
-rm -fr $TOC $OUT 
+rm -fr $OUT 
+
+# Table of contents for pods
+TOC=""
 
 for pod in ${POD_LIST}; do
   echo "Generating $pod logs"
   echo "<h2><a id=\"${pod}\">Pod ${pod} </a></h2>" >> $OUT 
 
-  echo "<li><a href=\"#${pod}\">${pod}</a></li>" >> $TOC 
+  TOC="$TOC <li><a href=\"#${pod}\">${pod}</a></li>"
 
   init_containers=$(kubectl -n=${NAMESPACE} get pod ${pod} -o jsonpath='{.spec.initContainers[*].name}' | tr " " "\n")
   containers=$(kubectl -n=${NAMESPACE} get pod ${pod} -o jsonpath='{.spec.containers[*].name}' | tr " " "\n")
@@ -46,32 +46,50 @@ for pod in ${POD_LIST}; do
 
   for container in ${init_containers}; do 
       echo "<h3>Logs for init container: $container</h3><br><pre>" >> $OUT
-       kubectl -n=${NAMESPACE} logs ${pod} ${container} | head -1000 >> $OUT
+       kubectl -n=${NAMESPACE} logs ${pod} ${container} | head -"$NUM_LINES" >> $OUT
        echo "</pre><br><hr>" >> $OUT 
   done
 
   for container in ${containers}; do
     echo "<h3>Logs for container: $container</h3><br><pre>" >> $OUT
-    kubectl -n=${NAMESPACE} logs ${pod} ${container} | head -1000 >> $OUT
+    kubectl -n=${NAMESPACE} logs ${pod} ${container} | head -"$NUM_LINES" >> $OUT
     echo "</pre><br><hr>" >> $OUT 
   done
 
 done
 
+OBJECT_TOC=""
+
+objects="service ingress configmap secrets"
+for object in $objects; do
+    echo "<h2><a id=\"$object\">$object</a></h2><br/><pre>" >> $OUT
+    kubectl describe $object  >> $OUT
+
+    OBJECT_TOC="$OBJECT_TOC <li><a href=\"#$object\">$object</a></li>"
+
+    echo "</pre><br/><p><a href=\"#toc\">Back to Index</a></p>" >> $OUT
+done
+
+
 FINAL=/tmp/forgeops/log.html
 
 
+## Create the Header 
 cat >$FINAL <<EOF 
 <html>
 <h1>Debug Output for namespace ${NAMESPACE} taken at $E_TIME</h1>
 <br>
-<h1><a id="toc">Pods</a></h1>
+<h3><a id="toc">Pod Logs</a></h3>
 <ul>
+  $TOC
+</ul >
+<br/>
+<h3>Other Objects</h3>
+<ul>
+    $OBJECT_TOC
+</ul>
+<hr>
 EOF
-
-cat >>$FINAL $TOC
-
-echo "</ul ><hr>" >> $FINAL 
 
 cat >>$FINAL $OUT
 
