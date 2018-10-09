f773f0c8125cc3d3770ef8ca267ce28c58e730ff
Wajih Ahmed <wahmed@mbpfr.wfoo.net>
2018-10-09 17:19:28 -0400

    Re-purposed exisitng script to create a nodepool based on nodes that have a local disk

--- bin/gke-create-nodepool.sh
+++ bin/gke-create-nodepool.sh
@@ -8,15 +8,21 @@ source "${BASH_SOURCE%/*}/../etc/gke-env.cfg"
 
 
 # This adds a small nodepool that is non preemtible. Used for services that can't go down (nfs...)
-gcloud beta container node-pools create "${GKE_CLUSTER_NAME}-pool1" \
+gcloud beta container node-pools create "pool2-local-storage" \
+    --project="${GKE_PROJECT_ID}" \
     --cluster="${GKE_CLUSTER_NAME}"  \
     --zone="${GKE_PRIMARY_ZONE}" \
-    --machine-type="n1-standard-1" \
+    --machine-type="custom-8-24576" \
+    --min-cpu-platform="Intel Skylake" \
     --num-nodes="1" \
+    --node-labels="disk=local-nvme" \
     --enable-autorepair \
     --enable-autoupgrade \
-    --node-version="${GKE_CLUSTER_VERSION}" \
-    --scopes "https://www.googleapis.com/auth/cloud-platform" 
+    --scopes "https://www.googleapis.com/auth/cloud-platform" \
+    --disk-type=pd-ssd \
+    --local-ssd-count 1
 
 
 #    --node-taints="preemptible=true:NoSchedule"
+#    --node-version="${GKE_CLUSTER_VERSION}" \
+
