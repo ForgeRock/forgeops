a6aed41accb056a65a6c80f2044c666b04f338fa
Wajih Ahmed <wahmed@mbpfr.wfoo.net>
2018-10-09 17:35:34 -0400

    Revert "Re-purposed exisitng script to create a nodepool based on nodes that have a local disk"
    This reverts commit 3208a208832158269c480cf64921f70cd4047427.

--- bin/gke-create-nodepool.sh
+++ bin/gke-create-nodepool.sh
@@ -8,21 +8,15 @@ source "${BASH_SOURCE%/*}/../etc/gke-env.cfg"
 
 
 # This adds a small nodepool that is non preemtible. Used for services that can't go down (nfs...)
-gcloud beta container node-pools create "pool2-local-storage" \
-    --project="${GKE_PROJECT_ID}" \
+gcloud beta container node-pools create "${GKE_CLUSTER_NAME}-pool1" \
     --cluster="${GKE_CLUSTER_NAME}"  \
     --zone="${GKE_PRIMARY_ZONE}" \
-    --machine-type="custom-8-24576" \
-    --min-cpu-platform="Intel Skylake" \
+    --machine-type="n1-standard-1" \
     --num-nodes="1" \
-    --node-labels="disk=local-nvme" \
     --enable-autorepair \
     --enable-autoupgrade \
-    --scopes "https://www.googleapis.com/auth/cloud-platform" \
-    --disk-type=pd-ssd \
-    --local-ssd-count 1
+    --node-version="${GKE_CLUSTER_VERSION}" \
+    --scopes "https://www.googleapis.com/auth/cloud-platform" 
 
 
 #    --node-taints="preemptible=true:NoSchedule"
-#    --node-version="${GKE_CLUSTER_VERSION}" \
-
