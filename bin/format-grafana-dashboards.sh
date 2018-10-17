7b4c053ce73f358f057b674030d9731a979fa421
dgoldssfo <dg@dgolds.com>
2018-10-17 09:03:59 +0100

    Update bin/format-grafana-dashboards.sh

--- bin/format-grafana-dashboards.sh
+++ bin/format-grafana-dashboards.sh
@@ -20,7 +20,7 @@ OVERWRITE=true
 # Loop through dashboards in dashboards/.  The following alterations are needed to allow the dashboards to be accepted by Grafana:
 # 1 - Wrap json content in a dashboard: object
 # 2 - Add overwrite option after dashboard content to allow to overwrite current dashboards. Set to true
-# 3 - API import doesn't recognise the datasource variables. The values are replaced with Prometheus.
+# 3 - API import doesn't recognize the datasource variables. The values are replaced with `prometheus`.
 # 4 - Passed into jq command to remove __input section and output in json format.
 for i in $DASH_DIR/*.json; do
     filename=$(basename $i) 
