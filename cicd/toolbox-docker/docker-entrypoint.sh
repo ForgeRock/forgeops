9b2d7bd012532ec66780e3a4f89ec81c70ff5958
pavel.balcarek <pavel.balcarek@forgerock.com>
2018-10-18 13:38:20 +0200

    Moved tests into cluster

--- cicd/toolbox-docker/docker-entrypoint.sh
+++ cicd/toolbox-docker/docker-entrypoint.sh
@@ -7,32 +7,44 @@ echo "Cloning forgeops"
 mkdir -p /workspace
 
 cd /workspace
-git clone https://github.com/ForgeRock/forgeops.git 
+git clone https://github.com/ForgeRock/forgeops.git
 
 # configure context so kubens does not complain
 kubectl config set-context default
-kubectl config use-context default 
+kubectl config use-context default
 
 
 pause() {
-    echo "Sleeping awaiting your command" 
+    GIT_UPTODATE="Your branch is up to date with 'origin/master'"
+    cd /workspace/forgeops
+
+    echo "Sleeping awaiting your command"
     while true
     do
-        sleep 10000
-    done 
+        git fetch
+        MSG=$(git status)
+        echo "Git message:"
+        echo $MSG
+        if [[ "$MSG" = *$GIT_UPTODATE* ]]; then
+            echo "Branch is up to date, tests postponed"
+        else
+            echo "We have a change on master, running tests(Tests will ensure that master is updated)"
+            cd /
+            ./run-smoke-tests.sh
+        fi
+        sleep 600
+    done
 }
 # assume command is startall. TODO: add different entry points
 
 case $1 in
-smoke-tests)   
+smoke-tests)
     /run-smoke-tests.sh
     ;;
-    
-pause) 
+
+pause)
     pause ;;
 
-*) 
+*)
     exec $@ ;;
 esac
-
-
