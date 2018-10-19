f0620cb7c86fa0b1d3997a2567c03ed901135d24
Pavel Balcarek <pavel.balcarek@forgerock.com>
2018-10-19 11:45:00 +0200

    Smoke tests self-contained (CLOUD-759) (#359)
    * Moved tests into cluster
    * Removed smoke tests from cloudbuilder
    * Fixed git check when waiting for changes
    * Added timestamps to waiting messages to keep track of when tests are executed
    * Fixed archived reports being updated with git messages
    * Added 20 minutes wait time to make sure images are built

--- cicd/toolbox-docker/docker-entrypoint.sh
+++ cicd/toolbox-docker/docker-entrypoint.sh
@@ -7,32 +7,46 @@ echo "Cloning forgeops"
 mkdir -p /workspace
 
 cd /workspace
-git clone https://github.com/ForgeRock/forgeops.git 
+git clone https://github.com/ForgeRock/forgeops.git
 
 # configure context so kubens does not complain
 kubectl config set-context default
-kubectl config use-context default 
+kubectl config use-context default
 
 
 pause() {
-    echo "Sleeping awaiting your command" 
+    GIT_UPTODATE="Your branch is up to date with 'origin/master'"
+
     while true
     do
-        sleep 10000
-    done 
+        cd /workspace/forgeops
+        git fetch
+        MSG=$(git status)
+        echo "$(date) - Git message:"
+        echo $MSG
+        if [[ "$MSG" = *$GIT_UPTODATE* ]]; then
+            echo "$(date): Branch is up to date, tests postponed"
+        else
+            echo "$(date): We have a change on master, updating forgeops"
+            git fetch -a; git reset --hard origin/master
+            cd /
+            echo "$(date): Waiting 20 minutes to make sure new images are built properly"
+            sleep 1200
+            ./run-smoke-tests.sh
+        fi
+        sleep 600
+    done
 }
 # assume command is startall. TODO: add different entry points
 
 case $1 in
-smoke-tests)   
+smoke-tests)
     /run-smoke-tests.sh
     ;;
-    
-pause) 
+
+pause)
     pause ;;
 
-*) 
+*)
     exec $@ ;;
 esac
-
-
