b1c767c4d2231462c7d6cfbb666795406d404d89
Wajih Ahmed <wahmed@mbpfr.wfoo.net>
2018-11-05 10:40:05 -0500

    Enhancing the nodepool script so that different type of nodepools can be created and deleted

--- bin/gke-create-nodepool.sh
+++ bin/gke-create-nodepool.sh
@@ -4,11 +4,12 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+
 source "${BASH_SOURCE%/*}/../etc/gke-env.cfg"
 
-
-# This adds a small nodepool that is non preemtible. Used for services that can't go down (nfs...)
-gcloud beta container node-pools create "pool2-local-storage" \
+function local_pool()
+{
+gcloud beta container node-pools create "local-storage-pool" \
     --project="${GKE_PROJECT_ID}" \
     --cluster="${GKE_CLUSTER_NAME}"  \
     --zone="${GKE_PRIMARY_ZONE}" \
@@ -21,8 +22,51 @@ gcloud beta container node-pools create "pool2-local-storage" \
     --scopes "https://www.googleapis.com/auth/cloud-platform" \
     --disk-type=pd-ssd \
     --local-ssd-count 1
+}
 
+
+function cts_pool()
+{
+gcloud beta container node-pools create "cts-pool" \
+    --project="${GKE_PROJECT_ID}" \
+    --cluster="${GKE_CLUSTER_NAME}"  \
+    --zone="${GKE_PRIMARY_ZONE}" \
+    --machine-type="custom-32-65536" \
+    --min-cpu-platform="Intel Skylake" \
+    --num-nodes="1" \
+    --node-labels="usage=cts" \
+    --enable-autorepair \
+    --enable-autoupgrade \
+    --scopes "https://www.googleapis.com/auth/cloud-platform" \
+    --disk-type=pd-ssd \
+    --disk-size=80
+}
 
 #    --node-taints="preemptible=true:NoSchedule"
 #    --node-version="${GKE_CLUSTER_VERSION}" \
 
+
+############################### Main ######################################
+
+choice=${1:-}
+
+case "${choice}" in
+
+      local)
+        local_pool
+        ;;
+
+      cts)
+        cts_pool
+        ;; 
+
+      all)
+        local_pool
+        cts_pool
+        ;; 
+
+      *)
+        echo "Usage: $0 [pool-type] where pool-type=local or cts or all"
+        ;;
+
+esac
