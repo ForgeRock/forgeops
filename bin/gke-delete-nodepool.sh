bf50f6f7223a1e8ca576d027f9e9ac40cd01ff6a
Wajih Ahmed <wahmed@mbpfr.wfoo.net>
2018-11-05 10:40:05 -0500

    Enhancing the nodepool script so that different type of nodepools can be created and deleted

--- bin/gke-delete-nodepool.sh
+++ bin/gke-delete-nodepool.sh
@@ -7,11 +7,25 @@ set -o nounset
 source "${BASH_SOURCE%/*}/../etc/gke-env.cfg"
 
 
-# This adds a small nodepool that is non preemtible. Used for services that can't go down (nfs...)
-gcloud beta container node-pools delete "pool2-local-storage" \
-    --project="${GKE_PROJECT_ID}" \
-    --cluster="${GKE_CLUSTER_NAME}"  \
-    --zone="${GKE_PRIMARY_ZONE}"
+function delete_pool()
+{
+echo  gcloud beta container node-pools delete "${1}" \
+      --project="${GKE_PROJECT_ID}" \
+      --cluster="${GKE_CLUSTER_NAME}"  \
+      --zone="${GKE_PRIMARY_ZONE}"
+}
 
-#    --node-taints="preemptible=true:NoSchedule"
+############################### Main ######################################
+
+choice=${1:-}
+
+if [ $# -lt 1 ] || [ $# -gt 1 ]; then
+   echo "Usage: $0 [pool-name]"
+   exit 1
+fi
+
+delete_pool ${choice}
+
+exit 0
+
 
