c7f9a2a4a7ba08e91e27ab823ef9b010822068a7
Wajih Ahmed <wahmed@mbpfr.wfoo.net>
2018-12-16 10:19:01 -0500

    Changing the "fast" storage class from io1 to gp2. Testing has shown that gp2 is well performat and the price is significantly lower than io1.  Also adding a new storage class called "fast10" which is based on io1 and iopsPerGB set to 10.  This new storage class might be used in l-cluster and other extreme scenerios.

--- bin/eks-create-sc.sh
+++ bin/eks-create-sc.sh
@@ -14,7 +14,7 @@
 # For io1 the maximum ratio of provisioned IOPS to requested volume size (in GiB) is 50:1.
 # For example, a 100 GiB volume can be provisioned with up to 5,000 IOPS.
 # Check this page out for more details https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/EBSVolumeTypes.html
-# For the "fast" sc, add "iopsPerGB" parameter if you want to specify IOPS for io1.
+
 
 kubectl create -f - <<EOF
 kind: StorageClass
@@ -23,8 +23,7 @@ metadata:
   name: fast
 provisioner: kubernetes.io/aws-ebs
 parameters:
-  type: io1
-  fstype: ext4
+  type: gp2
 ---
 kind: StorageClass
 apiVersion: storage.k8s.io/v1
@@ -33,7 +32,19 @@ metadata:
 provisioner: kubernetes.io/aws-ebs
 parameters:
   type: gp2
+---
+kind: StorageClass
+apiVersion: storage.k8s.io/v1
+metadata:
+  name: fast10
+provisioner: kubernetes.io/aws-ebs
+parameters:
+  type: io1
+  fstype: ext4
+  iopsPerGB: "10"
 EOF
 
 
 kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
+
