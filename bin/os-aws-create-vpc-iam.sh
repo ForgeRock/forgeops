c3b4d6559ab4ff2ce3ebe16b4f36e42e84159bbe
bvarnavas <bvarnavas@hubcitymedia.com>
2019-01-09 14:13:29 -0500

    Brought the AWS QS artifacts local

--- bin/os-aws-create-vpc-iam.sh
+++ bin/os-aws-create-vpc-iam.sh
@@ -16,50 +16,49 @@ set -o nounset
 
 source ../etc/os-aws-env.cfg
 
-aws cloudformation deploy \
+aws cloudformation create-stack \
           --stack-name $OS_AWS_STACK_NAME \
-          --template-file ../etc/openshift-master.template \
-          --parameter-overrides AvailabilityZones=${OS_AWS_AVAILABILITY_ZONES} \
-                                VPCCIDR=${OS_AWS_VPC_CIDR} \
-                                PrivateSubnet1CIDR=${OS_AWS_PRIVATE_SUBNET_1_CIDR} \
-                                PrivateSubnet2CIDR=${OS_AWS_PRIVATE_SUBNET_2_CIDR} \
-                                PrivateSubnet3CIDR=${OS_AWS_PRIVATE_SUBNET_3_CIDR} \
-                                PublicSubnet1CIDR=${OS_AWS_PUBLIC_SUBNET_1_CIDR} \
-                                PublicSubnet2CIDR=${OS_AWS_PUBLIC_SUBNET_2_CIDR} \
-                                PublicSubnet3CIDR=${OS_AWS_PUBLIC_SUBNET_3_CIDR} \
-                                RemoteAccessCIDR=${OS_AWS_REMOTE_ACCESS_CIDR} \
-                                ContainerAccessCIDR=${OS_AWS_CONTAINER_ACCESS_CIDR} \
-                                DomainName=${OS_AWS_DOMAIN_NAME} \
-                                HostedZoneID=${OS_AWS_HOSTED_ZONE_ID} \
-                                SubDomainPrefix=${OS_AWS_SUB_DOMAIN_PREFIX} \
-                                KeyPairName=${OS_AWS_KEY_PAIR_NAME} \
-                                NumberOfMaster=${OS_AWS_NUMBER_OF_MASTER} \
-                                NumberOfEtcd=${OS_AWS_NUMBER_OF_ETCD} \
-                                NumberOfNodes=${OS_AWS_NUMBER_OF_NODES} \
-                                MasterInstanceType=${OS_AWS_MASTER_INSTANCE_TYPE} \
-                                EtcdInstanceType=${OS_AWS_ETCD_INSTANCE_TYPE} \
-                                NodesInstanceType=${OS_AWS_NODES_INSTANCE_TYPE} \
-                                OpenShiftAdminPassword=${OS_AWS_OPENSHIFT_ADMIN_PASSWORD} \
-                                OpenshiftContainerPlatformVersion=${OS_AWS_OPENSHIFT_CONTAINER_PLATFORM_VERSION} \
-                                AWSServiceBroker=${OS_AWS_SERVICE_BROKER} \
-                                HawkularMetrics=${OS_AWS_HAWKULAR_METRICS} \
-                                AnsibleFromGit=${OS_AWS_ANSIBLE_FROM_GIT} \
-                                ClusterName=${OS_AWS_CLUSTER_NAME} \
-                                ClusterConsole=${OS_AWS_CLUSTER_CONSOLE} \
-                                GlusterFS=${OS_AWS_GLUSTER_FS} \
-                                GlusterStorageSize=${OS_AWS_GLUSTER_STORAGE_SIZE} \
-                                GlusterStorageType=${OS_AWS_STORAGE_TYPE} \
-                                GlusterStorageIops=${OS_AWS_GLUSTER_STORAGE_IOPS} \
-                                GlusterStorageEncrypted=${OS_AWS_GLUSTER_STORAGE_ENCRYPTED} \
-                                GlusterInstanceType=${OS_AWS_GLUSTER_INSTANCE_TYPE} \
-                                NumberOfGluster=${OS_AWS_NUMBER_OF_GLUSTER} \
-                                AutomationBroker=${OS_AWS_AUTOMATION_BROKER} \
-                                RedhatSubscriptionUserName=${OS_AWS_REDHAT_SUBSCRIPTION_USER_NAME} \
-                                RedhatSubscriptionPassword=${OS_AWS_REDHAT_SUBSCRIPTION_PASSWORD} \
-                                RedhatSubscriptionPoolID=${OS_AWS_REDHAT_SUBSCRIPTION_POOL_ID} \
-                                QSS3BucketName=${OS_AWS_QS_S3_BUCKET_NAME} \
-                                QSS3KeyPrefix=${OS_AWS_QS_S3_KEY_PREFIX} \
-                                OutputBucketName=${OS_AWS_OUTPUT_BUCKET_NAME} \
-          --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
+          --template-url https://s3.amazonaws.com/${OS_AWS_QS_S3_BUCKET_NAME}/${OS_AWS_QS_S3_KEY_PREFIX}templates/openshift-master.template \
+          --parameters  ParameterKey=AvailabilityZones,ParameterValue=${OS_AWS_AVAILABILITY_ZONE_1}\\,${OS_AWS_AVAILABILITY_ZONE_2}\\,${OS_AWS_AVAILABILITY_ZONE_3} \
+                        ParameterKey=VPCCIDR,ParameterValue=${OS_AWS_VPC_CIDR} \
+                        ParameterKey=PrivateSubnet1CIDR,ParameterValue=${OS_AWS_PRIVATE_SUBNET_1_CIDR} \
+                        ParameterKey=PrivateSubnet2CIDR,ParameterValue=${OS_AWS_PRIVATE_SUBNET_2_CIDR} \
+                        ParameterKey=PrivateSubnet3CIDR,ParameterValue=${OS_AWS_PRIVATE_SUBNET_3_CIDR} \
+                        ParameterKey=PublicSubnet1CIDR,ParameterValue=${OS_AWS_PUBLIC_SUBNET_1_CIDR} \
+                        ParameterKey=PublicSubnet2CIDR,ParameterValue=${OS_AWS_PUBLIC_SUBNET_2_CIDR} \
+                        ParameterKey=PublicSubnet3CIDR,ParameterValue=${OS_AWS_PUBLIC_SUBNET_3_CIDR} \
+                        ParameterKey=RemoteAccessCIDR,ParameterValue=${OS_AWS_REMOTE_ACCESS_CIDR} \
+                        ParameterKey=ContainerAccessCIDR,ParameterValue=${OS_AWS_CONTAINER_ACCESS_CIDR} \
+                        ParameterKey=DomainName,ParameterValue=${OS_AWS_DOMAIN_NAME} \
+                        ParameterKey=HostedZoneID,ParameterValue=${OS_AWS_HOSTED_ZONE_ID} \
+                        ParameterKey=SubDomainPrefix,ParameterValue=${OS_AWS_SUB_DOMAIN_PREFIX} \
+                        ParameterKey=KeyPairName,ParameterValue=${OS_AWS_KEY_PAIR_NAME} \
+                        ParameterKey=NumberOfMaster,ParameterValue=${OS_AWS_NUMBER_OF_MASTER} \
+                        ParameterKey=NumberOfEtcd,ParameterValue=${OS_AWS_NUMBER_OF_ETCD} \
+                        ParameterKey=NumberOfNodes,ParameterValue=${OS_AWS_NUMBER_OF_NODES} \
+                        ParameterKey=MasterInstanceType,ParameterValue=${OS_AWS_MASTER_INSTANCE_TYPE} \
+                        ParameterKey=EtcdInstanceType,ParameterValue=${OS_AWS_ETCD_INSTANCE_TYPE} \
+                        ParameterKey=NodesInstanceType,ParameterValue=${OS_AWS_NODES_INSTANCE_TYPE} \
+                        ParameterKey=OpenShiftAdminPassword,ParameterValue=${OS_AWS_OPENSHIFT_ADMIN_PASSWORD} \
+                        ParameterKey=OpenshiftContainerPlatformVersion,ParameterValue=${OS_AWS_OPENSHIFT_CONTAINER_PLATFORM_VERSION} \
+                        ParameterKey=AWSServiceBroker,ParameterValue=${OS_AWS_SERVICE_BROKER} \
+                        ParameterKey=HawkularMetrics,ParameterValue=${OS_AWS_HAWKULAR_METRICS} \
+                        ParameterKey=AnsibleFromGit,ParameterValue=${OS_AWS_ANSIBLE_FROM_GIT} \
+                        ParameterKey=ClusterName,ParameterValue=${OS_AWS_CLUSTER_NAME} \
+                        ParameterKey=ClusterConsole,ParameterValue=${OS_AWS_CLUSTER_CONSOLE} \
+                        ParameterKey=GlusterFS,ParameterValue=${OS_AWS_GLUSTER_FS} \
+                        ParameterKey=GlusterStorageSize,ParameterValue=${OS_AWS_GLUSTER_STORAGE_SIZE} \
+                        ParameterKey=GlusterStorageType,ParameterValue=${OS_AWS_STORAGE_TYPE} \
+                        ParameterKey=GlusterStorageIops,ParameterValue=${OS_AWS_GLUSTER_STORAGE_IOPS} \
+                        ParameterKey=GlusterStorageEncrypted,ParameterValue=${OS_AWS_GLUSTER_STORAGE_ENCRYPTED} \
+                        ParameterKey=GlusterInstanceType,ParameterValue=${OS_AWS_GLUSTER_INSTANCE_TYPE} \
+                        ParameterKey=NumberOfGluster,ParameterValue=${OS_AWS_NUMBER_OF_GLUSTER} \
+                        ParameterKey=AutomationBroker,ParameterValue=${OS_AWS_AUTOMATION_BROKER} \
+                        ParameterKey=RedhatSubscriptionUserName,ParameterValue=${OS_AWS_REDHAT_SUBSCRIPTION_USER_NAME} \
+                        ParameterKey=RedhatSubscriptionPassword,ParameterValue=${OS_AWS_REDHAT_SUBSCRIPTION_PASSWORD} \
+                        ParameterKey=RedhatSubscriptionPoolID,ParameterValue=${OS_AWS_REDHAT_SUBSCRIPTION_POOL_ID} \
+                        ParameterKey=QSS3BucketName,ParameterValue=${OS_AWS_QS_S3_BUCKET_NAME} \
+                        ParameterKey=QSS3KeyPrefix,ParameterValue=${OS_AWS_QS_S3_KEY_PREFIX} \
+                        ParameterKey=OutputBucketName,ParameterValue=${OS_AWS_OUTPUT_BUCKET_NAME} \
           --role-arn ${OS_AWS_IAM_Role}
 
