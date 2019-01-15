735093c4da41750c0e428cb8f78e17194a979539
markg <mark.gibson@forgerock.com>
2019-01-15 11:40:36 -0800

    CLOUD-1037, initial test case for ds replication, updates based upon initial PR review comments

--- cicd/forgeops-tests/tests/smoke/ds/DSSmoke.py
+++ cicd/forgeops-tests/tests/smoke/ds/DSSmoke.py
@@ -9,7 +9,8 @@ Test are sorted automatically so that's why it's needed to keep test_0[1,2,3]_ n
 """
 # Lib imports
 import unittest
-from requests import get, put
+from requests import get, put, delete
+import logging
 
 # Framework imports
 from config.ProductConfig import DSConfig
@@ -26,13 +27,11 @@ class DSSmoke(unittest.TestCase):
 
     def test_0_ping(self):
         """Pings OpenDJ instances to see if servers are alive"""
-        import time
-        time.sleep(5)
-        resp = get(verify=self.dscfg.ssl_verify, url=self.dscfg.ds0_rest_ping_url)
-        self.assertEqual(resp.status_code, 200, 'userstore-0 is alive')
+        response = get(verify=self.dscfg.ssl_verify, url=self.dscfg.ds0_rest_ping_url)
+        self.assertEqual(response.status_code, 200, 'userstore-0 is alive')
 
-        resp = get(verify=self.dscfg.ssl_verify, url=self.dscfg.ds1_rest_ping_url)
-        self.assertEqual(resp.status_code, 200, 'userstore-1 is alive')
+        response = get(verify=self.dscfg.ssl_verify, url=self.dscfg.ds1_rest_ping_url)
+        self.assertEqual(response.status_code, 200, 'userstore-1 is alive')
 
     def test_resource_replication(self):
         """Creates a new resource via rest2ldap interface and
@@ -46,7 +45,6 @@ class DSSmoke(unittest.TestCase):
         json_data = {
             "_id": "newuser_ds0",
             "displayName": ["newuser_added_to_ds0"],
-            "userPassword": "password",
             "contactInformation": {
                 "telephoneNumber": "+1 408 555 1212",
                 "emailAddress": "newuser_ds0@example.com"
@@ -58,6 +56,32 @@ class DSSmoke(unittest.TestCase):
             "_schema": "frapi:opendj:rest2ldap:user:1.0"
         }
 
-        resp = put(verify=self.dscfg.ssl_verify, url=self.dscfg.ds0_url + '/api/users/newuser_ds0',
+        logging.info("Creating a new user entry with ID newuser_ds0")
+        response = put(verify=self.dscfg.ssl_verify, url=self.dscfg.ds0_url + '/api/users/newuser_ds0',
                    auth=('am-identity-bind-account', 'password'), headers=headers,json=json_data)
-        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(response.status_code, 201)
+
+        logging.info("Verifying user entry with ID newuser_ds0 replicated in userstore-1")
+        response = get(verify=self.dscfg.ssl_verify, url=self.dscfg.ds1_url + '/api/users/newuser_ds0',
+                       auth=('am-identity-bind-account', 'password'))
+        self.assertEqual(response.status_code, 200)
+
+    @classmethod
+    def tearDownClass(self):
+        instance = DSSmoke()
+        """Delete datastore resource from users/"""
+
+        logging.info("Deleting user entry with ID newuser_ds0 from userstore-0")
+        response = delete(verify=self.dscfg.ssl_verify, url=self.dscfg.ds0_url + '/api/users/newuser_ds0',
+                          auth=('am-identity-bind-account', 'password'))
+        self.assertEqual(instance, response.status_code, 200)
+
+        logging.info("Verifying user entry with ID newuser_ds0 has been deleted from userstore-0")
+        response = get(verify=self.dscfg.ssl_verify, url=self.dscfg.ds0_url + '/api/users/newuser_ds0',
+                       auth=('am-identity-bind-account', 'password'))
+        self.assertEqual(instance, response.status_code, 404)
+
+        logging.info("Verifying user entry with ID newuser_ds0 has been deleted from userstore-1")
+        response = get(verify=self.dscfg.ssl_verify, url=self.dscfg.ds1_url + '/api/users/newuser_ds0',
+                       auth=('am-identity-bind-account', 'password'))
+        self.assertEqual(instance, response.status_code, 404)
