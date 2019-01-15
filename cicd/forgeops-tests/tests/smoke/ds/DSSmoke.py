2a24b62d324a2d885e1a0bc8d163de7651bd599c
markg <mark.gibson@forgerock.com>
2019-01-14 16:01:47 -0800

    ds replication test

--- cicd/forgeops-tests/tests/smoke/ds/DSSmoke.py
+++ cicd/forgeops-tests/tests/smoke/ds/DSSmoke.py
@@ -9,7 +9,7 @@ Test are sorted automatically so that's why it's needed to keep test_0[1,2,3]_ n
 """
 # Lib imports
 import unittest
-from requests import get
+from requests import get, put
 
 # Framework imports
 from config.ProductConfig import DSConfig
@@ -26,8 +26,38 @@ class DSSmoke(unittest.TestCase):
 
     def test_0_ping(self):
         """Pings OpenDJ instances to see if servers are alive"""
+        import time
+        time.sleep(5)
         resp = get(verify=self.dscfg.ssl_verify, url=self.dscfg.ds0_rest_ping_url)
         self.assertEqual(resp.status_code, 200, 'userstore-0 is alive')
 
         resp = get(verify=self.dscfg.ssl_verify, url=self.dscfg.ds1_rest_ping_url)
         self.assertEqual(resp.status_code, 200, 'userstore-1 is alive')
+
+    def test_resource_replication(self):
+        """Creates a new resource via rest2ldap interface and
+           verifies it is replicated to the second instance.
+           Ref:  https://ea.forgerock.com/docs/ds/rest-guide/create-rest.html#create-rest"""
+
+        headers = {'Content-Type': 'application/json',
+                   'If-None-Match': '*'
+                   }
+
+        json_data = {
+            "_id": "newuser_ds0",
+            "displayName": ["newuser_added_to_ds0"],
+            "userPassword": "password",
+            "contactInformation": {
+                "telephoneNumber": "+1 408 555 1212",
+                "emailAddress": "newuser_ds0@example.com"
+            },
+            "name": {
+                "familyName": "New",
+                "givenName": "UserDSZero"
+            },
+            "_schema": "frapi:opendj:rest2ldap:user:1.0"
+        }
+
+        resp = put(verify=self.dscfg.ssl_verify, url=self.dscfg.ds0_url + '/api/users/newuser_ds0',
+                   auth=('am-identity-bind-account', 'password'), headers=headers,json=json_data)
+        self.assertEqual(resp.status_code, 200)
