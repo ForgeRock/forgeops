fa581c7087ff0926a018ba0cea53a4162ce66249
lbaines <lee.baines@forgerock.com>
2019-01-29 10:03:18 +0000

    removed version

--- bin/deploy-cert-manager.sh
+++ bin/deploy-cert-manager.sh
@@ -33,7 +33,7 @@ do
 done
 
 # Deploy Cert Manager Helm chart
-helm upgrade -i cert-manager --namespace kube-system stable/cert-manager --version v0.5.0
+helm upgrade -i cert-manager --namespace kube-system stable/cert-manager
 
 # Check that cert-manager is up before deploying the cluster-issuer
 while true;
