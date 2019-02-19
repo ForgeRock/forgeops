c8334e8729445378354aa58e1d6dd4e24e587430
lbaines <lee.baines@forgerock.com>
2019-02-19 14:03:21 +0000

    added EKS service role var to template

--- bin/eks-create-service-role.sh
+++ bin/eks-create-service-role.sh
@@ -10,13 +10,15 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-ROLE_NAME="eksServiceRole"
+source "${BASH_SOURCE%/*}/../etc/eks-env.cfg"
 
-EKS_SERVICE_ROLE_ARN=$(aws iam create-role --role-name ${ROLE_NAME} --description "Allows EKS to manage clusters on your behalf." --assume-role-policy-document file://../etc/eks-service-role.json --query 'Role.Arn')
+#ROLE_NAME="eksServiceRole"
+
+EKS_SERVICE_ROLE_ARN=$(aws iam create-role --role-name ${EKS_SERVICE_ROLE} --description "Allows EKS to manage clusters on your behalf." --assume-role-policy-document file://../etc/eks-service-role.json --query 'Role.Arn')
 
 echo "EKS Role created with ARN ${EKS_SERVICE_ROLE_ARN}"
 
-aws iam attach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy
-aws iam attach-role-policy --role-name ${ROLE_NAME} --policy-arn arn:aws:iam::aws:policy/AmazonEKSServicePolicy
+aws iam attach-role-policy --role-name ${EKS_SERVICE_ROLE} --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy
+aws iam attach-role-policy --role-name ${EKS_SERVICE_ROLE} --policy-arn arn:aws:iam::aws:policy/AmazonEKSServicePolicy
 
 echo "EKS/EC2 policies attached to the IAM role."
