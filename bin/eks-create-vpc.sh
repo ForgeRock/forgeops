91b201d561aa6c905a90871c663616de97de2c26
lbaines <lee.baines@forgerock.com>
2019-03-01 10:32:50 +0000

    updated name of control plane security group

--- bin/eks-create-vpc.sh
+++ bin/eks-create-vpc.sh
@@ -22,6 +22,6 @@ aws cloudformation deploy \
 aws cloudformation describe-stacks --stack-name $EKS_VPC_STACK_NAME --output table --query 'Stacks[0].Outputs'
 
 echo "Please record the above values for creating your EKS cluster."
-echo "SecurityGroups: This security group allows the EKS cluster to communicate with your worker nodes. Value should be set to EC2_SECURITY_GROUP in your eks-env.cfg file."
+echo "SecurityGroups: This security group allows the EKS cluster to communicate with your worker nodes. Value should be set to EKS_CONTROL_PLANE_SECURITY_GROUP in your eks-env.cfg file."
 echo "VpcId: This is used for creating the subnets in your EC2 account. Value should be set to EKS_VPC_ID in your eks-env.cfg file."
 echo "SubnetIds: This is used to create subnets in your EC2 account where the worker nodes will be launched into. Value should be set to EKS_SUBNETS in your eks-env.cfg file."
