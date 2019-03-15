5ef29234a58b0acfb0fc988a67731a894c8e0654
lbaines <lee.baines@forgerock.com>
2019-03-15 21:47:33 +0000

    remove unused keystore.jks

--- helm/amster/makekey.sh
+++ helm/amster/makekey.sh
@@ -9,9 +9,6 @@ ssh-keygen -t rsa -b 4096 -m PEM -C "openam-install@example.com" -f secrets/id_r
 # This is just copied as amster also requires a copy of the authorized_keys
 cp secrets/id_rsa.pub ../openam/secrets/authorized_keys
 
-# The runtime needs the same secret if you want to use Amster.
-# cp secrets/*  ../openam/secrets
-
 # If you want to tighten up the authorized_keys to an IP range-, use a from option instead:
 #key=`cat secrets/id_rsa.pub`
 #echo "\"from=\"127.0.0.0/24,::1\" $key"   >secrets/authorized_keys
