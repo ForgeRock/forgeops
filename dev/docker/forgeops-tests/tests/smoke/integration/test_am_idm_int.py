794dbeb32371fe535765f69454e03e7d9bf5ab2d
pavel.balcarek <pavel.balcarek@forgerock.com>
2019-04-18 16:18:21 +0200

    Removed invalid test from AM/IDM integration tests

--- dev/docker/forgeops-tests/tests/smoke/integration/test_am_idm_int.py
+++ dev/docker/forgeops-tests/tests/smoke/integration/test_am_idm_int.py
@@ -7,6 +7,7 @@ from ProductConfig import AMConfig, IDMConfig
 from utils import logger, rest
 from requests import get
 
+
 class TestIntegration(object):
     amcfg = AMConfig()
     idmcfg = IDMConfig()
@@ -35,39 +36,4 @@ class TestIntegration(object):
         self.amcfg.login_user(username, password)
         logger.test_step('Deleting user with AM')
         self.amcfg.delete_user(username)
-
-    def test_2_am_create_user_idm_query_user(self):
-        username = 'am_user'
-        password = 'GreatP4ssword'
-
-        logger.test_step('Creating user in AM')
-        self.amcfg.create_user(username, password)
-        logger.test_step('Querying user in IDM')
-        userid = self.idmcfg.get_userid_by_name(username)
-        request = get(url=f'{self.idmcfg.rest_managed_user_url}/{userid}',
-                      headers=self.idmcfg.get_admin_headers(),
-                      verify=self.idmcfg.ssl_verify)
-        rest.check_http_status(request, 200)
-        assert f'{username}@forgerock.com' in request.json()['mail']
-
-    @classmethod
-    def teardown_class(cls):
-        """
-        Deletes all created users
-        """
-        headers1 = cls.idmcfg.get_admin_headers({
-            'Content-Type': 'application/json'
-        })
-
-        logger.test_step('Get user id')
-        response = get(verify=cls.idmcfg.ssl_verify, url=cls.idmcfg.rest_managed_user_url + '?_queryFilter=true',
-                       headers=headers1).json()
-        # rest.check_http_status(http_result=response, expected_status=200)
-        if response['resultCount'] == 0:
-            return
-
-        for result in response['result']:
-            _id = result['_id']
-            print(f'ID>>>>>>>>>>>>>>>>>>>>>{_id}')
-            logger.test_step('Delete user')
-            cls.idmcfg.delete_user(_id)
+ 
