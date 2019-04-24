b729bed4e6c7427725b175909d3df55e8b2fbb17
Warren Strange <warren.strange@gmail.com>
2019-04-24 11:09:36 -0600

    refactored kustomize to simplify deployment. FQDN can now be set at the top level env/ and is replaced in the ingress, etc.
    Remove legacy cloudbuild files
    
    update nginx to latest
    
    Update docker base images

--- bin/gke-create-ingress-cntlr.sh
+++ bin/gke-create-ingress-cntlr.sh
@@ -29,7 +29,7 @@ helm install --namespace nginx --name nginx \
   --set controller.stats.enabled=true \
   --set controller.service.externalTrafficPolicy=Local \
   --set controller.service.type=LoadBalancer \
-  --set controller.image.tag="0.21.0" \
+  --set controller.image.tag="0.24.1" \
    $IP_OPTS stable/nginx-ingress
 
 #--set controller.image.tag="0.17.1"
