c884fbc929329fce7120195a8620d38a01ce1118
Warren Strange <warren.strange@gmail.com>
2019-04-27 15:49:07 -0600

    add am logback.xml.  Update config-util to exit 0 on failure so it does not restart.
    add namespace to smoke test env. Add .gcloudignore
    
    Update forgeops-test ingress
    
    update /tests/ url on test ingress

--- dev/docker/am-fbc/dump-config.sh
+++ dev/docker/am-fbc/dump-config.sh
@@ -6,3 +6,7 @@ rm -fr tmp
 pod=`kubectl get pod -l app=am -o jsonpath='{.items[0].metadata.name}'`
 
 kubectl cp $pod:/home/forgerock ./tmp
+
+# clean up non essential files
+rm -fr ./tmp/openam/{log,debug,install.log} ./tmp/oepnam/am/{log,debug,install.log}
+
--- dev/docker/config-util/full-config.sh
+++ dev/docker/config-util/full-config.sh
@@ -5,3 +5,7 @@ echo "Running AM initial config script"
 python3 install_am.py
 echo "Running AM runtime configuration"
 python3 am_runtime_config.py
+
+# This last echo sets the exit status to 0, which prevents k8s from trying to restart the job 
+# on failure
+echo "Done"
