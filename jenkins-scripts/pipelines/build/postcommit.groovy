9df4da5c68fb976bd6ef5f60d89d131c6bba99d0
Robin Smith <robin.smith@forgerock.com>
2019-05-01 15:00:10 +0100

    RELENG-365 Create pipelines for ForgeOps Docker builds
    The ForgeOps repo contains several Dockerfiles, which only need to be
    built if they (or files related to them) have changed since the previous
    build.
    
    For postcommit builds, Docker images are build locally and pushed to
    GCR, tagged with '7.0.0-{gitCommitSha}' and 'latest'
    
    For PR builds, the Docker images are only build locally. Due to
    difficulties comparing the PR branch with the target branch, ALL images
    are built on the first run of a PR.
    
    Images/directories can be ignored by commenting them out of the
    `buildDirectories` map in the Jenkinsfile.

---
+++ jenkins-scripts/pipelines/build/postcommit.groovy
@@ -0,0 +1,70 @@
+#!/usr/bin/env groovy
+
+//===============================================
+// Postcommit pipeline for ForgeOps Docker images
+//===============================================
+
+import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
+
+import com.forgerock.pipeline.Build
+
+def build() {
+
+    properties([buildDiscarder(logRotator(numToKeepStr: '20'))])
+
+    def postcommitBuild = new Build(steps, env, currentBuild)
+    def slackChannel = '#cloud-deploy-notify'
+    def currentImage
+
+    try {
+        node('build&&linux') {
+            stage ('Clone repo') {
+                checkout scm
+                SHORT_GIT_COMMIT = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+                currentBuild.displayName = "#${BUILD_NUMBER} - ${SHORT_GIT_COMMIT}"
+                currentBuild.description = 'built:'
+            }
+
+            for (buildDirectory in buildDirectories) {
+                if (imageRequiresBuild(buildDirectory['name'], buildDirectory['forceBuild'])) {
+                    stage ("Build ${buildDirectory['name']} image") {
+                        echo "Building 'docker/${buildDirectory['name']}' ..."
+                        currentImage = buildDirectory['name']
+                        buildImage(buildDirectory['name'])
+                        currentBuild.description += " ${buildDirectory['name']}"
+                    }
+                } else {
+                    echo "Skipping build for 'docker/${buildDirectory['name']}'"
+                }
+            }
+        }
+
+    } catch (FlowInterruptedException ex) {
+        currentBuild.result = 'ABORTED'
+        throw ex
+    } catch (exception) {
+        currentBuild.result = 'FAILURE'
+        postcommitBuild.sendSlackNotification(
+            slackChannel,
+            true, // prepend @here
+            "${JOB_NAME} #${BUILD_NUMBER} FAILED while building the `${currentImage}` image"
+        )
+        throw exception
+    }
+}
+
+boolean imageRequiresBuild(String directoryName, boolean forceBuild) {
+    return forceBuild || BUILD_NUMBER == '1' || scmUtils.changesInFolder("docker/${directoryName}")
+}
+
+void buildImage(String directoryName) {
+    String imageBaseName = "gcr.io/forgerock-io/${directoryName}"
+    String gitShaLabel = "${BASE_VERSION}-${SHORT_GIT_COMMIT}" // e.g. 7.0.0-a7267fbc
+
+    sh "docker build --no-cache --pull --tag ${imageBaseName}:${gitShaLabel} docker/${directoryName}"
+    sh "docker push ${imageBaseName}:${gitShaLabel}"
+    sh "docker tag ${imageBaseName}:${gitShaLabel} ${imageBaseName}:latest"
+    sh "docker push ${imageBaseName}:latest"
+}
+
+return this
---
+++ jenkins-scripts/pipelines/build/pr.groovy
@@ -0,0 +1,67 @@
+#!/usr/bin/env groovy
+
+//=================================================
+// Pull request pipeline for ForgeOps Docker images
+//=================================================
+
+import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
+
+import com.forgerock.pipeline.PullRequestBuild
+
+def build() {
+
+    properties([buildDiscarder(logRotator(daysToKeepStr: '14', numToKeepStr: '10'))])
+
+    def prBuild = new PullRequestBuild(steps, env, currentBuild, scm)
+    def bitbucketCommentId = prBuild.commentOnPullRequest(buildStatus: 'IN PROGRESS')
+
+    try {
+        node('build&&linux') {
+            stage ('Clone repo') {
+                checkout scm
+                SHORT_GIT_COMMIT = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+                currentBuild.displayName = "#${BUILD_NUMBER} - ${SHORT_GIT_COMMIT}"
+                currentBuild.description = 'built:'
+            }
+
+            for (buildDirectory in buildDirectories) {
+                if (imageRequiresBuild(buildDirectory['name'], buildDirectory['forceBuild'])) {
+                    stage ("Build ${buildDirectory['name']} image") {
+                        echo "Building 'docker/${buildDirectory['name']}' ..."
+                        buildImage(buildDirectory['name'])
+                        currentBuild.description += " ${buildDirectory['name']}"
+                    }
+                } else {
+                    echo "Skipping build for 'docker/${buildDirectory['name']}'"
+                }
+            }
+        }
+
+        currentBuild.result = 'SUCCESS'
+        prBuild.commentOnPullRequest(originalCommentId: bitbucketCommentId)
+
+    } catch (FlowInterruptedException ex) {
+        currentBuild.result = 'ABORTED'
+        prBuild.commentOnPullRequest(buildStatus: 'ABORTED', originalCommentId: bitbucketCommentId)
+        throw ex
+    } catch (exception) {
+        currentBuild.result = 'FAILURE'
+        prBuild.commentOnPullRequest(originalCommentId: bitbucketCommentId)
+        throw exception
+    }
+}
+
+// Since it's not straightforward to detect changes between the PR branch and master, on the first PR build
+// we build everything. This can be disabled by temporarily commenting various lines out of buildDirectories.
+boolean imageRequiresBuild(String directoryName, boolean forceBuild) {
+    return forceBuild || BUILD_NUMBER == '1' || scmUtils.changesInFolder("docker/${directoryName}")
+}
+
+void buildImage(String directoryName) {
+    String imageBaseName = "gcr.io/forgerock-io/${directoryName}"
+    String gitShaLabel = "${BASE_VERSION}-${SHORT_GIT_COMMIT}" // e.g. 7.0.0-a7267fbc
+
+    sh "docker build --no-cache --pull --tag ${imageBaseName}:${gitShaLabel} docker/${directoryName}"
+}
+
+return this
