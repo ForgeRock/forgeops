973d8fbcfe1ae0dcd2e03c55c54b1084f16adfa2
Warren Strange <warren.strange@gmail.com>
2019-05-07 13:27:24 -0600

    Enhance/Simplify config-util.
    add prometheus, secrets
    
    move secrets to global config

--- dev/docker/config-util/am_runtime_config.py
+++ dev/docker/config-util/am_runtime_config.py
@@ -12,9 +12,8 @@ import requests
 
 requests.packages.urllib3.disable_warnings()
 
-
 class AMConfig(object):
-    def __init__(self, url):
+    def __init__(self, url, fqdn, folder):
         self.domain = os.getenv('DOMAIN', 'forgeops.com')
         self.admin_password = os.getenv('ADMIN_PASSWORD', 'password')
 
@@ -23,6 +22,14 @@ class AMConfig(object):
         self.am_url = url
         self.wait_for_am()
         self.admin_token = self.admin_login()
+        self.config_dir = folder
+        self.fqdn = fqdn
+        self.entityMapper = {
+            "rest" : "global-config/services",
+            "prometheus" : "global-config/services/monitoring/prometheus",
+            "KeyStoreSecretStore" :  "global-config/secrets/stores/KeyStoreSecretStore",
+            "FileSystemSecretStore" : "global-config/secrets/stores/FileSystemSecretStore",
+        }
 
     # UTILITY METHODS
     def wait_for_am(self):
@@ -58,21 +65,40 @@ class AMConfig(object):
                 'Accept-API-Version': 'resource=1.0',
                 'Content-Type': 'application/json'}
 
-    # Slurp a json file in amster format. Does search/replace on the string &{fqdn}
+    # Slurp a json file in amster format/ - return just the data payload
     def read_json_data(self, path, fqdn):
         return self.read_json_full(path, fqdn)['data']
 
+    #  search/replace on the string &{fqdn}
     def read_json_full(self, path, fqdn):
         with open(path) as jsonfile:
             text = jsonfile.read()
             text = text.replace(r'&{fqdn}', fqdn)
             json_data = json.loads(text)
             return json_data
+    
+    def put(self, url, config):
+        print(f'Put url={url}')
+        create_request = put(url=url, headers=self.admin_headers, verify=False, json=config)
+        print(create_request.status_code)
+
+    # Calculates the json path from data payload. 
+    def type_to_url(self,data):
+        # payload contains a _type struct
+        type = data['_type']
+        # The _id is the object type
+        _t = type['_id']
+        # get the json path
+        t = self.entityMapper[_t]
+        # The object instance is the _id of the data field
+        id = data['_id']
+        return f'{self.am_url}/json/{t}/{id}'
 
     # Import all config files for the root realm
-    def import_realm_config(self, dir, fqdn):
+    def import_realm_config(self):
+        dir = f'{self.config_dir}/realm'
         for filename in os.listdir(dir):
-            data = self.read_json_data(f'{dir}/{filename}', fqdn)
+            data = self.read_json_data(f'{dir}/{filename}', self.fqdn)
             _type = data['_type']
             id = _type['_id']
             name = _type['name']
@@ -84,79 +110,35 @@ class AMConfig(object):
             # else:
             #     print(f'I dont know how to import type {id}')
 
-    # ############## AMSTER-LIKE IMPORTS ##########################
-
-    def import_oauth2_configs(self, dir, fqdn):
+    def import_oauth2_configs(self):
+        dir = f'{self.config_dir}/oauth2'
         for filename in os.listdir(dir):
-            data = self.read_json_data(f'{dir}/{filename}', fqdn)
+            data = self.read_json_data(f'{dir}/{filename}', self.fqdn)
             id = data['_id']
             self.put(f'{self.am_url}/json/realms/root/realm-config/agents/OAuth2Client/{id}', data)
 
-    def put(self, url, config):
-        print(f'Put url={url}')
-        create_request = put(url=url, headers=self.admin_headers, verify=False, json=config)
-        print(create_request.status_code)
-
-    def import_global_configs(self, dir, fqdn):
+    def import_global_configs(self):
+        dir = f'{self.config_dir}/global'
         for filename in os.listdir(dir):
-            data = self.read_json_data(f'{dir}/{filename}', fqdn)
-            _type = data['_type']
-            id = _type['_id']
-            self.put(f'{self.am_url}/json/global-config/services/{id}', data)
+            data = self.read_json_data(f'{dir}/{filename}', self.fqdn)
+            url = self.type_to_url(data)
+            self.put(url, data)
 
-    def import_secrets_configs(self, dir, fqdn):
+    def import_policies(self):
+        dir =  f'{self.config_dir}/policies'
         for filename in os.listdir(dir):
-            data = self.read_json_data(f'{dir}/{filename}', fqdn)
-            _id = data['_id']
-            _type = data['_type']['_id']
-            self.put(f'{self.am_url}/json/global-config/secrets/stores/{_type}/{_id}', data)
-
-    def import_policies(self, dir, fqdn):
-        for filename in os.listdir(dir):
-            data = self.read_json_full(f'{dir}/{filename}', fqdn)
+            data = self.read_json_full(f'{dir}/{filename}', self.fqdn)
             _id = data['data']['_id']
             _type = data['metadata']['entityType'].lower()
             self.put(f'{self.am_url}/json/{_type}/{_id}', data['data'])
-
-    # ############## REST CONFIG METHODS ##########################
-    def create_policy_all_authenticated(self, name, resource, allow):
-        """
-        Simple example method to create policy to allow/deny all authenticated users to access resource
-        :param name: Name of the policy
-        :param resource: Resource url
-        :param allow: True/false resource access
-        """
-        policy = {
-            '_id': name,
-            'name': name,
-            'active': True,
-            'description': '',
-            'resources': [
-                resource
-            ],
-            'applicationName': 'iPlanetAMWebAgentService',
-            'actionValues': {
-                'HEAD': allow,
-                'DELETE': allow,
-                'POST': allow,
-                'GET': allow,
-                'OPTIONS': allow,
-                'PATCH': allow,
-                'PUT': allow
-            },
-            'subject': {
-                'type': 'AuthenticatedUsers'
-            }
-        }
-        url = f'{self.am_url}/json/policies/{name}'
-        self.put(url=url, config=policy)
-
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Configure AM')
     # This option is for debugging  / testing from your laptop. If you
-    # run in the cluster you can omit this.
+    # run in the cluster you can omit this. This lets you 
+    # run against the ingress (external) URL, instead of the internal (http://openam)
+    # To use this export FQDN=default.iam.example.com; python3 ./am_runtime_config.py
     parser.add_argument('--useFQDN', action='store_true',
                         help='Use the external FQDN to configure AM, not the internal service')
 
@@ -169,10 +151,9 @@ if __name__ == '__main__':
         am_url = 'http://openam:80/am'
 
     print(f'Doing minimal AM config using {am_cfg_folder} with url {am_url} external fqdn {am_fqdn}')
-    cfg = AMConfig(am_url)
-    cfg.import_global_configs(f'./{am_cfg_folder}/global', am_fqdn)
-    cfg.import_realm_config(f'./{am_cfg_folder}/realm', am_fqdn)
-    cfg.import_oauth2_configs(f'./{am_cfg_folder}/oauth2', am_fqdn)
-    cfg.import_secrets_configs(f'./{am_cfg_folder}/secrets', am_fqdn)
-    cfg.import_policies(f'./{am_cfg_folder}/policies', am_fqdn)
+    cfg = AMConfig(am_url, am_fqdn, am_cfg_folder)
+    cfg.import_global_configs()
+    cfg.import_realm_config()
+    cfg.import_oauth2_configs()
+    cfg.import_policies()
     print('Runtime config finished!')
