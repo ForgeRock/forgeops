ebab3297306ccd581719daa9a2904284f763ca96
Warren Strange <warren.strange@gmail.com>
2019-05-24 11:15:00 -0600

    Updates required for cert-manager 0.8.0
    Cert manager now supports an ingress shim. It is not longer required to create a Certificate CRD.
    If the ingress is annotated with the name of an issuer, cert-manager will auto
    create the certificate request.
    
    This commit removes the cert-manager configs from kustomize. The deploy-certmanager.sh
    script has been enhanced to create both the ca-issuer and the Let Encrypt issuer.

--- bin/deploy-cert-manager.sh
+++ bin/deploy-cert-manager.sh
@@ -5,50 +5,39 @@
 #
 # NOTE: You need to be on kubectl version >= 1.13
 
+cd "$(dirname "$0")"
+
 # Create namespace to run cert-manager in
 kubectl create namespace cert-manager
-
-PROVIDER=$(kubectl get nodes -o jsonpath={.items[0].spec.providerID} | awk -F: '{print $1}')
-if [[ "${PROVIDER}" == "gce" ]]; then
-  # Decrypt encoded service account
-  ./decrypt.sh ../etc/cert-manager/cert-manager.json
-fi
-
-# Create secret so the Cluster Issuer can gain access to CloudDNS
-kubectl create secret generic clouddns --from-file=../etc/cert-manager/cert-manager.json -n cert-manager
 
 # Disable resource validation on the cert-manager namespace
 kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
 
 # Install the CustomResourceDefinitions and cert-manager itself
-kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.7/deploy/manifests/cert-manager.yaml
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.8.0/cert-manager.yaml
 
-# Check that cert-manager is up before deploying the cluster-issuer
-while true;
-do
-  STATUS=$(kubectl get pod -n cert-manager | grep cert-manager-webhook | awk '{ print $3 }')
-  # kubectl get pods returns an empty string if the cluster is not available
-  if [ -z ${STATUS} ]
-  then
-    echo "The cluster is temporarily unavailable..."
-  else
-    if [ ${STATUS} == "Running" ]
-    then
-      echo "The cert-manager pod is available..."
-      break
-    else
-      echo "The cert-manager pod is not available..."
-    fi
-  fi
-  sleep 5
-done
 
 # Allow time for operator to be deployed so CRDs are recognized
 sleep 5
 
-# Deploy Cluster Issuer
-kubectl create -f ../etc/cert-manager/cluster-issuer.yaml -n cert-manager
+# Deploy base cert manager Issuer
+kubectl apply -k ../etc/cert-manager
 
-# Delete decrypted service account
-rm -f ../etc/cert-manager/cert-manager.json || true
+# This next section deploy the Let's Encrypt (LE) Issuer. This requires
+# setup of the LE issuer, including the dns secrets for the dns01 challenge.
+# We do not run this unless we are on GKE.
+PROVIDER=$(kubectl get nodes -o jsonpath={.items[0].spec.providerID} | awk -F: '{print $1}')
+if [[ "${PROVIDER}" == "gce" ]]; then
+  echo "Deploying Let's Encrypt Issuer"
+  # Decrypt encoded service account that has rights to control our dns for the dns01 challenge.
+  ./decrypt.sh ../etc/cert-manager/cert-manager.json
+
+  # Create secret so the Cluster Issuer can gain access to CloudDNS
+  kubectl create secret generic clouddns --from-file=../etc/cert-manager/cert-manager.json -n cert-manager
+
+  kubectl apply -n cert-manager -f ../etc/cert-manager/le-issuer.yaml 
+
+  # Delete decrypted service account
+  rm -f ../etc/cert-manager/cert-manager.json || true
+fi
 
