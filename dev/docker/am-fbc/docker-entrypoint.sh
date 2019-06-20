b80b76d6a725a1bd063043509a82ae3309748afb
Warren Strange <warren.strange@gmail.com>
2019-06-20 11:38:13 -0600

    promote a am-fbc skaffold to skaffold.yaml. Make gcflags optional by commenting it out (too verbose)

--- dev/docker/am-fbc/docker-entrypoint.sh
+++ dev/docker/am-fbc/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Add file based config option
+# -Dorg.forgerock.donotupgrade - optional - may cause issues
 export CATALINA_OPTS="${CATALINA_OPTS}  -Dcom.sun.identity.sm.sms_object_filebased_enabled=true"
 echo "CATALINAOPTS= $CATALINA_OPTS"
 
