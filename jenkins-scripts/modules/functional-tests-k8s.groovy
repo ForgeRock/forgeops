457c178700ffff742458da96503e563a79b93a8a
markg <mark.gibson@forgerock.com>
2019-06-25 11:34:24 -0700

    PIT-124, run smoke tests as part of forgeops pr and postcommit

---
+++ jenkins-scripts/modules/functional-tests-k8s.groovy
@@ -0,0 +1,20 @@
+#!/usr/bin/env groovy
+
+void runStage(String scope) {
+
+    node("google-cloud") {
+        stage("Run K8s FTs") {
+            def cfg = [
+                    TESTS_SCOPE         : "tests/smoke",
+                    SAMPLE_NAME         : "smoke-deployment",
+                    CLUSTER_IMAGE_TAG   : "7.0.0-latest-postcommit",
+                    CLUSTER_IMAGE_LEVEL : "pit1",
+                    SLACK_CHANNEL       : "#cloud-deploy-notify",
+            ]
+
+            withGKEPitNoStages(cfg)
+        }
+    }
+}
+
+return this
