0fe2d8cc5a2c9c24e36bd4bbe00c35da78f81d2b
lbaines <lee.baines@forgerock.com>
2019-07-17 10:11:41 +0100

    add packages folder and created npm package for nginx controller

--- pulumi/gke/nginx-controller.ts
+++ pulumi/gke/nginx-controller.ts
@@ -12,11 +12,11 @@ export const nsnginx = new k8s.core.v1.Namespace("nginx", {
 }, { dependsOn: [ primaryPool ], provider: clusterProvider });
 
 // Adds namespace to correct Helm field
-function addNamespace(o: any) {
-    if (o !== undefined) {
-        o.metadata.namespace = "nginx";
-    }
-}
+// function addNamespace(o: any) {
+//     if (o !== undefined) {
+//         o.metadata.namespace = "nginx";
+//     }
+// }
 
 // Check to see if static IP address has been provided. If not, create 1
 function assignIp() {
@@ -33,23 +33,23 @@ function assignIp() {
 export const lbIp = assignIp();
 
 // Deploy nginx-controller Helm chart
-export const nginx = new k8s.helm.v2.Chart("nginx-ingress", {
-    repo: "stable",
-    version: "0.24.1",
-    chart: "nginx-ingress",
-    transformations: [addNamespace],
-    namespace: "nginx",
-    values: {
-        rbac: {create: true},
-        controller: {
-            publishService: {enabled: true},
-            stats: {enabled: true},
-            service: {
-                type: "LoadBalancer",
-                externalTrafficPolicy: "Local",
-                loadBalancerIP: lbIp
-            },
-            image: {tag: "0.24.1"}
-        }
-    }
-}, { dependsOn: [nsnginx], provider: clusterProvider });
+// export const nginx = new k8s.helm.v2.Chart("nginx-ingress", {
+//     repo: "stable",
+//     version: "0.24.1",
+//     chart: "nginx-ingress",
+//     transformations: [addNamespace],
+//     namespace: "nginx",
+//     values: {
+//         rbac: {create: true},
+//         controller: {
+//             publishService: {enabled: true},
+//             stats: {enabled: true},
+//             service: {
+//                 type: "LoadBalancer",
+//                 externalTrafficPolicy: "Local",
+//                 loadBalancerIP: lbIp
+//             },
+//             image: {tag: "0.24.1"}
+//         }
+//     }
+// }, { dependsOn: [nsnginx], provider: clusterProvider });
