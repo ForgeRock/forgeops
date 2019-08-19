57cffad1dad5bb8e289f88a2a3045829a6657f83
Christophe Sovant <christophe.sovant@forgerock.com>
2019-08-19 12:27:05 +0200

    PIT-142 Simplify code for Greenfield tests

--- jenkins-scripts/stages/pit-greenfield.groovy
+++ jenkins-scripts/stages/pit-greenfield.groovy
@@ -6,70 +6,33 @@
  * to such license between the licensee and ForgeRock AS.
  */
 
-import java.time.Instant
 import com.forgerock.pipeline.reporting.PipelineRun
-
-def getTimeDiff(start, end) {
-    return end - start
-}
-
-def getEpochTime() {
-    return Instant.now().epochSecond
-}
 
 void runStage(PipelineRun pipelineRun, String stageName) {
 
     pipelineRun.pushStageOutcome(commonModule.normalizeStageName(stageName), stageDisplayName: stageName) {
-        def cfg = [
-            TESTS_SCOPE          : 'tests/integration',
-            SAMPLE_NAME          : 'ds-shared-repo',
-            CLUSTER_DOMAIN       : 'pit-24-7.forgeops.com',
-            SKIP_CLEANUP         : 'True',
-            RUN_NAME             : 'Initial',
-            CLUSTER_NAMESPACE    : 'greenfield',
-            STASH_LODESTAR_BRANCH: commonModule.LODESTAR_GIT_COMMIT,
-            SKIP_FORGEOPS        : 'True',
-        ]
+        node('google-cloud') {
+            stage(stageName) {
+                pipelineRun.updateStageStatusAsInProgress()
+                def forgeopsPath = localGitUtils.checkoutForgeops()
 
-        stage(stageName) {
-            pipelineRun.updateStageStatusAsInProgress()
-            // Initial run with deployment
-            def start = getEpochTime()
+                dir('lodestar') {
+                    def cfg = [
+                        TESTS_SCOPE          : 'tests/integration',
+                        SAMPLE_NAME          : 'ds-shared-repo',
+                        CLUSTER_DOMAIN       : 'pit-24-7.forgeops.com',
+                        CLUSTER_NAMESPACE    : 'greenfield',
+                        REPEAT               : 23,
+                        REPEAT_WAIT          : 3600,
+                        STASH_LODESTAR_BRANCH: commonModule.LODESTAR_GIT_COMMIT,
+                        SKIP_FORGEOPS        : 'True',
+                        EXT_FORGEOPS_PATH    : forgeopsPath
+                    ]
 
-            runTest(cfg)
-
-            def end = getEpochTime()
-            sleep(3600 - getTimeDiff(start, end))
-
-            /* This pipeline is run once per day; the greenfield tests are run once per hour. 22 runs fills the day.
-             * First run already done in first stage with deployment, last is done with cleanup in separate stage. */
-            def runs = 22
-            cfg.SKIP_DEPLOY = 'True'
-
-            runs.times {
-                start = getEpochTime()
-                runTest(cfg)
-                end = getEpochTime()
-                sleep(3600 - getTimeDiff(start, end))
-            }
-
-            // Last run with cleanup
-            cfg.SKIP_CLEANUP = 'False'
-            cfg.RUN_NAME = 'Last'
-
-            runTest(cfg)
-        }
-    }
-}
-
-private void runTest(cfg) {
-    node('google-cloud') {
-        def forgeopsPath = localGitUtils.checkoutForgeops()
-
-        dir('lodestar') {
-            cfg.EXT_FORGEOPS_PATH = forgeopsPath
-            commonModule.determinePitOutcome("${env.BUILD_URL}") {
-                withGKEPitNoStages(cfg)
+                    commonModule.determinePitOutcome("${env.BUILD_URL}/Allure_20Report_20Greenfield") {
+                        withGKEPitNoStages(cfg)
+                    }
+                }
             }
         }
     }
