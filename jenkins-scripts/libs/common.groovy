37a84ac520d8e1a989642742c89fa1bd7f74dc82
jenkins <techies@forgerock.com>
2019-09-13 13:13:08 +0000

    Increment Lodestar version to 8d3fb1378fb4478785b470c6ac8ee91c825ed90f

--- jenkins-scripts/libs/common.groovy
+++ jenkins-scripts/libs/common.groovy
@@ -27,7 +27,7 @@ FORGEOPS_GIT_COMMIT = sh(script: 'git rev-parse HEAD', returnStdout: true).trim(
 /**
  * Globally scoped git commit information for the Lodestar repo
  */
-LODESTAR_GIT_COMMIT = '707985b03cf55856f7231ab9add3f989b92f1194'
+LODESTAR_GIT_COMMIT = '8d3fb1378fb4478785b470c6ac8ee91c825ed90f'
 
 /**
  * Base versions for the PIT#2 upgrade test
