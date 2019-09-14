34f7b4f05357a503c9269e699582c5c9f561a940
Warren Strange <warren.strange@forgerock.com>
2019-09-13 19:17:54 -0600

    Updated AM/IDM to use the pod dns name, which is safer than the headless service fixed the headless service - the selector was app=ds for both the CTS and Idrepo

--- docker/am/openam/boot.sh
+++ docker/am/openam/boot.sh
@@ -9,9 +9,9 @@ AM_HOME=/home/forgerock/openam
 # First we wait for ds-idrepo to be up
 
 echo "Waiting for ds-idrepo to be available. Trying ds-idrepo:8080/alive endpoint"
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' ds-idrepo:8080/alive)" != "200" ]]; 
-do 
-        sleep 5; 
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' ds-idrepo:8080/alive)" != "200" ]];
+do
+        sleep 5;
 done
 echo "ds-idrepo is responding"
 
@@ -39,11 +39,10 @@ copy_secrets
 # This is not currently foolproof - it the ds-idrepo is not started yet the ldap search will also fail. This
 # can result in util installer running again - which in most cases is fine - it will refresh the configuraition.
 
-SVC="ou=services,$BASE_DN"
-r=$(ldapsearch -w password  -D "uid=admin" -A -H "ldap://ds-idrepo:1389" -s base -l 20 -b "$TEST_DN"  > /dev/null 2>&1)
+ldapsearch -w password  -D "uid=admin" -A -H "ldap://ds-idrepo-0.ds-idrepo:1389" -l 20 -b "$TEST_DN"  > /dev/null 2>&1
 status=$?
 echo "Is configured exit status is $status"
-if [ $status -ne 0 ]; then 
+if [ $status -ne 0 ]; then
     echo "Looks like ds-idrepo is not configured. I will remove boot.json"
     rm $AM_HOME/config/boot.json
 else
