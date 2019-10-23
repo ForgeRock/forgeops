2ea51c2e4535c9be193ff5f2686779b6224e1766
Christophe Sovant <christophe.sovant@forgerock.com>
2019-10-23 13:44:13 +0200

    PIT-196 Cleanup/simplification of upgrade mechanism

--- jenkins-scripts/stages/pit-upgrade.groovy
+++ jenkins-scripts/stages/pit-upgrade.groovy
@@ -15,7 +15,12 @@ void runStage(PipelineRun pipelineRun, String stageName) {
             stage(stageName) {
                 pipelineRun.updateStageStatusAsInProgress()
                 def forgeopsPath = localGitUtils.checkoutForgeops()
-                imageName = 'ds-empty'
+
+                String upgradeDsVersion = commonModule.UPGRADE_TEST_BASE_DS_VERSION
+                if (imageName == 'ds-empty') {
+                    upgradeDsVersion = commonModule.UPGRADE_TEST_BASE_DSEMPTY_VERSION
+                }
+
                 dir('lodestar') {
                     def cfg = [
                         TESTS_SCOPE                     : 'tests/upgrade',
@@ -24,19 +29,8 @@ void runStage(PipelineRun pipelineRun, String stageName) {
                         CLUSTER_DOMAIN                  : 'pit-24-7.forgeops.com',
                         COMPONENTS_AMSTER_IMAGE_TAG     : commonModule.UPGRADE_TEST_BASE_AMSTER_VERSION,
                         COMPONENTS_AM_IMAGE_TAG         : commonModule.UPGRADE_TEST_BASE_AM_VERSION,
-                        COMPONENTS_AM_IMAGE_UPGRADE_TAG : commonModule.getHelmChart('am').currentTag,
                         COMPONENTS_IDM_IMAGE_TAG        : commonModule.UPGRADE_TEST_BASE_IDM_VERSION,
-                        COMPONENTS_IDM_IMAGE_UPGRADE_TAG: commonModule.getHelmChart('idm').currentTag,
-                        COMPONENTS_CTSSTORE_IMAGE_TAG   : commonModule.UPGRADE_TEST_BASE_DSEMPTY_VERSION,
-                        COMPONENTS_CTSSTORE_IMAGE_REPOSITORY : "gcr.io/forgerock-io/${imageName}/pit1",
-                        COMPONENTS_USERSTORE_IMAGE_TAG  : commonModule.UPGRADE_TEST_BASE_DSEMPTY_VERSION,
-                        COMPONENTS_USERSTORE_IMAGE_REPOSITORY : "gcr.io/forgerock-io/${imageName}/pit1",
-                        COMPONENTS_USERSTORE_IMAGE_UPGRADE_TAG : commonModule.getHelmChart(imageName).currentTag,
-                        COMPONENTS_USERSTORE_IMAGE_UPGRADE_REPOSITORY : "gcr.io/forgerock-io/${imageName}/pit1",
-                        // The image used for AM upgrade contains old layout
-                        COMPONENTS_AM_UTILIMAGE_TAG     : '7.0.0-am-old-layout',
-                        COMPONENTS_AM_AM_SECRETSDIR     : '/home/forgerock/openam/am',
-                        COMPONENTS_AM_AM_KEYSTORESDIR   : '/home/forgerock/openam/am',
+                        COMPONENTS_DS_IMAGE_TAG         : commonModule.UPGRADE_TEST_BASE_DSEMPTY_VERSION,
                         STASH_LODESTAR_BRANCH           : commonModule.LODESTAR_GIT_COMMIT,
                         EXT_FORGEOPS_BRANCH             : 'fraas-production',
                         EXT_FORGEOPS_UPGRADE_BRANCH     : commonModule.FORGEOPS_GIT_COMMIT
