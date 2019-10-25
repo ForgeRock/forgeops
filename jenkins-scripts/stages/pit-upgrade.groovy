0bf326efefaeb391eb760a7a0b979c8fbc8c9832
Christophe Sovant <christophe.sovant@forgerock.com>
2019-10-25 15:04:35 +0200

    Remove upgrade-ds stage in PIT2 as the ds image is no more promoted

--- jenkins-scripts/stages/pit-upgrade.groovy
+++ jenkins-scripts/stages/pit-upgrade.groovy
@@ -8,36 +8,31 @@
 
 import com.forgerock.pipeline.reporting.PipelineRun
 
-void runStage(PipelineRun pipelineRun, String stageName, String scope, String deploymentName, String imageName) {
+void runStage(PipelineRun pipelineRun, String stageName) {
 
     pipelineRun.pushStageOutcome(commonModule.normalizeStageName(stageName), stageDisplayName: stageName) {
         node('google-cloud') {
             stage(stageName) {
                 pipelineRun.updateStageStatusAsInProgress()
                 def forgeopsPath = localGitUtils.checkoutForgeops()
-
-                String upgradeDsVersion = commonModule.UPGRADE_TEST_BASE_DS_VERSION
-                if (imageName == 'ds-empty') {
-                    upgradeDsVersion = commonModule.UPGRADE_TEST_BASE_DSEMPTY_VERSION
-                }
-
+                imageName = 'ds-empty'
                 dir('lodestar') {
                     def cfg = [
-                        TESTS_SCOPE                     : scope,
-                        DEPLOYMENT_NAME                 : deploymentName,
+                        TESTS_SCOPE                     : 'tests/upgrade',
+                        DEPLOYMENT_NAME                 : 'ds-shared-repo',
                         DS_IMAGE_NAME                   : imageName,
-                        CLUSTER_NAMESPACE               : "upgrade-${imageName}",
+                        CLUSTER_NAMESPACE               : "upgrade",
                         CLUSTER_DOMAIN                  : 'pit-24-7.forgeops.com',
                         COMPONENTS_AMSTER_IMAGE_TAG     : commonModule.UPGRADE_TEST_BASE_AMSTER_VERSION,
                         COMPONENTS_AM_IMAGE_TAG         : commonModule.UPGRADE_TEST_BASE_AM_VERSION,
                         COMPONENTS_AM_IMAGE_UPGRADE_TAG : commonModule.getHelmChart('am').currentTag,
                         COMPONENTS_IDM_IMAGE_TAG        : commonModule.UPGRADE_TEST_BASE_IDM_VERSION,
                         COMPONENTS_IDM_IMAGE_UPGRADE_TAG: commonModule.getHelmChart('idm').currentTag,
-                        COMPONENTS_CTSSTORE_IMAGE_TAG   : upgradeDsVersion,
+                        COMPONENTS_CTSSTORE_IMAGE_TAG   : commonModule.UPGRADE_TEST_BASE_DSEMPTY_VERSION,
                         COMPONENTS_CTSSTORE_IMAGE_REPOSITORY : "gcr.io/forgerock-io/${imageName}/pit1",
-                        COMPONENTS_USERSTORE_IMAGE_TAG  : upgradeDsVersion,
+                        COMPONENTS_USERSTORE_IMAGE_TAG  : commonModule.UPGRADE_TEST_BASE_DSEMPTY_VERSION,
                         COMPONENTS_USERSTORE_IMAGE_REPOSITORY : "gcr.io/forgerock-io/${imageName}/pit1",
-                        COMPONENTS_USERSTORE_IMAGE_UPGRADE_TAG : commonModule.getHelmChart('ds').currentTag,
+                        COMPONENTS_USERSTORE_IMAGE_UPGRADE_TAG : commonModule.getHelmChart(imageName).currentTag,
                         COMPONENTS_USERSTORE_IMAGE_UPGRADE_REPOSITORY : "gcr.io/forgerock-io/${imageName}/pit1",
                         // The image used for AM upgrade contains old layout
                         COMPONENTS_AM_UTILIMAGE_TAG     : '7.0.0-am-old-layout',
@@ -50,7 +45,7 @@ void runStage(PipelineRun pipelineRun, String stageName, String scope, String de
                         EXT_FORGEOPSINIT_BRANCH         : 'fraas-production'
                     ]
 
-                    commonModule.determinePitOutcome("${env.BUILD_URL}/Allure_20Report_20PIT_5fUpgrade_5fWith_5f${imageName}_5fImage") {
+                    commonModule.determinePitOutcome("${env.BUILD_URL}/Allure_20Report_20PIT_5fUpgrade") {
                         withGKEPitNoStages(cfg)
                     }
                 }
