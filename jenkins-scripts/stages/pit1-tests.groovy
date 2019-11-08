1c0d6dd7d1ea2ccd5db7ec8579d702cc74f8b99d
Christophe Sovant <christophe.sovant@forgerock.com>
2019-11-09 00:35:24 +0100

    Revert "LODESTAR-153 Create a new skaffold profile based on the new 'platform-deployment' helm sample"
    This reverts commit d61ff2a1899b54726a12ab7732f0849edb2a0ad6.

--- jenkins-scripts/stages/pit1-tests.groovy
+++ jenkins-scripts/stages/pit1-tests.groovy
@@ -20,12 +20,11 @@ void runStage(PipelineRun pipelineRun, String scope) {
                 pipelineRun.updateStageStatusAsInProgress()
                 dir('lodestar') {
                     def cfg = [
-                            TESTS_SCOPE                     : 'tests/platform-deployment',
-                            DEPLOYMENT_NAME                 : 'platform-deployment',
-                            STASH_LODESTAR_BRANCH           : commonModule.LODESTAR_GIT_COMMIT,
-                            COMPONENTS_FRCONFIG_GIT_BRANCH  : commonModule.FORGEOPS_GIT_COMMIT,
-                            SKIP_FORGEOPS                   : 'True',
-                            EXT_FORGEOPS_PATH               : "${env.WORKSPACE}/forgeops"
+                            STASH_LODESTAR_BRANCH   : commonModule.LODESTAR_GIT_COMMIT,
+                            TESTS_SCOPE             : scope,
+                            DEPLOYMENT_NAME         : 'smoke-deployment',
+                            SKIP_FORGEOPS           : 'True',
+                            EXT_FORGEOPS_PATH       : "${env.WORKSPACE}/forgeops"
                     ]
 
                     commonModule.determinePitOutcome("${env.BUILD_URL}/Allure_20Report_20Run_5fPIT_5fSmoke_5fTests/") {
