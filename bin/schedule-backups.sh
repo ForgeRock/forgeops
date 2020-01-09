7b90d7b5a7d1dde8e0cc9eb339d97125dd011d69
jcastillo <jcastillo@localhost>
2020-01-08 17:20:08 -0800

    CLOUD-XXXX schedule-backups can accept namespace as input.
    If none provided defaults to the one in kubeconfig

--- bin/schedule-backups.sh
+++ bin/schedule-backups.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 # Simple script to schedule DS backups
 
-pods=( $(kubectl get pods | grep ds | echo $(awk '{ print $1 }')) )
+
+kcontext=$(kubectl config current-context)
+NS=$(kubectl config view -o jsonpath="{.contexts[?(@.name==\"$kcontext\")].context.namespace}")
+if [ $# = '1' ]; then
+    NAMESPACE=$1
+else
+    NAMESPACE=$NS
+fi
+
+if [[ -z "$NAMESPACE" ]] ; then
+    echo 'Please provide the target namespace. e.a. schedule-backups.sh namespace-name'
+    exit -1
+fi
+
+pods=( $(kubectl get pods -n $NAMESPACE | grep ds | echo $(awk '{ print $1 }')) )
 for pod in "${pods[@]}"
 do
   echo ""
   echo "scheduling backup for pod: $pod"
-  kubectl exec -ti $pod ./scripts/schedule-backup.sh
+  kubectl -n $NAMESPACE exec -ti $pod ./scripts/schedule-backup.sh
 done
 
