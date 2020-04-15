aac9b78b0b4c8e998d125595c88384765bc752f1
Jhonattan Castillo <jhonattan.castillo@forgerock.com>
2020-04-14 23:12:58 -0700

    feat: Add new trigger for bitbucket PRs using PR notification plugin
    Add new trigger for bitbucket PRs using PR notification plugin
    
    CLOUD-2137

---
+++ cicd/tekton/utils/install-pipeline.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+if [ "$#" -ne 1 ]
+then
+  echo "Need namespace name to install the pipelines in. ./install_tekton.sh smoke"
+  exit 1
+fi
+
+NAMESPACE=$1
+kubectl create namespace $NAMESPACE || true #create $NAMESPACE namespace. Ignore if this namespace is already present.
+kubectl -n tekton-pipelines wait --for=condition=Ready pod --all
+
+kubectl -n $NAMESPACE apply -f .
+echo ""
+echo "Installation complete! You can now use the $NAMESPACE pipeline"
+
