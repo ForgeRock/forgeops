10998a2888b2594bc525090b10b52e8886bc6df7
Warren Strange <warren.strange@forgerock.com>
2020-05-26 17:21:41 +0000

    feat: Toolbox improvements
    Rename forgeoops-toolbox.sh to toolbox
    Convert to pod to make it easier to connect in vscode
    Move env vars to a configmap
    Add create-dev.sh script to create a development deployment
    
    refs: CLOUD-2238

--- bin/bootstrap-project.sh
+++ bin/bootstrap-project.sh
@@ -6,18 +6,18 @@ set -o pipefail
 
 [[ "${FR_DEBUG}" == true ]] && set -x
 
-NAMESPACE="${FR_NAMESPACE}"
-SUBDOMAIN="${FR_SUBDOMAIN:-iam}"
-DOMAIN="${FR_DOMAIN}"
-WORKSPACE="${FR_WORKSPACE:-/opt/workspace/forgeops}"
+NAMESPACE="${NAMESPACE}"
+FQDN="${FQDN:-default.iam.example.com}"
+WORKSPACE="${WORKSPACE:-/opt/workspace/forgeops}"
 UPSTREAM="${FR_UPSTREAM:-https://github.com/ForgeRock/forgeops.git}"
-DOCKER_REPO="${FR_DOCKER_REPO}"
+DOCKER_REPO="${DOCKER_REPO:-gcr.io/engineering-devops}"
 FORK="${FR_FORK}"
 
 usage() {
-    cat <<EOF
+  cat <<EOF
 Usage:
-$0 -n namespace -s subdomain -d domain -f fork_git_url -r docker_repo [-w workspace_dir] init-workspace|configure-fork|render-templates|regenerate-deploy-key
+$0 -n namespace -f FQDN -f fork_git_url -d docker_repo dev
+    dev  - Create a development skaffold instance for your fully qualified domain name
     init-workspace: clone forgeops and setup directory to work from [-w]
     configure-fork: configure forgeops master to be upstream and origin as fork -f[w]
     render-templates: render kustomization, skaffold, config, dev script -nsdfr[w]
@@ -25,56 +25,54 @@ $0 -n namespace -s subdomain -d domain -f fork_git_url -r docker_repo [-w worksp
     run-bootstrap: is to configure-fork and render-templates is run when no cmd is specified
 
 Example:
-$0 -n dev -s iam -d forgeops.com -f https://github.com/forgerock/forgeops.git -r gcr.io/engineering-devops
+$0 -n dev -f default.iam.example.com  -r gcr.io/engineering-devops create-dev
 
 
 EOF
 }
 
-setup_workspace () {
-    if [[ ! -d "${WORKSPACE}" ]];
-    then
-        echo "Cloning $UPSTREAM"
-        git clone --origin upstream --depth 1 "$UPSTREAM" "${WORKSPACE}"
-    fi
+setup_workspace() {
+  if [[ ! -d "${WORKSPACE}" ]]; then
+    echo "Cloning $UPSTREAM"
+    git clone --origin upstream --depth 1 "$UPSTREAM" "${WORKSPACE}"
+  fi
 }
 
-setup_fork () {
-    echo "Adding $FORK as the git remote origin"
-    if ! git remote add origin "$FORK";
-    then
-        return 1
-    fi
-    return 0
+setup_fork() {
+  echo "Adding $FORK as the git remote origin"
+  if ! git remote add origin "$FORK"; then
+    return 1
+  fi
+  return 0
 
 }
 
-keygen () {
-    mkdir -p ~/.ssh "${WORKSPACE}/.ssh"
-    ssh-keygen -b 4096 \
-               -C "ForgeOps toolbox deployment key" \
-               -t ed25519 \
-               -f "${WORKSPACE}/.ssh/id_ed"
-    cat <<EOF >"$HOME/.ssh/config"
+keygen() {
+  mkdir -p ~/.ssh "${WORKSPACE}/.ssh"
+  ssh-keygen -b 4096 \
+    -C "ForgeOps toolbox deployment key" \
+    -t ed25519 \
+    -f "${WORKSPACE}/.ssh/id_ed"
+  cat <<EOF >"$HOME/.ssh/config"
 Host *
     IdentityFile $WORKSPACE/.ssh/id_ed
     IdentitiesOnly
 EOF
-    echo "configure your repo to accept pushes from this public key:"
-    cat "${WORKSPACE}/.ssh/id_ed.pub"
-    echo ""
-    echo ""
-    echo "this key is destroyed with the PVC, its recommended that this key be configured with limited access like a deploy key: https://developer.github.com/v3/guides/managing-deploy-keys/"
+  echo "configure your repo to accept pushes from this public key:"
+  cat "${WORKSPACE}/.ssh/id_ed.pub"
+  echo ""
+  echo ""
+  echo "this key is destroyed with the PVC, its recommended that this key be configured with limited access like a deploy key: https://developer.github.com/v3/guides/managing-deploy-keys/"
 }
 
-render_templates () {
-    CDIR=./dev
-    echo "Generating Kustomize template in $CDIR directory for $NAMESPACE.$SUBDOMAIN.$DOMAIN"
-    mkdir -p "$CDIR"
-    cat <<EOF >"$CDIR/kustomization.yaml"
-# Generated Kustomization file. You may have to edit this for your requirements.
-# This deploys to $NAMESPACE.$SUBDOMAIN.$DOMAIN
-# for self signed SSL certs. It is suitable for local minikube development
+# Creates a ./dev folder that contains kustomize base patched to use the desired FQDN
+create_dev_config() {
+  CDIR=./dev
+  echo "Generating Kustomize template in $CDIR directory for $FQDN"
+  mkdir -p "$CDIR"
+  cat <<EOF >"$CDIR/kustomization.yaml"
+# Generated Kustomization file. Edit this for your requirements.
+# This deploys to the ingress $FQDN in the $NAMESPACE
 namespace: $NAMESPACE
 resources:
 - ../kustomize/overlay/7.0/all
@@ -86,147 +84,122 @@ patchesStrategicMerge:
   metadata:
     name: platform-config
   data:
-    DOMAIN: $DOMAIN
-    SUBDOMAIN: $SUBDOMAIN
-
-# Patches the ingress to use the Let's Encrypt issuer
-patchesJson6902:
-- target:
-    group: extensions
-    version: v1beta1
-    kind: Ingress
-    name: forgerock
-  patch: |-
-    - op: replace
-      path: /metadata/annotations/certmanager.io~1cluster-issuer
-      value: letsencrypt-prod
-      # value: default-issuer  # Default
+    FQDN: $FQDN
+    # This uses a cert-manager Let's encrypt issuer. Comment this out if you are not using cert-manager and LE
+    CERT_ISSUER: letsencrypt-prod
 EOF
 
-echo "Creating $CDIR/run.sh script"
-cat >"$CDIR/run.sh" <<EOF
+  echo "Creating $CDIR/run.sh script"
+  cat >"$CDIR/run.sh" <<EOF
 #!/usr/bin/env bash
-# Generated script
+# Generated script - edit for your requirements
 
+# This copies the default (CDK) configuration to the docker/folders.
+./bin/config.sh init
+
+# Specify the skaffold profile to use with -p
+# dev - use the docker builder profile
+# kdev - the Kaniko in-cluster builder.
 skaffold --default-repo=$DOCKER_REPO -p kdev dev
 
 EOF
-chmod +x $CDIR/run.sh
+  chmod +x $CDIR/run.sh
 
-./bin/config.sh -p "$PROFILE" init;
+
+  echo "Configuration created in ./dev. YOU MUST EDIT THESE FILES FIRST"
+  echo "Edit the run.sh"
+
 }
 
-run_render_templates () {
-    # check required variables
-    missing_opts=0
-    for req in NAMESPACE SUBDOMAIN DOMAIN WORKSPACE DOCKER_REPO;
-    do
-        if [[ "${!req}" == "" ]];
-        then
-            echo "${req} is required";
-            missing_opts=1
-        fi
-    done
-    if (( "${missing_opts}" != 0 ));
-    then
-        usage;
-        return 1
-    fi
-    render_templates
-    return $!
-}
-
-run_setup_workspace () {
-    if [[ "${WORKSPACE}" == "" ]]
-    then
-        echo "workspace required"
-        return 1
-    fi
-    setup_workspace
-    return $!
+run_setup_workspace() {
+  if [[ "${WORKSPACE}" == "" ]]; then
+    echo "workspace required"
+    return 1
+  fi
+  setup_workspace
+  return $!
 
 }
 
 run_bootstrap() {
-    echo "bootstrapping"
-    setup_workspace \
-        && run_setup_fork \
-            && run_render_templates \
-                && touch "${WORKSPACE}/.CONFIGURED"
-    return $!
+  echo "bootstrapping"
+  setup_workspace &&
+    run_setup_fork &&
+    run_render_templates &&
+    touch "${WORKSPACE}/.CONFIGURED"
+  return $!
 }
 
 run_setup_fork() {
-    cd "$WORKSPACE" || return 1
-    setup_fork
-    return $!
+  cd "$WORKSPACE" || return 1
+  setup_fork
+  return $!
 }
 
-run_keygen () {
-    if [[ ! -d "${WORKSPACE}/.ssh" ]];
-    then
-        echo "generating ssh key...please provide passphrase"
-        keygen
-        return $!
-    fi
-    echo "ssh key already generated, rm -rf ${WORKSPACE}/.ssh to replace it"
-    return 1
+run_keygen() {
+  if [[ ! -d "${WORKSPACE}/.ssh" ]]; then
+    echo "generating ssh key...please provide passphrase"
+    keygen
+    return $!
+  fi
+  echo "ssh key already generated, rm -rf ${WORKSPACE}/.ssh to replace it"
+  return 1
 }
 
 # dont do anything if workspace has been configured
-if [[ -f "${WORKSPACE}/.CONFIGURED" ]];
-then
-    echo "project already bootstrapped";
-    exit 0
+if [[ -f "${WORKSPACE}/.CONFIGURED" ]]; then
+  echo "project already bootstrapped"
+  exit 0
 fi
 
 # handle opts
-while getopts n:s:d:f:r:w:h option
-do
-    case "${option}"
-        in
-        n) NAMESPACE=${OPTARG};;
-        s) SUBDOMAIN=${OPTARG};;
-        d) DOMAIN=${OPTARG};;
-        f) FORK=${OPTARG};;
-        r) DOCKER_REPO=${OPTARG};;
-        w) WORKSPACE=${OPTARG};;
-        h) usage; exit 0;;
-        *) usage; exit 1;;
-    esac
+while getopts n:f:g:d:h option; do
+  case "${option}" in
+
+  n) NAMESPACE=${OPTARG} ;;
+  f) FQDN=${OPTARG} ;;
+  g) UPSTREAM=${OPTARG} ;;
+  d) DOCKER_REPO=${OPTARG} ;;
+  h)
+    usage
+    exit 0
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+  esac
 done
 shift $((OPTIND - 1))
 
-FQDN="$NAMESPACE.$SUBDOMAIN.$DOMAIN"
-PROFILE="cdk"
 
-while (( "$#" )); do
-    case "$1" in
-        init-workspace)
-            shift
-            run_setup_workspace
-            exit $!
-            ;;
-        configure-fork)
-            shift
-            run_setup_fork
-            exit $!
-            ;;
-        render-templates)
-            run_render_templates
-            exit 0
-            ;;
-        regenerate-deploy-key)
-            run_keygen
-            exit 0
-            ;;
-        run-bootstrap)
-            run_bootstrap
-            exit $!
-            ;;
-        *)
-            usage
-            exit 1
-            ;;
-    esac
+while (("$#")); do
+  case "$1" in
+  init-workspace)
+    shift
+    run_setup_workspace
+    exit $!
+    ;;
+  configure-fork)
+    shift
+    run_setup_fork
+    exit $!
+    ;;
+  dev)
+    create_dev_config
+    exit 0
+    ;;
+  regenerate-deploy-key)
+    run_keygen
+    exit 0
+    ;;
+  run-bootstrap)
+    run_bootstrap
+    exit $!
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+  esac
 done
