c211a127c5f6891cfc1a4a897d0d467ec04bcb19
Warren Strange <warren.strange@forgerock.com>
2020-06-12 00:13:36 +0000

    fix: tekton improvements
    Split wait logic out to a task. Add some utility scripts for assisting
    in testing.
    Update smoke test task to use fbc based secrets
    
    refs: CLOUD-2272
    
    fix typo

---
+++ cicd/tekton/smoke/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Utility to run the smoke test
+kubectl -n tekton-pipelines apply -f shared/tasks/wait-platform-up.yaml
+kubectl -n tekton-pipelines apply -f shared/tasks/smoke-test-task.yaml
+kubectl -n tekton-pipelines  apply -f smoke/smoke-pipeline.yaml
+kubectl -n tekton-pipelines  apply -f smoke/smoke-quick.yaml
+kubectl -n tekton-pipelines apply -f nightly/nightly-pipeline.yaml
+
+tkn -n tekton-pipelines pipeline start smoke-pipeline -s tekton-worker
+# tkn -n tekton-pipelines pipeline start smoke-quick -s tekton-worker
