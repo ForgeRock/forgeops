f64686a26c3a8043088d7ab29ea8cd56f5a1a14f
Jhonattan Castillo <jhonattan.castillo@forgerock.com>
2020-06-22 10:38:56 -0700

    chore: Clean up init-and-restore.sh
    DS passwords are set as part of the initialize. No need to set them here

--- docker/7.0/ds/scripts/init-and-restore.sh
+++ docker/7.0/ds/scripts/init-and-restore.sh
@@ -17,34 +17,8 @@ if [ -n "$(ls -A /opt/opendj/data -I lost+found)" ]; then
   ls -A /opt/opendj/data -I lost+found
 fi
 
-##
 # Initialize DS regarless of dsbackup restore settings
-# /opt/opendj/docker-entrypoint.sh initialize-only
-##
-
-# Remove this section once support for DS_SET_UID_ADMIN_AND_MONITOR_PASSWORDS is added
-update_pw() {
-     if [ ! -f "$1" ]; then
-        echo "Can't find the password file $1. Won't change the password in $2"
-        return
-    fi
-
-    echo "Updating the password in $2"
-    # Set the JVM args so we dont blow up the container memory.
-    pw=$(OPENDJ_JAVA_ARGS="-Xmx256m -Djava.security.egd=file:/dev/./urandom" bin/encode-password  -s "PBKDF2-HMAC-SHA256" -f $1 | sed -e 's/Encoded Password:  "//' -e 's/"//g' 2>/dev/null)
-    # $pw can contian / - so need to use alternate sed delimiter.
-    sed -ibak "s#userPassword: .*#userPassword: $pw#" "$2"
-}
-
 /opt/opendj/docker-entrypoint.sh initialize-only;
-
-DS_UID_ADMIN_PASSWORD_FILE=${DS_UID_ADMIN_PASSWORD_FILE-"/var/run/secrets/opendj-passwords/dirmanager.pw"}
-DS_UID_MONITOR_PASSWORD_FILE=${DS_UID_MONITOR_PASSWORD_FILE-"/var/run/secrets/opendj-passwords/monitor.pw"}
-ROOT_USER_LDIF=${ROOT_USER_LDIF-"/opt/opendj/data/db/rootUser/rootUser.ldif"}
-MONITOR_USER_LDIF=${MONITOR_USER_LDIF-"/opt/opendj/data/db/monitorUser/monitorUser.ldif"}
-update_pw "$DS_UID_ADMIN_PASSWORD_FILE" "${ROOT_USER_LDIF}"
-update_pw "$DS_UID_MONITOR_PASSWORD_FILE" "${MONITOR_USER_LDIF}"
-###
 
 if [ -z "${AUTORESTORE_FROM_DSBACKUP}" ] || [ "${AUTORESTORE_FROM_DSBACKUP}" != "true" ]; then
     echo "AUTORESTORE_FROM_DSBACKUP is missing or not set to true. Skipping restore"
