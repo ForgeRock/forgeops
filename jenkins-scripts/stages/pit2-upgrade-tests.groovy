25f1c37cd32a10d07d01803b0c07aefb5a16e248
Christophe Sovant <christophe.sovant@forgerock.com>
2020-08-12 19:00:53 +0200

    task: Add stage in PIT2 to run pit1 tests against complete upgraded latest stack (binary upgrade)
    ref: PIT-329

--- jenkins-scripts/stages/pit2-upgrade-tests.groovy
+++ jenkins-scripts/stages/pit2-upgrade-tests.groovy
@@ -12,6 +12,7 @@ void runStage(PipelineRun pipelineRun) {
 
     def stageName = 'PIT2 Binary Upgrade'
     def normalizedStageName = dashboard_utils.normalizeStageName(stageName)
+    def fraasTag = 'fraas-production'
 
     pipelineRun.pushStageOutcome(normalizedStageName, stageDisplayName: stageName) {
         node('google-cloud') {
@@ -21,27 +22,55 @@ void runStage(PipelineRun pipelineRun) {
                 dir('lodestar') {
                     def stagesCloud = [:]
 
-                    // Upgrade tests
-                    def subStageName = 'binary_upgrade'
+                    def initialSubStageName = 'binary_upgrade'
+
+                    def config_common = [
+                        CLUSTER_DOMAIN                                  : 'pit-24-7.forgeops.com',
+                        CLUSTER_NAMESPACE                               : initialSubStageName,
+                        COMPONENTS_AMSTER_IMAGE_TAG                     : fraasTag,
+                        COMPONENTS_AM_IMAGE_TAG                         : fraasTag,
+                        COMPONENTS_AM_IMAGE_UPGRADE_TAG                 : commonModule.getCurrentTag('am'),
+                        COMPONENTS_AM_IMAGE_UPGRADE_REPOSITORY          : 'gcr.io/forgerock-io/am-base/pit1',
+                        COMPONENTS_IDM_IMAGE_TAG                        : fraasTag,
+                        COMPONENTS_IDM_IMAGE_UPGRADE_TAG                : commonModule.getCurrentTag('idm'),
+                        COMPONENTS_IDM_IMAGE_UPGRADE_REPOSITORY         : 'gcr.io/forgerock-io/idm/pit1',
+                        COMPONENTS_IG_IMAGE_TAG                         : fraasTag,
+                        COMPONENTS_IG_IMAGE_UPGRADE_TAG                 : commonModule.getCurrentTag('ig'),
+                        COMPONENTS_IG_IMAGE_UPGRADE_REPOSITORY          : 'gcr.io/forgerock-io/ig/pit1',
+                        COMPONENTS_DSIDREPO_IMAGE_TAG                   : fraasTag,
+                        COMPONENTS_DSIDREPO_IMAGE_UPGRADE_TAG           : commonModule.getCurrentTag('ds-idrepo'),
+                        COMPONENTS_DSIDREPO_IMAGE_UPGRADE_REPOSITORY    : 'gcr.io/forgerock-io/ds/pit1',
+                        COMPONENTS_DSCTS_IMAGE_TAG                      : fraasTag,
+                        COMPONENTS_DSCTS_IMAGE_UPGRADE_TAG              : commonModule.getCurrentTag('ds-cts'),
+                        COMPONENTS_DSCTS_IMAGE_UPGRADE_REPOSITORY       : 'gcr.io/forgerock-io/ds/pit1',
+                        STASH_LODESTAR_BRANCH                           : commonModule.LODESTAR_GIT_COMMIT,
+                        STASH_FORGEOPS_BRANCH                           : 'fraas-production',
+                    ]
+
+                    // Binary upgrade tests
+                    stagesCloud[initialSubStageName] = dashboard_utils.spyglaasStageCloud(initialSubStageName)
+
+                    dashboard_utils.determineUnitOutcome(stagesCloud[initialSubStageName]) {
+                        def config = config_common.clone()
+                        config += [
+                            TESTS_SCOPE                                  : 'tests/pit2/upgrade',
+                            SKIP_CLEANUP                                 : true,
+                            REPORT_NAME_PREFIX                           : initialSubStageName,
+                        ]
+
+                        withGKESpyglaasNoStages(config)
+                    }
+
+                    // pit1 tests after binary upgrade tests
+                    subStageName = 'pit1_after_binary_upgrade'
                     stagesCloud[subStageName] = dashboard_utils.spyglaasStageCloud(subStageName)
 
                     dashboard_utils.determineUnitOutcome(stagesCloud[subStageName]) {
-                        def config = [
-                            TESTS_SCOPE                                     : 'tests/pit2/upgrade',
-                            CLUSTER_DOMAIN                                  : 'pit-24-7.forgeops.com',
-                            CLUSTER_NAMESPACE                               : subStageName,
-                            COMPONENTS_AMSTER_IMAGE_TAG                     : '7.0.0-1ba49f89bf13e3d0543c80fd5fa284e32b5fd9e4',
-                            COMPONENTS_AM_IMAGE_UPGRADE_TAG                 : commonModule.getCurrentTag('am'),
-                            COMPONENTS_AM_IMAGE_UPGRADE_REPOSITORY          : 'gcr.io/forgerock-io/am-base/docker-build',
-                            COMPONENTS_IDM_IMAGE_UPGRADE_TAG                : commonModule.getCurrentTag('idm'),
-                            COMPONENTS_IDM_IMAGE_UPGRADE_REPOSITORY         : 'gcr.io/forgerock-io/idm/pit1',
-                            COMPONENTS_IG_IMAGE_UPGRADE_TAG                 : commonModule.getCurrentTag('ig'),
-                            COMPONENTS_IG_IMAGE_UPGRADE_REPOSITORY          : 'gcr.io/forgerock-io/ig/pit1',
-                            COMPONENTS_DSIDREPO_IMAGE_UPGRADE_TAG           : commonModule.getCurrentTag('ds-idrepo'),
-                            COMPONENTS_DSIDREPO_IMAGE_UPGRADE_REPOSITORY    : 'gcr.io/forgerock-io/ds/pit1',
-                            STASH_LODESTAR_BRANCH                           : commonModule.LODESTAR_GIT_COMMIT,
-                            STASH_FORGEOPS_BRANCH                           : 'fraas-production',
-                            REPORT_NAME_PREFIX                              : subStageName,
+                        def config = config_common.clone()
+                        config += [
+                            TESTS_SCOPE                                  : 'tests/pit1',
+                            SKIP_DEPLOY                                  : true,
+                            REPORT_NAME_PREFIX                           : subStageName,
                         ]
 
                         withGKESpyglaasNoStages(config)
