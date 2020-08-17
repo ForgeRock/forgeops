891bee338d2191fb43c7477597255469409d0076
Jake Feasel <jake.feasel@forgerock.com>
2020-08-17 11:46:00 -0700

    task: introduces IDM-specific scope
    This is part two of a change, broken up to coordinate test passage. This change involves updating client registration so they can start referring to the scope introduced in commit e454e817232cf6ab07cc5e6e8947991ccce9eebd. After these registrations are updated, the next step will be updating the platform-ui clients and the various test clients to start including the scope.
    
    ref: IAM-580

--- docker/gatling/src/gatling/simulations/util.scala
+++ docker/gatling/src/gatling/simulations/util.scala
@@ -13,7 +13,7 @@ class BenchConfig {
     val host: String   = Properties.envOrElse("TARGET_HOST", "prod.iam.example.com")
     val port: String =  Properties.envOrElse("PORT", "443")
     val protocol: String =  Properties.envOrElse("protocol", "https")
-    val scope: String =  Properties.envOrElse("SCOPE", "openid")
+    val scope: String =  Properties.envOrElse("SCOPE", "fr:idm:*")
     val client_id: String = Properties.envOrElse("CLIENT_ID", "idm-provisioning")
     val client_password: String = Properties.envOrElse("CLIENT_PASSWORD", "password")
     val idmUser: String = Properties.envOrElse("IDM_USER", "openidm-admin")
@@ -40,7 +40,7 @@ class AMAuth (val config: BenchConfig) {
     val safetyMargin = 2 minutes
 
     // sample curl:
-    // curl -u idm-provisioning:password --data 'grant_type=client_credentials&scope=openid'
+    // curl -u idm-provisioning:password --data 'grant_type=client_credentials&scope=fr:idm:*'
     // -X POST https://smoke.iam.forgeops.com/am/oauth2/access_token
     // add this to a chain to get an access token.
     // tood: Do paramterize the client, grant, etc.
