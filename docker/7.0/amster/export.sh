b2ca0f7d9588458826afa618f2b38ca750c0548c
lbaines <lee.baines@forgerock.com>
2020-09-07 15:59:05 +0100

    feat: Increase the scope of dynamic configurations exported by Amster
    Export all agent types.
    Export policies.
    Export federation configs.
    Export OIDCClients.
    
    ref: CLOUD-2419

--- docker/7.0/amster/export.sh
+++ docker/7.0/amster/export.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-# TODO - create an job that exports the dynamic config
 
+# List of realm entities to export.
+realmEntities="OAuth2Clients IdentityGatewayAgents J2eeAgents WebAgents SoapStsAgents OIDCClient Policies CircleOfTrust Saml2Entity"
+
+# Create a temporary export folder.
 rm -fr /var/tmp/amster
 mkdir -p /var/tmp/amster
 
 # Create Amster export script.
 cat > /tmp/do_export.amster <<EOF
 connect -k  /var/run/secrets/amster/id_rsa http://am:80/am
-export-config --path /var/tmp/amster --realmEntities 'OAuth2Clients IdentityGatewayAgents' --globalEntities ' '
+export-config --path /var/tmp/amster --realmEntities '${realmEntities}' --globalEntities ' '
 :quit
 EOF
 
