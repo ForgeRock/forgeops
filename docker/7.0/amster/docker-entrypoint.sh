eba17c81f36cadd2193202ec068049c3c677ed24
Christophe Sovant <christophe.sovant@forgerock.com>
2020-09-12 11:07:39 +0200

    fix: Update regex used to extract Amster version

--- docker/7.0/amster/docker-entrypoint.sh
+++ docker/7.0/amster/docker-entrypoint.sh
@@ -23,7 +23,7 @@ pause() {
 # Extract amster version for commons parameter to modify configs
 echo "Extracting amster version"
 VER=$(./amster --version)
-[[ "$VER" =~ ([0-9].[0-9].[0-9](\.[0-9]*)?-([a-zA-Z][0-9]+|SNAPSHOT|RC[0-9]+)|[0-9].[0-9].[0-9](\.[0-9]*)?) ]]
+[[ "$VER" =~ ([0-9].[0-9].[0-9](\.[0-9]*)?-([a-zA-Z][0-9]+|([-a-zA-Z]+)?SNAPSHOT|RC[0-9]+)|[0-9].[0-9].[0-9](\.[0-9]*)?) ]]
 VERSION=${BASH_REMATCH[1]}
 echo "Amster version is: '${VERSION}'"
 export VERSION
