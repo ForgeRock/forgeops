3b72db696e5a6bd831a1d48830dde4a35eef3c4e
Warren Strange <warren.strange@forgerock.com>
2020-09-17 13:10:20 -0600

    fix: Release GCP load balancer before deleting the cluster
    This attempts to delete the L4 nginx service before deleting the cluster.
    
    refs: CLOUD-2579

--- cluster/gke/cluster-down.sh
+++ cluster/gke/cluster-down.sh
@@ -11,5 +11,16 @@ REGION=${REGION:-$R}
 
 ZONE=${ZONE:-"$REGION-a"}
 
+echo "Getting the cluster credentials for $NAME in Zone $ZONE"
+gcloud container clusters get-credentials "$NAME" --zone "$ZONE" || exit 1
+
+# Try to release any L4 service load balancers
+echo "Cleaning up the ngnix load balancer. Ignore any errors below"
+kubectl -n nginx delete deployment --all
+kubectl -n nginx delete svc --all
+kubectl delete ns nginx
+
+echo "About to delete the cluster in 10 seconds"
+sleep 10
 # Delete the cluster. Defaults to the current project.
-gcloud container clusters delete $NAME --zone "$ZONE"
+gcloud container clusters delete "$NAME" --zone "$ZONE"
