0f5eaba030f5174044979c95aad8d3fb8342c9f7
wajih.ahmed <wajih.ahmed@forgerock.com>
2020-09-17 10:03:16 -0400

    Refactored by removing hard coded callbacks to use json path.  Fixed the feeder and removed unused code.
    ref: perf-1688

--- docker/gatling/src/gatling/simulations/platform/Register.scala
+++ docker/gatling/src/gatling/simulations/platform/Register.scala
@@ -16,21 +16,23 @@ import io.gatling.http.protocol.HttpProtocolBuilder
 import io.gatling.core.structure.ChainBuilder
 import scala.concurrent.duration._
 import frutil._
+import com.fasterxml.jackson.databind.ObjectMapper
+import ujson.Value
 
 
-//Test flow
 // User self registers
 
 class Register extends Simulation {
 
     val config = new BenchConfig()
-    val amAuth = new AMAuth(config)
 
-    var userFeeder: Array[Map[String,String]] =
-      (1 to config.userPoolSize).toArray map ( x => { Map( "username" ->  x.toString) })
+    val userFeeder: Array[Map[String,String]] =
+        (0 to config.userPoolSize).toArray map ( x => { Map( 
+            "username" ->  (config.userPrefix+x).toString, "password" ->  config.userPassword.toString)
+        })
 
     val registrationUrl = config.amUrl + 
-            "/json/realms/root/authenticate?service=Registration&authIndexType=service&authIndexValue=Registration"
+        "/json/realms/root/authenticate?service=Registration&authIndexType=service&authIndexValue=Registration"
 
     val httpProtocol: HttpProtocolBuilder = http
         .baseUrls(config.idmUrl)
@@ -47,73 +49,43 @@ class Register extends Simulation {
           .body(StringBody(""))
           .check(status.is(200))
           .check(jsonPath("$.authId").find.saveAs("authId"))
+          .check(jsonPath("$").find.saveAs("callbacks"))
          )
     }
 
-    def registrationCallback(authId: String, username: String, password: String,
-                           requestName: String = "registationCallback"): ChainBuilder = {
+    def registrationCallback(requestName: String = "registationCallback"): ChainBuilder = {
       exec(http(requestName)
         .post(registrationUrl)
         .asJson
-        .body(StringBody(
-          """{"authId":"%s",""".format(authId) +
-            """"callbacks":[{"type":"ValidatedCreateUsernameCallback","output":[{"name":"policies","value":[{"policyId":"unique"},""" +
-            """{"policyId":"no-internal-user-conflict"},{"policyId":"cannot-contain-characters","params":{"forbiddenChars":["/"]}}]},""" +
-            """{"name":"failedPolicies","value":[]},{"name":"validateOnly","value":false},{"name":"prompt","value":"Username"}],""" +
-            """"input":[{"name":"IDToken1","value":"%s"},{"name":"IDToken1validateOnly","value":false}],"_id":0},""".format(username) +
-            """{"type":"StringAttributeInputCallback","output":[{"name":"name","value":"givenName"},""" +
-            """{"name":"prompt","value":"First Name"},{"name":"required","value":true},{"name":"policies","value":[]},""" +
-            """{"name":"failedPolicies","value":[]},{"name":"validateOnly","value":false},{"name":"value","value":""}],""" +
-            """"input":[{"name":"IDToken2","value":"%s"},{"name":"IDToken2validateOnly","value":false}],"_id":1},""".format(username) +
-            """{"type":"StringAttributeInputCallback","output":[{"name":"name","value":"sn"},{"name":"prompt","value":"Last Name"},""" +
-            """{"name":"required","value":true},{"name":"policies","value":[]},{"name":"failedPolicies","value":[]},""" +
-            """{"name":"validateOnly","value":false},{"name":"value","value":""}],"input":[{"name":"IDToken3","value":"%s"},""".format(username) +
-            """{"name":"IDToken3validateOnly","value":false}],"_id":2},{"type":"StringAttributeInputCallback",""" +
-            """"output":[{"name":"name","value":"mail"},{"name":"prompt","value":"Email Address"},{"name":"required","value":true},""" +
-            """{"name":"policies","value":[{"policyId":"valid-email-address-format"}]},{"name":"failedPolicies","value":[]},""" +
-            """{"name":"validateOnly","value":false},{"name":"value","value":""}],"input":[{"name":"IDToken4","value":"%s@gmail.com"},""".format(username) +
-            """{"name":"IDToken4validateOnly","value":false}],"_id":3},{"type":"BooleanAttributeInputCallback",""" +
-            """"output":[{"name":"name","value":"preferences/marketing"},{"name":"prompt","value":"Send me special offers and services"},""" +
-            """{"name":"required","value":true},{"name":"policies","value":[]},{"name":"failedPolicies","value":[]},""" +
-            """{"name":"validateOnly","value":false},{"name":"value","value":false}],"input":[{"name":"IDToken5","value":false},""" +
-            """{"name":"IDToken5validateOnly","value":false}],"_id":4},{"type":"BooleanAttributeInputCallback",""" +
-            """"output":[{"name":"name","value":"preferences/updates"},{"name":"prompt","value":"Send me news and updates"},""" +
-            """{"name":"required","value":true},{"name":"policies","value":[]},{"name":"failedPolicies","value":[]},""" +
-            """{"name":"validateOnly","value":false},{"name":"value","value":false}],"input":[{"name":"IDToken6","value":false},""" +
-            """{"name":"IDToken6validateOnly","value":false}],"_id":5},{"type":"ValidatedCreatePasswordCallback",""" +
-            """"output":[{"name":"echoOn","value":false},{"name":"policies","value":[{"policyId":"at-least-X-capitals",""" +
-            """"params":{"numCaps":1}},{"policyId":"at-least-X-numbers","params":{"numNums":1}},""" +
-            """{"policyId":"cannot-contain-others","params":{"disallowedFields":["userName","givenName","sn"]}}]},""" +
-            """{"name":"failedPolicies","value":[]},{"name":"validateOnly","value":false},{"name":"prompt","value":"Password"}],""" +
-            """"input":[{"name":"IDToken7","value":"%s"},{"name":"IDToken7validateOnly","value":false}],"_id":6},""" .format(password) +
-            """{"type":"KbaCreateCallback","output":[{"name":"prompt","value":"Select a security question"},""" +
-            """{"name":"predefinedQuestions","value":["What's your favorite color?","Who was your first employer?"]}],""" +
-            """"input":[{"name":"IDToken8question","value":"What's your favorite color?"},{"name":"IDToken8answer","value":"red"}],"_id":7},""" +
-            """{"type":"KbaCreateCallback","output":[{"name":"prompt","value":"Select a security question"},""" +
-            """{"name":"predefinedQuestions","value":["What's your favorite color?","Who was your first employer?"]}],""" +
-            """"input":[{"name":"IDToken9question","value":"Who was your first employer?"},{"name":"IDToken9answer","value":"forgerock"}],"_id":8},""" +
-            """{"type":"TermsAndConditionsCallback","output":[{"name":"version","value":"0.0"},""" +
-            """{"name":"terms","value":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},{"name":"createDate","value":"2019-10-28T04:20:11.320Z"}],""" +
-            """"input":[{"name":"IDToken10","value":true}],"_id":9}],""" +
-            """"header":"Sign Up","description":"Signing up is fast and easy.<br>Already have an account? <a href='#/service/Login'>Sign In</a>"}"""
-        ) )
+        .body(StringBody("${callbacks}"))
         .check(status.is(200))
-        .check(jsonPath("$.tokenId").find.saveAs("tokenId"))
-      )
-        .exec(addCookie(Cookie(config.loginCookie, "${tokenId}")))
-    }
+        .check(jsonPath("$.tokenId")) // Presence of the tokenId in the body indicates success
+    )}
 
-    val scn: ScenarioBuilder = scenario("Managed User Self Service")
+    val scn: ScenarioBuilder = scenario("Managed User Registration")
       .during (config.duration) {
         feed(userFeeder)
           .exec(flushCookieJar)
           .exec(registrationTreeInitiate())
-          .exec(registrationCallback(authId = "${authId}", config.userPrefix+"${username}", config.userPassword))
+          .exec(session => {
+            var data = ujson.read(session("callbacks").as[String])
+            data("authId") = session("authId").as[String]
+            data("callbacks")(0)("input")(0)("value") = session("username").as[String]
+            data("callbacks")(1)("input")(0)("value") = session("username").as[String]
+            data("callbacks")(2)("input")(0)("value") = session("username").as[String]
+            data("callbacks")(3)("input")(0)("value") = session("username").as[String]+"@fogerock.com"
+            data("callbacks")(4)("input")(0)("value") = false
+            data("callbacks")(5)("input")(0)("value") = false
+            data("callbacks")(6)("input")(0)("value") = session("password").as[String]
+            data("callbacks")(7)("input")(0)("value") = "red"
+            data("callbacks")(8)("input")(0)("value") = "forgerock"
+            data("callbacks")(9)("input")(0)("value") = true
+            session.set("callbacks", ujson.write(data))
+          })
+          .exec(registrationCallback())
+
     }
 
     setUp(scn.inject(rampUsers(config.concurrency) during config.warmup)).protocols(httpProtocol)
 
 }
-
-
-
