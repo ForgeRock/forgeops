d56f04ac85f901a8db68a6d92e2a786828943635
Jhonattan Castillo <jhonattan.castillo@forgerock.com>
2020-10-16 13:45:41 -0700

    feat: Improve reliability of the nightly pipelines
    1. Improve reliability of the nightly pipelines
    1. Removed unnecessary step to scale AM pods to zero in AM scale up task
    1. Rename  `tasks` to `task`. Fixes compatibility issue with vscode
    
    ref: CLOUD-2646

--- cicd/tekton/smoke/run.sh
+++ cicd/tekton/smoke/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Utility to run the smoke test
-kubectl -n tekton-pipelines apply -f shared/tasks/wait-platform-up.yaml
-kubectl -n tekton-pipelines apply -f shared/tasks/smoke-test-task.yaml
+kubectl -n tekton-pipelines apply -f shared/task/wait-platform-up.yaml
+kubectl -n tekton-pipelines apply -f shared/task/smoke-test-task.yaml
 kubectl -n tekton-pipelines  apply -f smoke/smoke-pipeline.yaml
 kubectl -n tekton-pipelines  apply -f smoke/smoke-quick.yaml
 kubectl -n tekton-pipelines apply -f nightly/nightly-pipeline.yaml
