6ff285ea118db9faaf4251ba411d9a5244562195
Jhonattan Castillo <jhonattan.castillo@forgerock.com>
2020-10-28 09:01:51 -0700

    fix: Only install secret-agent if not present
    1. Only install secret-agent if not previously installed

--- bin/secret-agent.sh
+++ bin/secret-agent.sh
@@ -4,7 +4,22 @@ SECRET_AGENT_VERSION=${SECRET_AGENT_VERSION:-latest}
 USAGE="Usage: $0 install|remove"
 
 install() {
-    kubectl apply -f https://github.com/ForgeRock/secret-agent/releases/${SECRET_AGENT_VERSION}/download/secret-agent.yaml
+    printf "Checking secret-agent operator and related CRDs: "
+    if ! $(kubectl get crd secretagentconfigurations.secret-agent.secrets.forgerock.io &> /dev/null); then
+        printf "secret-agent not found. Installing secret-agent version: '${SECRET_AGENT_VERSION}'\n"
+        if [ "$SECRET_AGENT_VERSION" == "latest" ]; then
+            kubectl apply -f "https://github.com/ForgeRock/secret-agent/releases/latest/download/secret-agent.yaml"
+        else
+            kubectl apply -f "https://github.com/ForgeRock/secret-agent/releases/download/${SECRET_AGENT_VERSION}/secret-agent.yaml"
+        fi
+        echo "Waiting for secret agent operator..."
+        sleep 5
+        kubectl wait --for=condition=Established crd secretagentconfigurations.secret-agent.secrets.forgerock.io --timeout=30s
+        kubectl -n secret-agent-system wait --for=condition=available deployment  --all --timeout=60s 
+        kubectl -n secret-agent-system wait --for=condition=ready pod --all --timeout=60s
+    else
+        printf "secret-agent CRD found in cluster. Skipping secret-agent installation.\n"
+    fi
 }
 
 remove() {
