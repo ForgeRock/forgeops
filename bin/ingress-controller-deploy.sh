4255efdfbc9bf6d132c5a54f5327fb6a4c89c39b
lbaines <lee.baines@forgerock.com>
2020-12-09 14:47:16 +0000

    chore: Use longtail options in Ingress Controller script
    Switch to longtail options.
    Clean up script.
    
    ref: CLOUD-2028

--- bin/ingress-controller-deploy.sh
+++ bin/ingress-controller-deploy.sh
@@ -8,51 +8,57 @@
 IP_OPTS=""
 
 usage() {
-  printf "Usage: $0 -e|-g|-a [-i IP] [-d] \n\n"
-  exit 2
+    echo -e "\nUsage: $0 [-g|--gke <ip-address>] [-e|--eks] [-a|--aks] [-d|delete] [-h|--help]\n"
+    echo -e "\t -g, --gke:      -   deploy to GKE. Optionally provide IP address (default: dynamically generate IP address)"
+    echo -e "\t -e, --eks:      -   deploy to EKS"
+    echo -e "\t -a, --aks:      -   deploy to AKS"
+    echo -e "\t -d, --delete:   -   delete ingress controller"
+    echo -e "\t -h, --help:     -   display options"
+
+    exit 2
 }
 
+# Delete Helm chart
 delete() {
     helm uninstall ingress-nginx --namespace nginx || true
     exit 1
 }
 
+# If an argument is provided, ensure that it is either delete or an IP address for GKE
+if [[ $# > 0 ]]; then
+    case $1 in
+        -d|--delete)
+            echo "Deleting Ingress Controller Helm chart."
+            delete
+        ;;
+        -g|--gke)
+            if [[ "$#" == 2 ]]; then
+                # Check IP address format and don't continue if not valid.
+                [[ ! "$2" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]] && echo -e "\nERROR: Can't detect a valid IP address" && usage
 
-# Output help if no arguments or -h is included
-if [[ $1 == "-h" ]];then
-    printf "\nUsage: $0 -e|-g|-a [-i IP] [-d]\n"
-    echo "-e                        : Deploy to EKS."
-    echo "-g                        : Deploy to GKE."
-    echo "-a                        : Deploy to AKS."
-    echo "-i  <static IP address>   : Provide an existing static IP address(GKE only)"
-    echo "-d                        : delete nginx-ingress Helm chart."
-    exit 1
-fi
-
-# Read arguments
-while getopts :aegc:i:d option; do
-    case "${option}" in
-        e) PROVIDER="eks";;
-        g) PROVIDER="gke";;
-        a) PROVIDER="aks";;
-        i) IP=${OPTARG};;
-        d) delete;;
-        h) usage ;;
-        \?) echo "Error: Incorrect usage"
+                # Set the loadBalancerIP annotation.
+                echo "IP: $2"
+                IP_OPTS="--set controller.service.loadBalancerIP=${2}"
+            fi
+            echo -e "Deploying Ingress Controller to GKE...\n"
+            PROVIDER="GKE"
+        ;;
+        -e|--eks)
+            echo -e "Deploying Ingress Controller to EKS...\n"
+            PROVIDER="EKS"
+        ;;
+        -a|--aks)
+            echo -e "Deploying Ingress Controller to AKS...\n"
+            PROVIDER="AKS"
+        ;;
+        -h|--help)
             usage
-            exit 1;;
+        ;;
+        *)
+            usage
+        ;;
     esac
-done
-
-#******** VALIDATING ARGUMENTS ********
-# Ensure provider flag has been provided.
-[ -z "${PROVIDER}" ] && printf "\n** Please provide a provider flag (-g|-e|-a) **\n\n" && usage
-# If -g selected with -i then validate IP address format.
-[[ "${PROVIDER}" =~ ^(gke)$ ]] && [ "${IP}" ] && [[ ! "${IP}" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]] && printf '\n** IP address is not valid **\n\n' && usage
-# If -g selected with -i then set loadbalancer IP field.
-[[ "${PROVIDER}" =~ ^(gke)$ ]] && [ "${IP}" ] && IP_OPTS="--set controller.service.loadBalancerIP=${IP}"
-# If -e or -a is selected with -i, echo that IP address will be ignored
-[[ "${PROVIDER}" =~ ^(eks|aks)$ ]] && [ "${IP}" ] && printf "\n** IP address not required so ignoring **\n\n" && usage
+fi
 
 # Create namespace
 ns=$(kubectl get namespace | grep nginx | awk '{ print $1 }' || true)
@@ -61,13 +67,15 @@ ns=$(kubectl get namespace | grep nginx | awk '{ print $1 }' || true)
 clustsize=$(kubectl get nodes --label-columns forgerock.io/cluster --no-headers  | head -n 1 | awk '{print $6}')
 
 if [ -z "${ns}" ]; then
-  kubectl create namespace nginx
+    kubectl create namespace nginx
 else
-  printf "*** nginx namespace already exists ***\n"
+    printf "*** nginx namespace already exists ***\n"
 fi
+
 if [ -n "$clustsize" ]; then
     echo "Detected cluster of type: $clustsize"
 fi 
+
 if [[ -n "$clustsize" && ($clustsize == "cdm-medium" || $clustsize == "cdm-large") ]]; then 
     echo "Setting ingress pod count to 3"
     INGRESS_POD_COUNT=3
@@ -89,13 +97,4 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx --force-u
 
 # Deploy ingress controller Helm chart
 helm upgrade -i ingress-nginx --namespace nginx ingress-nginx/ingress-nginx \
-  $IP_OPTS -f ${ADDONS_DIR}/${PROVIDER}.yaml --set controller.replicaCount=${INGRESS_POD_COUNT}
-
-# This other repo requires changes to the values in cluster/addons/nginx-ingress-controller
-# We're using `stable`, but need to explore if we should move to this other one. See CLOUD-2426
-# # Add Helm repo
-# helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx/ > /dev/null
-
-# # Deploy ingress controller Helm chart
-# helm upgrade -i nginx-ingress --namespace nginx ingress-nginx/ingress-nginx \
-#   $IP_OPTS -f ${ADDONS_DIR}/${PROVIDER}.yaml
+    $IP_OPTS -f ${ADDONS_DIR}/${PROVIDER}.yaml --set controller.replicaCount=${INGRESS_POD_COUNT}
