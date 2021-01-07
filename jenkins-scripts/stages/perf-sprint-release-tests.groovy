eb4c775a08f53afe2c4b5070b17fe022b10c764c
Narita Saxena <narita.saxena@forgerock.com>
2021-01-07 09:35:07 +0000

    fix: enabling access token long duration test for sprint testing

--- jenkins-scripts/stages/perf-sprint-release-tests.groovy
+++ jenkins-scripts/stages/perf-sprint-release-tests.groovy
@@ -54,19 +54,20 @@ void runStage(PipelineRun pipelineRun) {
                     }
 
                     // perf am access token test
-                    //subStageName = 'am_access_token_long'
-                    //stagesCloud[subStageName] = dashboard_utils.pyrockStageCloud('access_token')
-//
-                    //dashboard_utils.determineUnitOutcome(stagesCloud[subStageName]) {
-                    //    def config = config_common.clone()
-                    //    config += [
-                    //        TEST_NAME       : "access_token",
-                    //        BASELINE_RPS    : '[3075,3115]',
-                    //        SET_OPTIONS     : "--set phases['scenario'].duration=6 --set phases['scenario'].duration-unit=h --set components.servers['ds-idrepo'].num-entries=10000000",
-                    //    ]
-//
-                    //    withGKEPyrockNoStages(config)
-                    //}
+                    subStageName = 'am_access_token_long'
+                    stagesCloud[subStageName] = dashboard_utils.pyrockStageCloud('access_token')
+
+                    dashboard_utils.determineUnitOutcome(stagesCloud[subStageName]) {
+                        def config = config_common.clone()
+                        config += [
+                            TEST_NAME                  : "access_token",
+                            DEPLOYMENT_RESTOREBUCKETURL: 'gs://performance-bucket-us-east1/nemanja/3ds-10M-bis',
+                            DEPLOYMENT_MAKEBACKUP      : false,
+                            SET_OPTIONS                : "--set phases['scenario'].duration=6 --set phases['scenario'].duration-unit=h --set components.servers['ds-idrepo'].num-entries=10000000",
+                        ]
+
+                        withGKEPyrockNoStages(config)
+                    }
 
                     //// perf platform test
                     //subStageName = 'platform_long'
