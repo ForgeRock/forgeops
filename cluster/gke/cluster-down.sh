b7085f95415d75da00d7727a34c6131c8d456a97
Warren Strange <warren.strange@forgerock.com>
2021-01-22 14:33:16 -0700

    chore: Clean up PVC disks before deleting the cluster
    add read

--- cluster/gke/cluster-down.sh
+++ cluster/gke/cluster-down.sh
@@ -15,8 +15,18 @@ REGION=${REGION:-$R}
 
 ZONE=${ZONE:-"$REGION-a"}
 
+
+echo "About to delete the cluster $NAME in zone $ZONE, and ALL data. Press any key to continue, or control-c to quit"
+read;
+
+
+
 echo "Getting the cluster credentials for $NAME in Zone $ZONE"
 gcloud container clusters get-credentials "$NAME" --zone "$ZONE" || exit 1
+
+echo "About to delete the cluster in 10 seconds. YOU WILL LOSE ALL DATA! Control-c now to stop this"
+
+sleep 10
 
 # Try to release any L4 service load balancers
 echo "Cleaning up the ngnix load balancer. Ignore any errors below"
@@ -24,7 +34,11 @@ kubectl -n nginx delete deployment --all
 kubectl -n nginx delete svc --all
 kubectl delete ns nginx
 
-echo "About to delete the cluster in 10 seconds"
+
+echo "Deleting all PVC disks in 5 seconds... YOU WILL LOSE ALL DATA. Control-c now to stop this"
+sleep 5
+kubectl delete pvc --all-namespaces --all
+
 sleep 10
 # Delete the cluster. Defaults to the current project.
 gcloud container clusters delete "$NAME" --zone "$ZONE"
@@ -34,3 +48,5 @@ if [ "$DELETE_STATIC_IP" == true ]; then
   echo "Deleting static IP ${STATIC_IP_NAME}..."
   gcloud compute addresses delete "$STATIC_IP_NAME" --project "$PROJECT" --region "$REGION"
 fi
+
+echo "Check your GCP console for any orphaned project resources such as disks!"
