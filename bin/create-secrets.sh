4a86076e3a4fc4b520f8c0f40b6f757d101ddbbc
Max Resnick <max.resnick@forgerock.com>
2021-03-01 15:43:34 -0800

    feat: adds a script to deploy SAC with retry and backoff
    ref: CLOUD-2961

---
+++ bin/create-secrets.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+SCRIPT_NAME="$(basename "$0")"
+
+usage () {
+read -r -d '' help <<-EOF
+
+Create ForgeOps Secrets
+
+This script deploys the SecretAgentConfiguration in a manner that's robust
+against busy clusters.
+
+Usage:  ${SCRIPT_NAME} NAMESPACE
+
+EOF
+    printf "%-10s \n" "$help"
+}
+
+# arg check
+if [[ "$#" -ne 1 ]]; then
+    echo "Missing required argument"
+    usage
+    exit 1
+fi
+
+ns=$1
+i=0
+
+while [ $i -le  30 ];
+do
+    if kubectl --namespace "${ns}" apply --filename=kustomize/base/secrets/secret_agent_config.yaml > /dev/null 2>&1;
+    then
+        break
+    fi
+    echo "deploying secret agent configuration failed, trying again"
+    sleepTime=$(( $i * 2 ))
+    sleep $sleepTime
+
+    i=$(( $i + 1 ))
+done
