47c6b7910740d7de0c7c3a960ffc205f04793581
Christophe Sovant <christophe.sovant@forgerock.com>
2021-03-11 19:47:21 +0100

    task: PIT2 Upgrade: remove dependency on image tag 'fraas-production'
    ref: LODESTAR-628

--- jenkins-scripts/stages/pit2-upgrade-tests.groovy
+++ jenkins-scripts/stages/pit2-upgrade-tests.groovy
@@ -12,7 +12,6 @@ void runStage(PipelineRunLegacyAdapter pipelineRun) {
 
     def stageName = 'PIT2 Binary Upgrade'
     def normalizedStageName = dashboard_utils.normalizeStageName(stageName)
-    def fraasTag = 'fraas-production'
 
     pipelineRun.pushStageOutcome(normalizedStageName, stageDisplayName: stageName) {
         node('pit2-upgrade') {
@@ -25,20 +24,14 @@ void runStage(PipelineRunLegacyAdapter pipelineRun) {
                     def config_common = [
                         CLUSTER_DOMAIN                                  : 'pit-24-7.forgeops.com',
                         CLUSTER_NAMESPACE                               : initialSubStageName,
-                        COMPONENTS_AMSTER_IMAGE_TAG                     : fraasTag,
-                        COMPONENTS_AM_IMAGE_TAG                         : fraasTag,
                         COMPONENTS_AM_IMAGE_UPGRADE_TAG                 : commonModule.getCurrentTag('am'),
                         COMPONENTS_AM_IMAGE_UPGRADE_REPOSITORY          : 'gcr.io/forgerock-io/am-base/pit1',
-                        COMPONENTS_IDM_IMAGE_TAG                        : fraasTag,
                         COMPONENTS_IDM_IMAGE_UPGRADE_TAG                : commonModule.getCurrentTag('idm'),
                         COMPONENTS_IDM_IMAGE_UPGRADE_REPOSITORY         : 'gcr.io/forgerock-io/idm/pit1',
-                        COMPONENTS_IG_IMAGE_TAG                         : fraasTag,
                         COMPONENTS_IG_IMAGE_UPGRADE_TAG                 : commonModule.getCurrentTag('ig'),
                         COMPONENTS_IG_IMAGE_UPGRADE_REPOSITORY          : 'gcr.io/forgerock-io/ig/pit1',
-                        COMPONENTS_DSIDREPO_IMAGE_TAG                   : fraasTag,
                         COMPONENTS_DSIDREPO_IMAGE_UPGRADE_TAG           : commonModule.getCurrentTag('ds-idrepo'),
                         COMPONENTS_DSIDREPO_IMAGE_UPGRADE_REPOSITORY    : 'gcr.io/forgerock-io/ds/pit1',
-                        COMPONENTS_DSCTS_IMAGE_TAG                      : fraasTag,
                         COMPONENTS_DSCTS_IMAGE_UPGRADE_TAG              : commonModule.getCurrentTag('ds-cts'),
                         COMPONENTS_DSCTS_IMAGE_UPGRADE_REPOSITORY       : 'gcr.io/forgerock-io/ds/pit1',
                         STASH_LODESTAR_BRANCH                           : commonModule.LODESTAR_GIT_COMMIT,
