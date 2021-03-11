2fa41ff899210102cc1f3ff581605b2b9e68f3a5
Christophe Sovant <christophe.sovant@forgerock.com>
2021-03-11 15:42:53 +0100

    task: Ability to select which PIT2 tests to run depending on changes
    ref: PIT-343

--- jenkins-scripts/stages/pit2-all-tests.groovy
+++ jenkins-scripts/stages/pit2-all-tests.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 ForgeRock AS. All Rights Reserved
+ * Copyright 2019-2021 ForgeRock AS. All Rights Reserved
  *
  * Use of this code requires a commercial software license with ForgeRock AS.
  * or with one of its affiliates. All use shall be exclusively subject
@@ -7,11 +7,17 @@
  */
 
 void runStage(pipelineRun) {
-    def parallelTestsMap = [
-        Greenfield: { greenfieldTests.runStage(pipelineRun) },
-        Upgrade: { upgradeTests.runStage(pipelineRun) },
-        Perf: { perfTests.runStage(pipelineRun) },
-    ]
+    def parallelTestsMap = [:]
+
+    if (params.PIT2_Greenfield.toInteger() > 0) {
+        parallelTestsMap.put('Greenfield', { greenfieldTests.runStage(pipelineRun) })
+    }
+    if (params.PIT2_Upgrade.toBoolean()) {
+        parallelTestsMap.put('Upgrade', { upgradeTests.runStage(pipelineRun) })
+    }
+    if (env.getEnvironment().any { name, value -> name.startsWith('PIT2_Perf') && value.toBoolean() }) {
+        parallelTestsMap.put('Perf', { perfTests.runStage(pipelineRun) })
+    }
 
     parallel parallelTestsMap
 }
