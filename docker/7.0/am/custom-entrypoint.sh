f2d9092020bd8bcaed201871c57cc74e43df489a
Jhonattan Castillo <jhonattan.castillo@forgerock.com>
2021-03-31 16:24:09 -0700

    feat: Combine java cacerts and platform-ca during init
    1. New init container for am that combines java cacerts and platform-ca
    1. am-cdk is now an standalone kustomize profile
    1. Change AM directory protocol to LDAPS
    1. Change AM directory port config to 1636
    1. Use `pemFormat` for the cacerts truststore generated by secret-agent
    
    ref: CLOUD-2925

--- docker/7.0/am/custom-entrypoint.sh
+++ docker/7.0/am/custom-entrypoint.sh
@@ -1,24 +1,55 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# This script copies the default cacerts to $TRUSTSTORE_PATH 
+# and imports all the certs contained in the $SOURCE_PEM_FILE if it exists
 
 #
-# Copyright 2019-2020 ForgeRock AS. All Rights Reserved
+# Copyright 2019-2021 ForgeRock AS. All Rights Reserved
 #
 
+set -e 
+set -o pipefail
 
-am-crypto() {
-    java -jar /home/forgerock/crypto-tool.jar $@
-}
+# SOURCE_PEM_FILE="${SOURCE_PEM_FILE:-/var/run/secrets/truststore/cacerts}"
+export TRUSTSTORE_PATH="${TRUSTSTORE_PATH:-/home/forgerock/amtruststore}"
+export TRUSTSTORE_PASSWORD="${TRUSTSTORE_PASSWORD:-changeit}"
 
-# Additiomal place holder
+SOURCE_TRUSTSTORE=${JAVA_HOME}/lib/security/cacerts
 
 
-export AM_STORES_SSL_ENABLED=false
-export TRUSTSTORE_PATH=/var/run/secrets/truststore/cacerts
-export TRUSTSTORE_PASSWORD=changeit
+if [ ! -f "$SOURCE_TRUSTSTORE" ]; then
+    echo "$SOURCE_TRUSTSTORE is not a valid file or does not exist."
+    echo "check the value of \$SOURCE_TRUSTSTORE and restart the pod"
+    exit 1
+fi
 
-# Let AM docker base generat this
-# export AM_SELFSERVICE_LEGACY_CONFIRMATION_EMAIL_LINK_SIGNING_KEY=$(echo -n "$AM_SELFSERVICE_LEGACY_CONFIRMATION_EMAIL_LINK_SIGNING_KEY" | base64)
+echo "Copying ${SOURCE_TRUSTSTORE} to ${TRUSTSTORE_PATH}"
+cp ${SOURCE_TRUSTSTORE} ${TRUSTSTORE_PATH}
 
-export AM_STORES_USER_TYPE=LDAPv3ForForgeRockIAM
+# If a $SOURCE_PEM_FILE is provice, import it into the truststore. Otherwise, use the trustore as-is
+echo "Attempting to import PEM File into truststore"
+if [ ! -f "$SOURCE_PEM_FILE" ]; then
+    echo "$SOURCE_PEM_FILE is not a valid file or does not exist."
+    echo "Will use ${TRUSTSTORE_PATH} as-is. No import will be done"
+else
+    echo "Found: ${SOURCE_PEM_FILE}"
+    # Calculate the number of certs in the PEM file
+    CERTS=$(grep 'END CERTIFICATE' $SOURCE_PEM_FILE| wc -l)
+    echo "Found (${CERTS}) certificates in the provided PEM file."
+    echo "Will import certs into truststore: ${TRUSTSTORE_PATH}"
+    # For every cert in the PEM file, extract it and import into the JKS truststore
+    for N in $(seq 0 $(($CERTS - 1))); do
+    ALIAS="${SOURCE_PEM_FILE%.*}-$N"
+    cat $SOURCE_PEM_FILE |
+        awk "n==$N { print }; /END CERTIFICATE/ { n++ }" |
+        keytool -noprompt -import -trustcacerts -storetype PKCS12 \
+                -alias "${ALIAS}" -keystore "${TRUSTSTORE_PATH}" \
+                -storepass "${TRUSTSTORE_PASSWORD}"
+    done
+    echo "Import complete!"    
+fi
+
+export CATALINA_OPTS="$CATALINA_OPTS -Djavax.net.ssl.trustStore=$TRUSTSTORE_PATH \
+                                     -Djavax.net.ssl.trustStorePassword=$TRUSTSTORE_PASSWORD \
+                                     -Djavax.net.ssl.trustStoreType=jks" 
 
 exec /home/forgerock/docker-entrypoint.sh
