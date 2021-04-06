db2403a08065d95c44c86b4259743d7eb5f9d0f6
Jhonattan Castillo <jhonattan.castillo@forgerock.com>
2021-04-06 15:12:27 -0700

    noop: Move the ingress manifests to kustomize/base
    Move the ingress manifests to kustomize/base

--- bin/quickstart.sh
+++ bin/quickstart.sh
@@ -137,7 +137,7 @@ deploylocalmanifest () {
     (cd kustomize/dev/image-defaulter && kustomize edit remove resource ../../../kustomize/*/*/* ../../../kustomize/*/*)
     case "${1}" in
     "base")
-        INSTALL_COMPONENTS=("dev/kustomizeConfig" "base/secrets" "base/7.0/ingress" "base/git-server" "dev/scripts")
+        INSTALL_COMPONENTS=("dev/kustomizeConfig" "base/secrets" "base/ingress" "base/git-server" "dev/scripts")
         ;;
     "ds")
         INSTALL_COMPONENTS=("base/ds-idrepo") #no "ds-cts" in dev-mode
