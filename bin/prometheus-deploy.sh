aaba90ef9a7690d78cb77b4f98289a5817dfa43c
Jhonattan Castillo <jhonattan.castillo@forgerock.com>
2021-04-23 11:27:42 -0700

    feat: Update Prometheus helm chart for Kubernetes 1.19+
    Update Prometheus helm chart for Kubernetes 1.19+

--- bin/prometheus-deploy.sh
+++ bin/prometheus-deploy.sh
@@ -34,7 +34,7 @@ deploy() {
     # Add stable repo to helm
     helm repo add "stable" "https://charts.helm.sh/stable" --force-update
     helm repo add "prometheus-community" "https://prometheus-community.github.io/helm-charts" --force-update
-
+    helm repo update
     helm upgrade -i prometheus-operator prometheus-community/kube-prometheus-stack  -f $PROM_VALUES --namespace=$NAMESPACE
 
     kubectl -n $NAMESPACE wait --for condition=established --timeout=60s \
