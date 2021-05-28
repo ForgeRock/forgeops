f558e3d742787abfb361f782173bd68154e26fb6
Jhonattan Castillo <jhonattan.castillo@forgerock.com>
2021-05-27 18:19:55 -0700

    fix: Skip boot.json during AM config export
    Skip boot.json during AM config export

--- docker/7.0/am/export.sh
+++ docker/7.0/am/export.sh
@@ -6,4 +6,4 @@ cd /home/forgerock/openam
 # Pass `-` as the argument to output the tar stream to stdout. Use kubectl exec am-pod -- export.sh - > tar.out
 dest=${1:-"/home/forgerock/updated-config.tar"}
 
-tar -c config -f $dest
+tar --exclude boot.json  -c config -f $dest
