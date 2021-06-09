d2db7ab20b7fc614a0dadf6cb5cf2e50f8e1b0ce
Christophe Sovant <christophe.sovant@forgerock.com>
2021-06-09 14:38:44 +0200

    fix: (PIT2) Execute PIT2 perf on a separate node to avoid conflicts

--- jenkins-scripts/stages/pit2-perf-tests.groovy
+++ jenkins-scripts/stages/pit2-perf-tests.groovy
@@ -11,85 +11,85 @@
 import com.forgerock.pipeline.reporting.PipelineRunLegacyAdapter
 
 void runStage(PipelineRunLegacyAdapter pipelineRun) {
-    node('perf-cloud') {
-        def config_common = [
-                STASH_LODESTAR_BRANCH   : commonModule.LODESTAR_GIT_COMMIT,
-                STASH_FORGEOPS_BRANCH   : commonModule.FORGEOPS_GIT_COMMIT,
-                PIPELINE_NAME           : "ForgeOps-PIT2-promotion",
-                CHECK_REGRESSION        : true,
-                MAX_VARIATION           : '0.10',
-        ]
+    def config_common = [
+            STASH_LODESTAR_BRANCH   : commonModule.LODESTAR_GIT_COMMIT,
+            STASH_FORGEOPS_BRANCH   : commonModule.FORGEOPS_GIT_COMMIT,
+            PIPELINE_NAME           : "ForgeOps-PIT2-promotion",
+            CHECK_REGRESSION        : true,
+            MAX_VARIATION           : '0.10',
+    ]
 
-        def parentStageName = 'PIT2 Perf'
-        def tags = ['PIT2', 'performance']
-        def parallelTestsMap = [:]
+    def parentStageName = 'PIT2 Perf'
+    def tags = ['PIT2', 'performance']
+    def parallelTestsMap = [:]
 
-        // perf platform test
-        if (params.PIT2_Perf_platform) {
-            parallelTestsMap.put("${parentStageName} platform",
-                    {
-                        runPyrock(pipelineRun, "${parentStageName} platform", tags, config_common +
-                                [TEST_NAME   : 'platform',
-                                 BASELINE_RPS: '[1983,1722,1136,360]']
-                        )
-                    }
-            )
-        }
-
-        // perf am authn rest test
-        if (params.PIT2_Perf_am_authn) {
-            parallelTestsMap.put("${parentStageName} am_authn",
-                    {
-                        runPyrock(pipelineRun, "${parentStageName} am_authn", tags, config_common +
-                                [TEST_NAME   : 'authn_rest',
-                                 BASELINE_RPS: '2550']
-                        )
-                    }
-            )
-        }
-
-        // perf am access token test
-        if (params.PIT2_Perf_am_access_token) {
-            parallelTestsMap.put("${parentStageName} am_access_token",
-                    {
-                        runPyrock(pipelineRun, "${parentStageName} am_access_token", tags, config_common +
-                                [TEST_NAME   : 'access_token',
-                                 BASELINE_RPS: '[2733,2453]']
-                        )
-                    }
-            )
-        }
-
-        // perf IDM CRUD on simple managed users tests
-        if (params.PIT2_Perf_idm_crud) {
-            parallelTestsMap.put("${parentStageName} idm_crud",
-                    {
-                        runPyrock(pipelineRun, "${parentStageName} idm_crud", tags, config_common +
-                                [TEST_NAME   : 'simple_managed_users',
-                                 BASELINE_RPS: '[5688,0,0,0,1803,3977,1274,955]']
-                        )
-                    }
-            )
-        }
-
-        parallel parallelTestsMap
+    // perf platform test
+    if (params.PIT2_Perf_platform) {
+        parallelTestsMap.put("${parentStageName} platform",
+                {
+                    runPyrock(pipelineRun, "${parentStageName} platform", tags, config_common +
+                            [TEST_NAME   : 'platform',
+                             BASELINE_RPS: '[1983,1722,1136,360]']
+                    )
+                }
+        )
     }
+
+    // perf am authn rest test
+    if (params.PIT2_Perf_am_authn) {
+        parallelTestsMap.put("${parentStageName} am_authn",
+                {
+                    runPyrock(pipelineRun, "${parentStageName} am_authn", tags, config_common +
+                            [TEST_NAME   : 'authn_rest',
+                             BASELINE_RPS: '2550']
+                    )
+                }
+        )
+    }
+
+    // perf am access token test
+    if (params.PIT2_Perf_am_access_token) {
+        parallelTestsMap.put("${parentStageName} am_access_token",
+                {
+                    runPyrock(pipelineRun, "${parentStageName} am_access_token", tags, config_common +
+                            [TEST_NAME   : 'access_token',
+                             BASELINE_RPS: '[2733,2453]']
+                    )
+                }
+        )
+    }
+
+    // perf IDM CRUD on simple managed users tests
+    if (params.PIT2_Perf_idm_crud) {
+        parallelTestsMap.put("${parentStageName} idm_crud",
+                {
+                    runPyrock(pipelineRun, "${parentStageName} idm_crud", tags, config_common +
+                            [TEST_NAME   : 'simple_managed_users',
+                             BASELINE_RPS: '[5688,0,0,0,1803,3977,1274,955]']
+                    )
+                }
+        )
+    }
+
+    parallel parallelTestsMap
 }
 
 def runPyrock(PipelineRunLegacyAdapter pipelineRun, String stageName, ArrayList tags, Map config) {
     def normalizedStageName = dashboard_utils.normalizeStageName(stageName)
 
     pipelineRun.pushStageOutcome([tags: tags, stageDisplayName: stageName], normalizedStageName) {
-        stage(stageName) {
-            dir('lodestar') {
-                def stagesCloud = [:]
-                stagesCloud[normalizedStageName] = dashboard_utils.pyrockStageCloud(config.TEST_NAME)
+        node('perf-cloud') {
+            stage(stageName) {
+                dir('lodestar') {
+                    def stagesCloud = [:]
+                    stagesCloud[normalizedStageName] = dashboard_utils.pyrockStageCloud(config.TEST_NAME)
 
-                dashboard_utils.determineUnitOutcome(stagesCloud[normalizedStageName]) {
-                    withGKEPyrockNoStages(config)
+                    dashboard_utils.determineUnitOutcome(stagesCloud[normalizedStageName]) {
+                        withGKEPyrockNoStages(config)
+                    }
+
+                    return dashboard_utils.finalLodestarOutcome(stagesCloud, stageName)
                 }
-
-                return dashboard_utils.finalLodestarOutcome(stagesCloud, stageName)
             }
         }
     }
