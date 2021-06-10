7d47ceb58c9fc920448774acf1d02a8b50c981f1
Jhonattan Castillo <jhonattan.castillo@forgerock.com>
2021-06-10 14:59:37 -0700

    feat: Add support for config-profiles to `cdk` and `cdm`
    1. Add support for config-profiles to `cdk` and `cdm`
    1. Add CONFIG_PROFILE build arg to AM, IDM, AMSTER and IG Dockerfiles
    1.  `config.sh` init, export, save now copies config to `PRODUCT/config-profiles/temp`
    1. Use `temp` as the default config-profile to maintain backwards compatibility
    
    ref: CLOUD-3176

--- bin/utils.py
+++ bin/utils.py
@@ -83,13 +83,13 @@ def warning(s):
     print(f"{PURPLE}{s}{ENDC}")
 
 
-def run(cmd, *cmdArgs, stdin=None, cstdout=False, cstderr=False, cwd=None):
+def run(cmd, *cmdArgs, stdin=None, cstdout=False, cstderr=False, cwd=None, env=None):
     """rC runs a given command. Raises error if command returns non-zero code"""
     runcmd = f'{cmd} {" ".join(cmdArgs)}'
     stde_pipe = subprocess.PIPE if cstderr else None
     stdo_pipe = subprocess.PIPE if cstdout else None
     _r = subprocess.run(runcmd.split(), stdout=stdo_pipe, stderr=stde_pipe,
-                        check=True, input=stdin, cwd=cwd)
+                        check=True, input=stdin, cwd=cwd, env=env)
     return _r.returncode == 0, _r.stdout, _r.stderr
 
 
@@ -246,7 +246,7 @@ def dsoperator(k8s_op, tag='latest'):
         run('kubectl', '-n fr-system wait --for=condition=available deployment  --all --timeout=120s')
         run('kubectl', '-n fr-system wait --for=condition=ready pod --all --timeout=120s')
 
-def build_docker_image(component, default_repo):
+def build_docker_image(component, default_repo, config_profile=None):
     """Builds custom docker images. Returns the tag of the built image"""
     # Clean out the temp kustomize files
     base_dir = os.path.join(sys.path[0], '../')
@@ -255,7 +255,11 @@ def build_docker_image(component, default_repo):
         default_repo_cmd = f'--default-repo={default_repo}'
     else:
         default_repo_cmd = ''
-    run('skaffold', f'build -p {component} --file-output=tag.json {default_repo_cmd}', cwd=base_dir)
+    envVars = None
+    if config_profile:
+        envVars = os.environ
+        envVars['CONFIG_PROFILE'] = str(config_profile)
+    run('skaffold', f'build -p {component} --file-output=tag.json {default_repo_cmd}', cwd=base_dir, env=envVars)
     with open(os.path.join(base_dir, 'tag.json')) as tag_file:
         tag_data = json.load(tag_file)['builds'][0]["tag"]
     return tag_data
