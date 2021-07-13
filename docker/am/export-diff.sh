b3697c502d73a46e091af6bdf6a643494b6dffb2
Max Resnick <max.resnick@forgerock.com>
2021-07-13 16:04:03 -0700

    fix: export-diff exit 0 instead of 1 for if there are no changes.
    ./bin/config export and export-diff.sh in AM now cleanly terminate if no changes are found
    
    ref: cloud-3214

--- docker/am/export-diff.sh
+++ docker/am/export-diff.sh
@@ -10,7 +10,7 @@ cd /home/forgerock/openam
 git add -N config
 
 # Get a list of files that have changed. Prune boot.json as we can ignore.
-git diff --name-only | grep -v boot.json  >/var/tmp/export-list
+git diff --name-only | grep -v boot.json  >/var/tmp/export-list || exit 0
 
 # tar destination defaults to /home/forgerock/updated-config.tar
 # Pass `-` as the argument to output the tar stream to stdout. Use kubectl exec am-pod -- export.sh - > tar.out
