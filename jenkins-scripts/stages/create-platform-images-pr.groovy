0dbc2094bc9b455af2a811c6a991b51ac0eca588
Christophe Sovant <christophe.sovant@forgerock.com>
2021-10-03 13:41:36 +0200

    task: Use GCE VM node in Forgeops postcommit
    ref: LODESTAR-758

--- jenkins-scripts/stages/create-platform-images-pr.groovy
+++ jenkins-scripts/stages/create-platform-images-pr.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 ForgeRock AS. All Rights Reserved
+ * Copyright 2021-2022 ForgeRock AS. All Rights Reserved
  *
  * Use of this code requires a commercial software license with ForgeRock AS.
  * or with one of its affiliates. All use shall be exclusively subject
@@ -14,18 +14,16 @@ import com.forgerock.pipeline.stage.Status
 /** Open a pull request against the Platform-Images repo, containing latest ForgeOps commit */
 void runStage(PipelineRunLegacyAdapter pipelineRun) {
     pipelineRun.pushStageOutcome('create-platform-images-pr', stageDisplayName: 'Create Platform-Images PR') {
-        node('forgeops-postcommit-cloud') {
-            privateWorkspace {
-                def dockerProperties = [
-                        'gitCommit':            commonModule.GIT_COMMIT,
-                        'platformImagesCommit': commonModule.platformImagesRevision,
-                        'lodestarCommit':       commonModule.lodestarRevision,
-                ]
+        privateWorkspace {
+            def dockerProperties = [
+                    'gitCommit':            commonModule.GIT_COMMIT,
+                    'platformImagesCommit': commonModule.platformImagesRevision,
+                    'lodestarCommit':       commonModule.lodestarRevision,
+            ]
 
-                return platformImageUtils.createPlatformImagePR('forgeops', env.BRANCH_NAME, dockerProperties)
-                        ? Status.SUCCESS.asOutcome()
-                        : Status.SKIPPED.asOutcome()
-            }
+            return platformImageUtils.createPlatformImagePR('forgeops', env.BRANCH_NAME, dockerProperties)
+                    ? Status.SUCCESS.asOutcome()
+                    : Status.SKIPPED.asOutcome()
         }
     }
 }
