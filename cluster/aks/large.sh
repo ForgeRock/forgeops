0f50a5862b88cb56ba417b945c3871f705817ee6
shankar.raman <shankar.raman@forgerock.com>
2022-02-09 10:27:22 -0800

    Update the Kubernetes version to 1.21.7 in cluster creation / configuration shell scripts for AKS - in master branch
    CLOUD-3587

--- cluster/aks/large.sh
+++ cluster/aks/large.sh
@@ -6,7 +6,7 @@ export NAME=large
 export CLUSTER_LABELS="forgerock.io/cluster=cdm-large"
 
 # Override default Kubernetes version
-export K8S_VERSION="1.21.1"
+export K8S_VERSION="1.21.7"
 
 # cluster-up.sh retrieves the location from the user's az config.  Uncomment below to override:
 # export LOCATION=eastus
