175b715032d722ab773a2789c22a7a42d013d2be
Lee Baines <lee.baines@forgerock.com>
2022-03-18 11:26:54 +0000

    chore: Fix commands and move to bin folder
    Fix the commands as they no longer work.
    Add help.
    Move to bin folder.
    
    ref: CLOUD-3635

--- bin/ds-debug.sh
+++ bin/ds-debug.sh
@@ -2,19 +2,20 @@
 # Utility script to run commands inside a ds container
 # This is an internal script provided for debugging purposes only and is not supported by ForgeRock.
 
-if [ "$#" -lt 1 ]; then
+usage() {
   echo "Usage: $0 status|rstatus|disaster|monitor|list-backups|purge [pod name]"
   exit 1
-fi
-# Get the uid=admin password
-pw=$(kubectl get secret ds-passwords -o jsonpath="{.data.dirmanager\\.pw}" | base64 --decode)
+}
 
-pwd_args="-w $pw"
+if [ "$#" -lt 1 ]; then
+  usage
+fi
+
 cmd="$1"
 
 BACKUP_LOCATION="gs://ds-operator-engineering-devops/ds-backup-test"
 
-HOST="ds-0"
+HOST="ds-idrepo-0"
 
 if [ ! -z "$2" ]; then
   HOST="$2"
@@ -27,13 +28,18 @@ for b in $backends; do
   dr_args="$dr_args --baseDN $b"
 done
 
+setArgs() {
+  pw=$(kubectl get secret ds-passwords -o jsonpath="{.data.${1}\\.pw}" | base64 --decode)
+  args="-w $pw -p $2"
+}
+
 disaster() {
   echo "Running disaster recovery procedure to reset change log db for $*"
   echo "Starting in 5 seconds. Kill this NOW if you dont want to lose your changelog!"
   sleep 5
-  kcmd  dsrepl start-disaster-recovery -X $pwd_args $*
+  kcmd  dsrepl start-disaster-recovery -X $args $*
   echo "About to run the end DR command..."
-  kcmd  dsrepl end-disaster-recovery -X  $pwd_args $*
+  kcmd  dsrepl end-disaster-recovery -X  $args $*
 }
 
 kcmd() {
@@ -43,33 +49,49 @@ kcmd() {
 
 case "$cmd"  in
 status)
- kcmd status $pwd_args
+  # Display basic server information
+  setArgs dirmanager 4444
+  kcmd status $args
   ;;
 disaster)
-  disaster $dr_args
+  # Run disaster recovery
+  setArgs dirmanager 4444
+  disaster $args
   ;;
 rstatus)
-  kcmd dsrepl status --showReplicas --showChangeLogs $pwd_args
+  # Check replication status
+  setArgs monitor 4444
+  kcmd dsrepl status --showReplicas --showChangeLogs $args
   ;;
 idsearch)
-  kcmd ldapsearch $pwd_args --baseDN ou=identities "(objectclass=*)"
+  # List identities
+  setArgs dirmanager 1389
+  kcmd ldapsearch -D "uid=admin" $args --baseDN ou=identities "(objectclass=*)"
   ;;
 monitor)
-   kcmd ldapsearch $pwd_args --baseDN cn=monitor "(objectclass=*)"
+  # List monitor entries
+  setArgs dirmanager 1389
+  kcmd ldapsearch -D "uid=admin" $args --baseDN cn=monitor "(objectclass=*)"
    ;;
 list-backups)
+  # List backups
   kcmd dsbackup list --noPropertiesFile \
     --storageProperty gs.credentials.path:/var/run/secrets/cloud-credentials-cache/gcp-credentials.json \
     --backupLocation "${BACKUP_LOCATION}"
   ;;
 purge)
-kcmd dsbackup purge --noPropertiesFile --offline \
+  # Remove backups
+  kcmd dsbackup purge --noPropertiesFile --offline \
     --storageProperty gs.credentials.path:/var/run/secrets/cloud-credentials-cache/gcp-credentials.json \
     --backupLocation "${BACKUP_LOCATION}" \
     --olderThan '12h'
     ;;
+-h)
+  # Help
+  usage
+  ;;
 *)
-  kubectl exec $HOST -it -- $* $pwd_args \
+  kubectl exec $HOST -it -- $* $args \
 ;;
 esac
 
