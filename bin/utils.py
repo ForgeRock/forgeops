c340e6880eed26a854e582d8c2345eb5ffc204a0
Lee Baines <lee.baines@forgerock.com>
2022-05-11 14:50:07 +0100

    fix: Fix order of deleted objects to ensure backup objects are deleted if backup fails
    Delete pvcs before volumesnapshots otherwise if volumesnapshots are in pending state it can't be deleted as it is bound to a pvc.
    Delete the directorybackup and directoryrestore objects as part of `forgeops delete --force`.
    
    ref: CLOUD-3736

--- bin/utils.py
+++ bin/utils.py
@@ -428,9 +428,11 @@ def uninstall_component(component, ns, force):
         _, contents = generate_package(component, 'cdk', ns, '.', '', custom_path=uninstall_dir)
         run('kubectl', f'-n {ns} delete --ignore-not-found=true -f -', stdin=bytes(contents, 'ascii'))
         if component in ['base', 'base-cdm'] and force:
-            run('kubectl', f'-n {ns} delete all -l app.kubernetes.io/part-of=forgerock')
-            run('kubectl', f'-n {ns} delete volumesnapshot -l app.kubernetes.io/managed-by=ds-operator --ignore-not-found=true')
+            run('kubectl', f'-n {ns} delete directorybackup -l app.kubernetes.io/part-of=forgerock --ignore-not-found=true')
+            run('kubectl', f'-n {ns} delete directoryrestore -l app.kubernetes.io/part-of=forgerock --ignore-not-found=true')
             run('kubectl', f'-n {ns} delete pvc -l app.kubernetes.io/managed-by=ds-operator --ignore-not-found=true')
+            run('kubectl', f'-n {ns} delete volumesnapshot -l app.kubernetes.io/managed-by=ds-operator --ignore-not-found=true')
+            run('kubectl', f'-n {ns} delete all -l app.kubernetes.io/part-of=forgerock')
             uninstall_component('secrets', ns, False)
     except Exception as e:
         print(f'Could not delete {component}. Got: {e}')
