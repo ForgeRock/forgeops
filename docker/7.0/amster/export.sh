2caf1603636723650bcc569f5e713a538ef573b9
Lee Baines <lee.baines@forgerock.com>
2022-10-12 14:34:51 +0100

    fix: Fix amster overlays and export bugs
    Move amster overlays from kustomize/base to kustomize/overlay.
    Fix issues around export for cdk tool and Skaffold.
    Move realm entities to platform-config configmap so users can customize it.
    
    ref: CLOUD-4000

--- docker/7.0/amster/export.sh
+++ docker/7.0/amster/export.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 EXPORT_DIR="${EXPORT_DIR:-/var/tmp/amster}"
 
-# List of realm entities to export.
-realmEntities="OAuth2Clients IdentityGatewayAgents J2eeAgents WebAgents SoapStsAgents Policies CircleOfTrust Saml2Entity Applications"
-
 # Create a temporary export folder.
-rm -fr /var/tmp/amster
 mkdir -p /var/tmp/amster
 
 # Create Amster export script.
 cat > /tmp/do_export.amster <<EOF
 connect -k  /var/run/secrets/amster/id_rsa http://am:80/am
-export-config --path $EXPORT_DIR --realmEntities '${realmEntities}' --globalEntities ' '
+export-config --path '${EXPORT_DIR}' --realmEntities '${REALM_ENTITIES}' --globalEntities ' '
 :quit
 EOF
 
