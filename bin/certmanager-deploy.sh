c039a7b9f61e14d36bfc965622d13d8a19a075b4
Damon Conway <damon.conway@forgerock.com>
2022-11-10 13:13:25 -0600

    Refactoring to work with newest version of cert-manager.
    ref: CLOUD-4061

--- bin/certmanager-deploy.sh
+++ bin/certmanager-deploy.sh
@@ -9,25 +9,74 @@
 #kubectl delete apiservice v1beta1.webhook.cert-manager.io
 set -oe pipefail
 
-VERSION="v0.14.1"
+VERSION="v1.10.0"
 ISSUER="ca"
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CM_DIR="${CURRENT_DIR}/../cluster/addons/certmanager"
+DELETE=false
+
 
 # Print usage message to screen
 usage() {
-  printf "Usage: $0 [-l] [-d] \n\n"
-  exit 2
+  exit_code=$1
+  err=$2
+  prog=$(basename $0)
+  cat <<EOF
+Usage:
+$prog [OPTIONS] -i <instance_id>[,intance_id,...] -t <instance_type>
+
+Resize one or more EC2 instances.
+
+OPTIONS:
+-h|--help             : display usage and exit
+-V|--Version          : version of helm chart to install (vX.Y.Z)
+-d|--delete           : delete cert-manager and associated resources
+-l|--lets-encrypt     : use Let's Encrypt as issuer
+
+Requirements:
+* kubectl installed
+* helm 3.0+ installed
+
+Examples:
+Install with self-signed CA:
+$prog
+
+Install with Let's Encrypt CA:
+$prog -l
+
+Install with different version:
+$prog -V "v1.11.0"
+
+Delete install (using self-signed CA):
+$prog -d
+
+Delete install (using Let's Encrypt CA):
+$prog -d -l
+
+EOF
+
+  if [ ! -z "$err" ] ; then
+  echo "ERROR: $err"
+  echo
+  fi
+
+  exit $exit_code
 }
 
 # Deploy cert-manager
 deploy() {
 
-    # Deploy manifests
-    kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/${VERSION}/cert-manager.yaml
+    # Setup helm repo
+    helm repo add jetstack https://charts.jetstack.io --force-update
+    helm repo update
 
-    # Wait for webhook deployment to be ready
-    kubectl wait --for=condition=available deployment/cert-manager-webhook --timeout=300s -n cert-manager
+    # Install helm chart
+    helm install \
+      cert-manager jetstack/cert-manager \
+      --namespace cert-manager \
+      --create-namespace \
+      --version $VERSION \
+      --set installCRDs=true
 
     # Deploy Issuer.
     kubectl apply -f ${CM_DIR}/files/${ISSUER}-issuer.yaml -n cert-manager
@@ -48,19 +97,33 @@ deploy() {
 
 # Delete cert-manager and namespace
 delete() {
-    kubectl delete -f https://github.com/jetstack/cert-manager/releases/download/${VERSION}/cert-manager.yaml
+    # Delete our issuers and ca certificate
+    kubectl delete -f ${CM_DIR}/files/${ISSUER}-issuer.yaml -n cert-manager
+    # Delete cert-manager helm chart
+    helm --namespace cert-manager delete cert-manager
+    # Delete CRDs
+    kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/$VERSION/cert-manager.crds.yaml
 
     exit 1
 }
 
-# Validate arguments".
-[ $# -gt 0 ] && [[ ! ${1} =~ ^(-l|-d) ]] && usage
-[ $# -gt 0 ] && [[ ${1} =~ "-d" ]] && delete
-[ $# -gt 0 ] && [[ ${1} =~ "-l" ]] && ISSUER="le"
+while true; do
+  case "$1" in
+    -h|--help) usage 0 ;;
+    -V|--version) VERSION=$2; shift 2 ;;
+    -d|--delete) DELETE=true; shift ;;
+    -l|--lets-encrypt) ISSUER=le; shift ;;
+    *) break ;;
+  esac
+done
 
-# Deploy cert-mamager manifests
-deploy
+if [ "$DELETE" = true ] ; then
+  # Delete helm chart and Resources
+  delete
+else
+  # Deploy cert-mamager manifests
+  deploy
+fi
 
 # Delete decrypted service account
 rm -f ${CM_DIR}/secrets/cert-manager.json || true
-
