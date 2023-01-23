5063c64bf1f3b33cefdc5883421ac36ea43af3ff
Damon Conway <damon.conway@forgerock.com>
2023-01-23 16:38:07 -0600

    Adding in missing bin/stdlib.sh
    ref: CLOUD-4150

---
+++ bin/stdlib.sh
@@ -0,0 +1,31 @@
+runOrPrint() {
+  local result=0
+  if [ "$DRYRUN" = true ] || [ "$VERBOSE" = true ] || [ "$DEBUG" = true ] ; then
+    echo "$1"
+  fi
+
+  if [ "$DRYRUN" = false ] ; then
+    eval "$1"
+    result=$?
+  fi
+
+  return $result
+}
+
+message() {
+  if [ "$DEBUG" = true ] && [ "$2" == "debug" ] ; then
+    echo "$1"
+  elif [ "$DRYRUN" = true ] || [ "$VERBOSE" = true ] || [ "$DEBUG" = true ] && [ "$2" != "debug" ] ; then
+    echo "$1"
+  fi
+  if [ "$AUTO" = true ] && [ "$2" == "auto" ] ; then
+    echo "$1"
+  fi
+}
+
+# From http://stackoverflow.com/questions/3685970/check-if-an-array-contains-a-value
+containsElement () {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
