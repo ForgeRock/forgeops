337f23b007f392cfaf8937a476c73e9c5872443d
guillaume.andru <guillaume.andru@forgerock.com>
2023-06-01 17:06:27 +0200

    fix: Guillotine functional tests pipeline : updates

--- jenkins-scripts/stages/functional-tests.groovy
+++ jenkins-scripts/stages/functional-tests.groovy
@@ -7,6 +7,11 @@
  */
 
 // functional-tests.groovy
+  def authenticateGcloud() {
+    withCredentials([file(credentialsId: 'jenkins-guillotine-sa-key', variable: 'GC_KEY')]) {
+        sh("gcloud auth activate-service-account --key-file=${env.GC_KEY} --project=engineering-devops")
+    }
+}
 
 void runStage() {
     stage('Functional Tests')
@@ -14,6 +19,9 @@ void runStage() {
     // Create container to be able to use python3
     dockerUtils.insideGoogleCloudImage(dockerfilePath: 'docker/google-cloud', getDockerfile: true) {
         dir('guillotine') {
+
+            authenticateGcloud()
+
             localGitUtils.deepCloneBranch('ssh://git@stash.forgerock.org:7999/cloud/guillotine.git', 'master')
             def branchName = isPR() ? env.CHANGE_TARGET : env.BRANCH_NAME
             sh("./configure.py env --gke-only")
