858ccc047852fd7b463d5684d3d300ea1033a141
Lee Baines-Dark <lee.baines-dark@forgerock.com>
2023-06-21 15:10:24 +0100

    refactor: Remove the duplicate am and idm Kustomize bases
    Update IDM handling of truststore to ensure it can open an SSL connection to DS.
    Align the behaviour in am and idm bases with am-cdk and idm-cdk to work with both ds-operator and legacy deployments.
    Remove the am and idm base folders.
    Rename the am-cdk and idm-cdk bases to am and idm.
    Update the forgeops and utils scripts to work with the new base structure.
    
    ref: FORGEOPS-4386

---
+++ docker/idm/bin/import-pem-certs.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# This script copies the default cacerts to $TRUSTSTORE_PATH 
+# and imports all the certs contained in the $IDM_PEM_TRUSTSTORE if it exists
+
+#
+# Copyright 2019-2021 ForgeRock AS. All Rights Reserved
+#
+
+set -e 
+set -o pipefail
+
+IDM_DEFAULT_TRUSTSTORE=${IDM_DEFAULT_TRUSTSTORE:-$JAVA_HOME/lib/security/cacerts}
+# If a $IDM_PEM_TRUSTSTORE is provided, import it into the truststore. Otherwise, do nothing
+if [ -f "$IDM_DEFAULT_TRUSTSTORE" ] && ( [ -f "$IDM_PEM_TRUSTSTORE" ] || [ -f "$IDM_PEM_TRUSTSTORE_DS" ] ); then
+    TRUSTSTORE_PATH="${TRUSTSTORE_PATH:-/opt/openidm/idmtruststore}"
+    TRUSTSTORE_PASSWORD="${TRUSTSTORE_PASSWORD:-changeit}"
+    echo "Copying ${IDM_DEFAULT_TRUSTSTORE} to ${TRUSTSTORE_PATH}"
+    cp ${IDM_DEFAULT_TRUSTSTORE} ${TRUSTSTORE_PATH}
+    # Combine certs in a single file
+    cat $IDM_PEM_TRUSTSTORE $IDM_PEM_TRUSTSTORE_DS > idm_combined_truststore
+    # Calculate the number of certs in the PEM file
+    CERTS=$(grep 'END CERTIFICATE' idm_combined_truststore| wc -l)
+    echo "Found (${CERTS}) certificates in idm_combined_truststore"
+    echo "Importing (${CERTS}) certificates into ${TRUSTSTORE_PATH}"
+    # For every cert in the PEM file, extract it and import into the JKS truststore
+    for N in $(seq 0 $(($CERTS - 1))); do
+        ALIAS="imported-certs-$N"
+        cat idm_combined_truststore |
+            awk "n==$N { print }; /END CERTIFICATE/ { n++ }" |
+            keytool -noprompt -importcert -trustcacerts -storetype JKS \
+                    -alias "${ALIAS}" -keystore "${TRUSTSTORE_PATH}" \
+                    -storepass "${TRUSTSTORE_PASSWORD}"
+    done
+    echo "Import complete!"
+else
+    echo "Nothing was imported to the truststore. Check ENVs IDM_DEFAULT_TRUSTSTORE and IDM_PEM_TRUSTSTORE"
+    exit -1
+fi
