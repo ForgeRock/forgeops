5702f1788206a8859195634681e60d987119580e
Lee Baines-Dark <lee.baines-dark@forgerock.com>
2023-11-15 13:08:22 +0000

    fix: Update export.sh to copy the configurationservice into the folder to be exported.
    Update export.sh to copy the configurationservice into the folder to be exported so the config upgrader can read it.
    
    ref: FORGEOPS-4716

--- docker/am/export.sh
+++ docker/am/export.sh
@@ -2,6 +2,13 @@
 # Script to assist in exporting AM configuration
 
 cd /home/forgerock/openam
+
+cfgServiceDir="config/services/realm/root/configurationversionservice/1.0/globalconfig"
+
+# copy the configurationservice over so it can be exported and used by the am-config-upgrader
+mkdir -p ${cfgServiceDir}
+cp "/home/forgerock/cdk/${cfgServiceDir}/default.json" ${cfgServiceDir}
+
 # tar destination defaults to /home/forgerock/updated-config.tar
 # Pass `-` as the argument to output the tar stream to stdout. Use kubectl exec am-pod -- export.sh - > tar.out
 dest=${1:-"/home/forgerock/updated-config.tar"}
