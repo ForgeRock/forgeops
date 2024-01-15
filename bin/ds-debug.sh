96f99fcba256ada61da8aa0dcc0960a909766e84
guillaume.andru <guillaume.andru@forgerock.com>
2024-01-15 16:11:35 +0100

    FORGEOPS-4398 fix: allow ds-debug.sh rstatus to use dsrepl status options

--- bin/ds-debug.sh
+++ bin/ds-debug.sh
@@ -2,29 +2,54 @@
 # Utility script to run commands inside a ds container
 # This is an internal script provided for debugging purposes only and is not supported by ForgeRock.
 
+POD_NAME="ds-idrepo-0"
+POD_CREDENTIALS_FILE="/var/run/secrets/cloud-credentials-cache/gcp-credentials.json"
+
 usage() {
-  echo "Usage: $0 status|rstatus|disaster|idsearch|monitor|list-backups|purge [pod name]"
-  exit 1
+  echo "Usage: $0 [-p|--pod-name POD_NAME] SUB_COMMAND [SUB_COMMAND_OPTIONS]"
+  echo "Options:"
+  echo "  -p, --pod-name       Name of DS pod. Default is ${POD_NAME}"
+  echo "  SUB-COMMAND (run on DS pod) :"
+  echo "    status             Display basic server information"
+  echo "    rstatus            Check replication status"
+  echo "    disaster           Run disaster recovery"
+  echo "    idsearch           Run ldapsearch on ou=identities base dn"
+  echo "    monitor            Run ldapsearch on cn=monitor base dn"
+  echo "    list-backups       List backups stored in a cloud bucket"
+  echo "                       usage: $0 [-p|--pod-name POD_NAME] list-backups --backupLocation gs://BUCKET_PATH/POD_NAME"
+  echo "    purge              Remove backups older than 12h"
+  echo "                       usage: $0 [-p|--pod-name POD_NAME] purge --backupLocation gs://BUCKET_PATH/POD_NAME"
+  echo ""
+  echo "Note for list-backups and purge sub-commands :"
+  echo "  - limitation : work with Google Storage. Could be updated to work with AKS, EKS,... storages"
+  echo "  - DS pod must have Google Storage credentials file in ${POD_CREDENTIALS_FILE}"
+  echo ""
+  echo "Examples: $0 list-backups --backupLocation gs://my-bucket/ds-backup/project-1/site-1/ds-idrepo-0"
+  echo "          $0 purge --backupLocation gs://my-bucket/ds-backup/project-1/site-1/ds-idrepo-0"
+  echo "          $0 rstatus"
+  echo "          $0 -p ds-idrepo-0 rstatus -X"
+  exit 0
 }
 
 if [ "$#" -lt 1 ]; then
   usage
 fi
 
-cmd="$1"
 
-BACKUP_LOCATION="gs://ds-operator-engineering-devops/ds-backup-test"
-
-HOST="ds-idrepo-0"
-
-if [ -n "$2" ] ; then
-  if [[ "$2" == *"--help"* ]]; then
-    echo "[error] '${1}' subcommand does not have help usage"
-    usage
-    exit 1
-  fi
-  HOST="$2"
+# PARSING : First parse pod name
+OPTIND="1"
+if [ "${1}" == "-p" ] || [ "${1}" == "--pod-name"  ] ; then
+  POD_NAME="${2}"
+  shift
+  shift
 fi
+
+# PARSING : get command name
+ds_debug_custom_cmd="${*}"
+cmd="$1"
+shift
+# PARSING : get command options (if any)
+cmd_options="$*"
 
 # All the backends we know about..
 backends="ou=identities ou=tokens ou=am-config dc=openidm,dc=forgerock,dc=io"
@@ -35,68 +60,76 @@ done
 
 setArgs() {
   pw=$(kubectl get secret ds-passwords -o jsonpath="{.data.${1}\\.pw}" | base64 --decode)
-  args="-w $pw -p $2"
+  bind_args="-w $pw -p $2"
 }
 
 disaster() {
   echo "Running disaster recovery procedure to reset change log db for $*"
   echo "Starting in 5 seconds. Kill this NOW if you dont want to lose your changelog!"
   sleep 5
-  kcmd  dsrepl start-disaster-recovery -X $args $*
+  kcmd dsrepl start-disaster-recovery "${*}"
   echo "About to run the end DR command..."
-  kcmd  dsrepl end-disaster-recovery -X  $args $*
+  kcmd dsrepl end-disaster-recovery "${*}"
 }
 
 kcmd() {
-  echo $*
-  kubectl exec $HOST -it -- $*
+  echo "${*}"
+  kubectl exec "${POD_NAME}" -it -- ${*}
+}
+
+checkKey() {
+    kubectl exec "${POD_NAME}" -it -- ls ${POD_CREDENTIALS_FILE} > /dev/null 2>&1
+    if [[ $? -eq 2 ]] ; then
+      echo "error : looks like ${POD_NAME} does not have /var/run/secrets/cloud-credentials-cache/gcp-credentials.json"
+      echo "        please copy the json credential file (to access to your backup bucket) into this pod location
+      and rerun the script"
+      exit 1
+    fi
 }
 
 case "$cmd"  in
 status)
   # Display basic server information
   setArgs dirmanager 4444
-  kcmd status $args
+  kcmd status "${bind_args}" "${cmd_options}"
   ;;
 disaster)
   # Run disaster recovery
   setArgs dirmanager 4444
-  disaster $args
+  disaster "${bind_args}" "${cmd_options}"
   ;;
 rstatus)
   # Check replication status
   setArgs monitor 4444
-  kcmd dsrepl status --showReplicas --showChangeLogs $args
+  kcmd dsrepl status --showReplicas --showChangeLogs "${bind_args}" "${cmd_options}"
   ;;
 idsearch)
   # List identities
   setArgs dirmanager 1389
-  kcmd ldapsearch -D "uid=admin" $args --baseDN ou=identities "(objectclass=*)"
+  kcmd ldapsearch -D "uid=admin" "${bind_args}" --baseDN ou=identities "(objectclass=*)"
   ;;
 monitor)
   # List monitor entries
   setArgs dirmanager 1389
-  kcmd ldapsearch -D "uid=admin" $args --baseDN cn=monitor "(objectclass=*)"
+  kcmd ldapsearch -D "uid=admin" "${bind_args}" --baseDN cn=monitor "(objectclass=*)"
    ;;
 list-backups)
   # List backups
-  kcmd dsbackup list --noPropertiesFile \
-    --storageProperty gs.credentials.path:/var/run/secrets/cloud-credentials-cache/gcp-credentials.json \
-    --backupLocation "${BACKUP_LOCATION}"
+  checkKey
+  kcmd dsbackup list --noPropertiesFile --storageProperty gs.credentials.path:${POD_CREDENTIALS_FILE} "${cmd_options}"
   ;;
 purge)
   # Remove backups
-  kcmd dsbackup purge --noPropertiesFile --offline \
-    --storageProperty gs.credentials.path:/var/run/secrets/cloud-credentials-cache/gcp-credentials.json \
-    --backupLocation "${BACKUP_LOCATION}" \
-    --olderThan '12h'
+  checkKey
+  kcmd dsbackup purge --noPropertiesFile --storageProperty gs.credentials.path:${POD_CREDENTIALS_FILE}  --offline "${cmd_options}" --olderThan '12h'
     ;;
 -h | --help)
   # Help
   usage
   ;;
 *)
-  kubectl exec $HOST -it -- $* $args \
+  set -x
+  kubectl exec "${POD_NAME}" -it -- "${ds_debug_custom_cmd}"
 ;;
 esac
 
