9efc28b913e72a1fdf85997068307e9fd34d5491
Robin Banbury <robin.banbury@forgerock.com>
2024-02-28 16:46:15 +0000

    Revert "Remove lodestar-commit.txt file as not used anymore"
    This reverts commit 4e6cc089b8c15c57e8499eb6781f5e9bba9b4462.
    
    The lodestar-commit.txt file is still used by Core Identity
    product pipelines on the sustaining.7.1.x branches.

--- jenkins-scripts/libs/common.groovy
+++ jenkins-scripts/libs/common.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2024 ForgeRock AS. All Rights Reserved
+ * Copyright 2019-2021 ForgeRock AS. All Rights Reserved
  *
  * Use of this code requires a commercial software license with ForgeRock AS.
  * or with one of its affiliates. All use shall be exclusively subject
