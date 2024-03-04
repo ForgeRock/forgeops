b76f08f4012285c8456c58b9ae7b5edf8f8be68a
Christophe Sovant <christophe.sovant@forgerock.com>
2024-03-04 11:29:26 +0100

    Run the create-platform-images-pr stage on a specific node

--- jenkins-scripts/stages/create-platform-images-pr.groovy
+++ jenkins-scripts/stages/create-platform-images-pr.groovy
@@ -13,24 +13,26 @@ import com.forgerock.pipeline.stage.Status
 
 def runStage(PipelineRunLegacyAdapter pipelineRun) {
     pipelineRun.pushStageOutcome('create-platform-images-pr', stageDisplayName: 'Create Platform Images PR') {
-        stage('Create Platform Images PR') {
-            privateWorkspace {
-                def DEFAULT_PLATFORM_IMAGES_TAG = isPR() ? env.CHANGE_TARGET : env.BRANCH_NAME
+        node('google-cloud') {
+            stage('Create Platform Images PR') {
+                privateWorkspace {
+                    def DEFAULT_PLATFORM_IMAGES_TAG = isPR() ? env.CHANGE_TARGET : env.BRANCH_NAME
 
-                def platformImagesRevision = bitbucketUtils.getLatestCommitHash(
-                        'cloud',
-                        'platform-images',
-                        DEFAULT_PLATFORM_IMAGES_TAG)
+                    def platformImagesRevision = bitbucketUtils.getLatestCommitHash(
+                            'cloud',
+                            'platform-images',
+                            DEFAULT_PLATFORM_IMAGES_TAG)
 
-                def dockerProperties = [
-                        'gitCommit'           : commonModule.FORGEOPS_GIT_COMMIT,
-                        'platformImagesCommit': platformImagesRevision,
-                        'lodestarCommit'      : commonModule.LODESTAR_GIT_COMMIT,
-                ]
+                    def dockerProperties = [
+                            'gitCommit'           : commonModule.FORGEOPS_GIT_COMMIT,
+                            'platformImagesCommit': platformImagesRevision,
+                            'lodestarCommit'      : commonModule.LODESTAR_GIT_COMMIT,
+                    ]
 
-                return platformImageUtils.createPlatformImagePR('forgeops', env.BRANCH_NAME, dockerProperties)
-                        ? Status.SUCCESS.asOutcome()
-                        : Status.SKIPPED.asOutcome()
+                    return platformImageUtils.createPlatformImagePR('forgeops', env.BRANCH_NAME, dockerProperties)
+                            ? Status.SUCCESS.asOutcome()
+                            : Status.SKIPPED.asOutcome()
+                }
             }
         }
     }
