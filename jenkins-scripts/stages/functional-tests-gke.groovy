f995582ee5d3ea7e328366230e344a159956cca0
guillaume.andru <guillaume.andru@forgerock.com>
2024-11-21 17:11:38 +0100

    tests: add guillotine tests in jenkins daily tests

--- jenkins-scripts/stages/functional-tests-gke.groovy
+++ jenkins-scripts/stages/functional-tests-gke.groovy
@@ -9,7 +9,7 @@
 
 // functional-tests-gke.groovy
 void runStage() {
-    commonModule.runGuillotine(null, 'functional', 'GKE', '--keywords FUNCTIONAL')
+    commonModule.runGuillotine(null, 'functional', 'GKE', '--keywords "FUNCTIONAL GUILLOTINE"')
 }
 
 return this
