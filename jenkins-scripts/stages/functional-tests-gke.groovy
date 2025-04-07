b1474b17955e39a7ff1713fc95e592532d1319ae
guillaume.andru <guillaume.andru@forgerock.com>
2025-04-07 11:39:37 +0200

    test : disable tests

--- jenkins-scripts/stages/functional-tests-gke.groovy
+++ jenkins-scripts/stages/functional-tests-gke.groovy
@@ -10,8 +10,6 @@
 // functional-tests-gke.groovy
 void runStage() {
     commonModule.runGuillotine(null, 'forgeopsDevWithLatestPlatform', 'GKE', '--keywords "FUNCTIONAL"', '')
-    commonModule.runGuillotine(null, 'forgeopsDevWith7.5Platform', 'GKE', '--keywords "FUNCTIONAL"', 'sustaining/7.5.x-ready-for-dev-pipelines')
-    commonModule.runGuillotine(null, 'forgeopsDevWith7.4Platform', 'GKE', '--keywords "FUNCTIONAL"', 'sustaining/7.4.x-ready-for-dev-pipelines')
 }
 
 return this
