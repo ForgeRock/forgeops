5004c10f37891ddeaaff736454f465118ca01016
Lee Baines-Dark <lee.baines-dark@pingidentity.com>
2025-04-30 13:58:59 +0100

    refactor: Configure am-env-secrets for secret generator use
    Configure am-env-secrets in values.yaml to work with secret generator.
    Add custom entrypoint script to AM to base64 encode specific secrets prior to startup.
    
    ref: FORGEOPS-5759

---
+++ charts/identity-platform/files/am/entrypoint-scripts/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Base64 encode secret generate provisioned secrets if enabled
+if [ -n "${SECRET_GENERATOR_AM_ENV_SECRETS}" ] ; then
+    echo "updating env vars..."
+    AM_AUTHENTICATION_SHARED_SECRET=$(echo $AM_AUTHENTICATION_SHARED_SECRET|base64)
+    AM_SESSION_STATELESS_ENCRYPTION_KEY=$(echo $AM_SESSION_STATELESS_ENCRYPTION_KEY|base64)
+    AM_SESSION_STATELESS_SIGNING_KEY=$(echo $AM_SESSION_STATELESS_SIGNING_KEY|base64)
+    AM_SELFSERVICE_LEGACY_CONFIRMATION_EMAIL_LINK_SIGNING_KEY=$(echo $AM_SELFSERVICE_LEGACY_CONFIRMATION_EMAIL_LINK_SIGNING_KEY|base64)
+fi
+
+# Run upstream docker-entrypoint.sh
+/home/forgerock/docker-entrypoint.sh
+
