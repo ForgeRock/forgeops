f0de74a8fc687c8fb50b89a22c37fa303d450686
guillaume.andru <guillaume.andru@forgerock.com>
2025-08-04 19:43:20 +0200

    update guillotine cluster

--- jenkins-scripts/stages/functional-tests.groovy
+++ jenkins-scripts/stages/functional-tests.groovy
@@ -9,7 +9,7 @@
 
 // functional-tests.groovy
 void runStage() {
-    commonModule.runGuillotine(null, 'functional', '--keywords OLD_RELEASE --forgeops-profile cdk')
+    commonModule.runGuillotine(null, 'functional', '--keywords OLD_RELEASE --forgeops-profile cdk --cluster-name guillotine-tests-old')
 }
 
 return this
