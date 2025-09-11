11680b5da845f7c436dfd3329b9beae41d3d29cd
Lee Baines-Dark <lee.baines-dark@pingidentity.com>
2025-09-11 16:33:23 +0100

    fix: Add import-pem-certs.sh to AM docker dir to support backwards compatibility
    import-pem-certs.sh moved to a configmap in 2025.2.0 so images built from 2025.2.0 broken earlier releases.
    Adding import-pem-certs.sh to docker/am directory to support backwards compatibility.
    import-pem-certs.sh is still mounted via a configmap too. This allows for dynamic updates if required.
    
    ref: FORGEOPS-6071

---
+++ docker/am/import-pem-certs.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# This script copies the default cacerts to $TRUSTSTORE_PATH
+# and imports all the certs contained in the $AM_PEM_TRUSTSTORE if it exists
+
+#
+# Copyright 2023-2025 Ping Identity Corporation. All Rights Reserved
+#
+# This code is to be used exclusively in connection with Ping Identity
+# Corporation software or services. Ping Identity Corporation only offers
+# such software or services to legal entities who have entered into a
+# binding license agreement with Ping Identity Corporation.
+#
+
+set -e
+set -o pipefail
+
+AM_DEFAULT_TRUSTSTORE=${AM_DEFAULT_TRUSTSTORE:-$JAVA_HOME/lib/security/cacerts}
+# If a $AM_PEM_TRUSTSTORE is provided, import it into the truststore. Otherwise, do nothing
+if [ -f "$AM_DEFAULT_TRUSTSTORE" ] && ( [ -f "$AM_PEM_TRUSTSTORE" ] || [ -f "$AM_PEM_TRUSTSTORE_DS" ] || [ -f "$AM_PEM_TRUSTSTORE_EXTRA" ); then
+    TRUSTSTORE_PATH="${TRUSTSTORE_PATH:-/home/forgerock/amtruststore}"
+    TRUSTSTORE_PASSWORD="${TRUSTSTORE_PASSWORD:-changeit}"
+    echo "Copying ${AM_DEFAULT_TRUSTSTORE} to ${TRUSTSTORE_PATH}"
+    cp ${AM_DEFAULT_TRUSTSTORE} ${TRUSTSTORE_PATH}
+    # Combine certs in a single file
+    touch am_combined_truststore
+    [ -f "$AM_PEM_TRUSTSTORE" ] && cat $AM_PEM_TRUSTSTORE >> am_combined_truststore
+    [ -f "$AM_PEM_TRUSTSTORE_DS" ] && cat $AM_PEM_TRUSTSTORE_DS >> am_combined_truststore
+    [ -f "$AM_PEM_TRUSTSTORE_EXTRA" ] && cat $AM_PEM_TRUSTSTORE_EXTRA >> am_combined_truststore
+    # Calculate the number of certs in the PEM file
+    CERTS=$(grep 'END CERTIFICATE' am_combined_truststore| wc -l)
+    echo "Found (${CERTS}) certificates in am_combined_truststore"
+    echo "Importing (${CERTS}) certificates into ${TRUSTSTORE_PATH}"
+    # For every cert in the PEM file, extract it and import into the JKS truststore
+    for N in $(seq 0 $(($CERTS - 1))); do
+        ALIAS="imported-certs-$N"
+        cat am_combined_truststore |
+            awk "n==$N { print }; /END CERTIFICATE/ { n++ }" |
+            keytool -noprompt -importcert -trustcacerts -storetype JKS \
+                    -alias "${ALIAS}" -keystore "${TRUSTSTORE_PATH}" \
+                    -storepass "${TRUSTSTORE_PASSWORD}" || /bin/true
+    done
+    echo "Import complete!"
+else
+    echo "Nothing was imported to the truststore. Check ENVs AM_DEFAULT_TRUSTSTORE and AM_PEM_TRUSTSTORE"
+    exit -1
+fi
