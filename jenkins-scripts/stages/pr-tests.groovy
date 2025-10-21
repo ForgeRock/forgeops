38a6f2a89c419da62f0d6876f64342096ec12bd3
Christophe Sovant <christophe.sovant@pingidentity.com>
2025-10-21 14:40:45 +0200

    chore: Run pit1 tests in parallel of Guillotine tests in PRs
    ref: LODESTAR-2052

--- jenkins-scripts/stages/pr-tests.groovy
+++ jenkins-scripts/stages/pr-tests.groovy
@@ -11,72 +11,32 @@
 
 import com.forgerock.pipeline.reporting.PipelineRunLegacyAdapter
 
-void runStage(PipelineRunLegacyAdapter pipelineRun) {
+void runStage(PipelineRunLegacyAdapter pipelineRun, Random random) {
     def parallelTestsMap = [:]
 
     parallelTestsMap.put('PIT1',
         {
-            commonLodestarModule.runLodestar(pipelineRun, 'PIT1') { c -> cloud_tests.runCdmOrAicFuncPit1(c) }
+            commonLodestarModule.runSpyglaas(pipelineRun, random, 'Pit1',
+                [
+                    TESTS_SCOPE: 'tests/pit1',
+                ]
+            )
         }
     )
-
     parallelTestsMap.put('Perf postcommit',
         {
-            commonLodestarModule.runLodestar(pipelineRun, 'Perf Postcommit') { c -> cloud_tests.runCdmPerfPostcommit(c) }
+            commonLodestarModule.runPyrock(pipelineRun, random, 'Perf Postcommit',
+                [
+                    TEST_NAME      : 'postcommit',
+                    CONFIGFILE_NAME: 'conf-closed.yaml',
+                    PROFILE_NAME   : 'small',
+                ]
+            )
         }
     )
-
-    parallelTestsMap.put('AM K8s Postcommit',
+    parallelTestsMap.put('Guillotine',
         {
-            commonLodestarModule.runLodestar(pipelineRun, 'AM K8s Postcommit') { c -> cloud_tests.runCdmFuncAmK8sPostcommit(c) }
-        }
-    )
-
-    parallelTestsMap.put('Guillotine - Forgeops cli',
-            {
-                commonModule.runGuillotine(pipelineRun, 'Guillotine - Forgeops Test Group', '--test-names Forgeops', '')
-            }
-    )
-
-    parallelTestsMap.put('Guillotine - DS',
-            {
-                commonModule.runGuillotine(pipelineRun, 'Guillotine - DS', '--test-names Kustomize.DsBackup,Kustomize.DsBackupSnapshot,Kustomize.DsDebug', '')
-            }
-    )
-
-    parallelTestsMap.put('Guillotine - Upgrade',
-            {
-                commonModule.runGuillotine(pipelineRun, 'Guillotine - Upgrade', '--test-names Helm.UpgradeForgeops,Kustomize.UpgradePlatform74To75,Kustomize.UpgradeForgeops,Helm.BackwardsCompatibilityDev', '')
-            }
-    )
-
-    parallelTestsMap.put('Guillotine - IG',
-            {
-                commonModule.runGuillotine(pipelineRun, 'Guillotine - IG', '--test-names Kustomize.SmokeIG,Helm.SmokeIG', '')
-            }
-    )
-
-    parallelTestsMap.put('Guillotine - Acceptance',
-            {
-                commonModule.runGuillotine(pipelineRun, 'Guillotine - Acceptance', '--test-names Kustomize.Acceptance,Helm.Acceptance,Helm.ChangeSizeDeployment', '')
-            }
-    )
-
-    parallelTestsMap.put('Guillotine - Smoke - small profile',
-            {
-                commonModule.runGuillotine(pipelineRun, 'Guillotine - Smoke - small profile', '--test-names Kustomize.SmallProfile', '')
-            }
-    )
-
-    parallelTestsMap.put('Guillotine - Set Images',
-            {
-                commonModule.runGuillotine(pipelineRun, 'Guillotine - Set Images', '--test-names Kustomize.SetImages', '')
-            }
-    )
-
-    parallelTestsMap.put('Guillotine - Misc',
-            {
-                commonModule.runGuillotine(pipelineRun, 'Guillotine - Misc', '--test-names Kustomize.ForgeopsInfo,', '')
+            commonModule.runGuillotine(null, 'functional', 'GKE', '--keywords "PULL_REQUEST PLATFORM_IMAGE_REF"', '')
         }
     )
 
