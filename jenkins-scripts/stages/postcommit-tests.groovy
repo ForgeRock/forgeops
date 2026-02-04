cd32f9d677e3e50904ddb67f112810238c54fe29
Christophe Sovant <christophe.sovant@pingidentity.com>
2026-02-04 14:17:59 +0100

    chore: Update conf file name for postcommit 'Perf Restore' stage
    ref: LODESTAR-2166

--- jenkins-scripts/stages/postcommit-tests.groovy
+++ jenkins-scripts/stages/postcommit-tests.groovy
@@ -151,7 +151,7 @@ def runPostcommitSet0(PipelineRunLegacyAdapter pipelineRun, Random random, Linke
                     commonLodestarModule.runPyrock(pipelineRun, random, 'Perf Restore', clusterConfig +
                             [
                                     TEST_NAME      : 'platform',
-                                    CONFIGFILE_NAME: 'conf-postcommit-forgeops-perf-restore-100k-closed.yaml',
+                                    CONFIGFILE_NAME: 'conf-postcommit-restore-100k-closed.yaml',
                             ]
                     )
                 }
