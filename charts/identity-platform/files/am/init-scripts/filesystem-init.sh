e367e54bddae0d86b020e53decbc429a2d33afbc
Damon Conway <damon.conway@pingidentity.com>
2026-03-05 17:08:58 -0600

    Setting up AM filesystem so root can be read-only
    ref: FORGEOPS-6453

--- charts/identity-platform/files/am/init-scripts/filesystem-init.sh
+++ charts/identity-platform/files/am/init-scripts/filesystem-init.sh
@@ -15,3 +15,10 @@ else
   # Keep a copy of the default boot.json to use in the main container in case of a container restart
   cp /fbc/config/boot.json /fbc/default-boot.json
 fi
+
+echo "Setting up writeable volume."
+echo "Creating tmp"
+mkdir -p /writeable/tmp
+echo "Copying /home/forgerock"
+mkdir -p /writeable/home
+cp -av /home/forgerock/ /writeable/home/forgerock
