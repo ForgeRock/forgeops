40c53e075a826fcff590bdefa2d81630f6c4118d
guillaume.andru <guillaume.andru@pingidentity.com>
2026-03-05 13:39:46 +0100

    FORGEOPS-6450 split testuites in parallel in jenkins

--- jenkins-scripts/stages/pr-tests.groovy
+++ jenkins-scripts/stages/pr-tests.groovy
@@ -34,10 +34,53 @@ void runStage(PipelineRunLegacyAdapter pipelineRun, Random random) {
             )
         }
     )
-    parallelTestsMap.put('Guillotine',
-        {
-            commonModule.runGuillotine(null, 'functional', 'GKE', '--keywords "PULL_REQUEST PLATFORM_IMAGE_REF"', '')
-        }
+
+    parallelTestsMap.put('Guillotine - Forgeops cli',
+            {
+                commonModule.runGuillotine(pipelineRun, 'Guillotine - Forgeops Test Group', '--test-names Forgeops', '')
+            }
+    )
+
+    parallelTestsMap.put('Guillotine - DS',
+            {
+                commonModule.runGuillotine(pipelineRun, 'Guillotine - DS', '--test-names Kustomize.DsBackup,Kustomize.DsBackupSnapshot,Kustomize.DsDebug', '')
+            }
+    )
+
+    parallelTestsMap.put('Guillotine - Upgrade',
+            {
+                commonModule.runGuillotine(pipelineRun, 'Guillotine - Upgrade', '--test-names Helm.UpgradeForgeops,Kustomize.UpgradePlatform74To75,Kustomize.UpgradeForgeops,Helm.BackwardsCompatibilityDev', '')
+            }
+    )
+
+    parallelTestsMap.put('Guillotine - IG',
+            {
+                commonModule.runGuillotine(pipelineRun, 'Guillotine - IG', '--test-names Kustomize.SmokeIG,Helm.SmokeIG', '')
+            }
+    )
+
+    parallelTestsMap.put('Guillotine - Acceptance',
+            {
+                commonModule.runGuillotine(pipelineRun, 'Guillotine - Acceptance', '--test-names Kustomize.Acceptance,Helm.Acceptance,Helm.ChangeSizeDeployment', '')
+            }
+    )
+
+    parallelTestsMap.put('Guillotine - Smoke - small profile',
+            {
+                commonModule.runGuillotine(pipelineRun, 'Guillotine - Smoke - small profile', '--test-names Kustomize.SmallProfile', '')
+            }
+    )
+
+    parallelTestsMap.put('Guillotine - Set Images',
+            {
+                commonModule.runGuillotine(pipelineRun, 'Guillotine - Set Images', '--test-names Kustomize.SetImages', '')
+            }
+    )
+
+    parallelTestsMap.put('Guillotine - Misc',
+            {
+                commonModule.runGuillotine(pipelineRun, 'Guillotine - Misc', '--test-names Kustomize.ForgeopsInfo,', '')
+            }
     )
 
     parallel parallelTestsMap
